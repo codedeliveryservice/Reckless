52279e594efe8bab266cf3e9267849c07b7a9d02
Arseny <93079612+codedeliveryservice@users.noreply.github.com>
2022-12-04 20:54:00 +0300

    Implement FEN

--- game/src/board/fen.rs
+++ game/src/board/fen.rs
@@ -1,11 +1,98 @@
+use crate::core::{color::Color, piece::Piece, square::Square};
+
 use super::Board;
 
-pub enum ParseFenError {}
+#[derive(Debug)]
+pub enum ParseFenError {
+    InvalidSpecification,
+    InvalidTurnColor,
+    InvalidPiece,
+}
 
+/// Implements interaction with the Forsyth–Edwards notation which is a standard way for describing
+/// a particular board position of a chess game.
+/// 
+/// See [Wikipedia article](https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation) for more information.
 pub struct Fen;
 
 impl Fen {
+    /// Returns the board corresponding to the specified Forsyth–Edwards notation.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the given notation is not valid.
     pub(crate) fn parse(fen: &str) -> Result<Board, ParseFenError> {
-        todo!()
+        InnerFen::default().parse(fen)
+    }
+}
+
+#[derive(Default)]
+struct InnerFen {
+    board: Board,
+}
+
+impl InnerFen {
+    const SEPARATOR: char = '/';
+
+    pub fn parse(mut self, fen: &str) -> Result<Board, ParseFenError> {
+        let parts: Vec<&str> = fen.split_whitespace().collect();
+
+        if parts.len() != 6 {
+            return Err(ParseFenError::InvalidSpecification);
+        }
+
+        self.set_pieces(parts[0])?;
+        self.set_turn(parts[1])?;
+
+        Ok(self.board)
+    }
+
+    fn set_pieces(&mut self, text: &str) -> Result<(), ParseFenError> {
+        let mut rank = 7;
+        let mut file = 0;
+
+        Ok(for c in text.chars() {
+            if c == Self::SEPARATOR {
+                rank -= 1;
+                file = 0;
+            } else if let Some(skip) = c.to_digit(10) {
+                file += skip;
+            } else {
+                let piece = self.parse_piece(c)?;
+                let color = self.parse_color(c);
+                let square = Square::from_axes(rank, file);
+
+                self.board.add_piece(piece, color, square);
+
+                file += 1;
+            }
+        })
+    }
+
+    fn parse_piece(&self, c: char) -> Result<Piece, ParseFenError> {
+        Ok(match c {
+            'P' | 'p' => Ok(Piece::Pawn),
+            'N' | 'n' => Ok(Piece::Knight),
+            'B' | 'b' => Ok(Piece::Bishop),
+            'R' | 'r' => Ok(Piece::Rook),
+            'Q' | 'q' => Ok(Piece::Queen),
+            'K' | 'k' => Ok(Piece::King),
+            _ => Err(ParseFenError::InvalidPiece),
+        }?)
+    }
+
+    fn parse_color(&self, c: char) -> Color {
+        match c.is_uppercase() {
+            true => Color::White,
+            false => Color::Black,
+        }
+    }
+
+    fn set_turn(&mut self, text: &str) -> Result<(), ParseFenError> {
+        Ok(self.board.state.turn = match text {
+            "w" => Ok(Color::White),
+            "b" => Ok(Color::Black),
+            _ => Err(ParseFenError::InvalidTurnColor),
+        }?)
     }
 }
