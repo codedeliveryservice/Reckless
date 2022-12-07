9c8b8e0d02cf9c9e2469478eb328a6310ed48a1a
Arseny <93079612+codedeliveryservice@users.noreply.github.com>
2022-12-07 16:10:14 +0300

    Add king move map wrapper

--- game/src/lookup.rs
+++ game/src/lookup.rs
@@ -1,2 +1,9 @@
+use crate::core::{bitboard::Bitboard, square::Square};
+
 mod attacks;
 mod maps;
+
+#[inline(always)]
+pub fn king_attacks(square: Square) -> Bitboard {
+    Bitboard(maps::KING_MAP[square.0 as usize])
+}
