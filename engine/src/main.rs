d8e50bf9a81294afe274c958531bb7f23fd59b4f
Arseny <93079612+codedeliveryservice@users.noreply.github.com>
2022-12-09 03:42:36 +0300

    Create Perft

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,15 +1,10 @@
+mod perft;
+
 use game::board::Board;
 
 fn main() {
-    let fen = "8/8/8/3nk3/8/3Q4/7p/7K w - - 0 1";
-    let board = Board::from_fen(fen).unwrap();
-
-    for m in board.generate_moves() {
-        println!(
-            "{:?} {:?} (capture: {})",
-            m.start(),
-            m.target(),
-            m.is_capture()
-        );
-    }
+    perft::run(
+        10,
+        &mut Board::from_fen("8/8/8/8/8/1k6/8/1K6 b - - 0 1").unwrap(),
+    );
 }
---
+++ engine/src/perft.rs
@@ -0,0 +1,69 @@
+//! Perft used for testing, debugging and benchmarking the move generator.
+//! This is achieved by enumerating the number of leaf nodes for a given depth.
+//! 
+//! See [Chess Programming Wiki article](https://www.chessprogramming.org/Perft) for more information.
+
+use std::time::Instant;
+
+use game::board::Board;
+
+/// Runs a performance test on the `Board` with the specified depth.
+#[rustfmt::skip]
+pub fn run(depth: u32, board: &mut Board) -> u64 {
+    println!("{}", "-".repeat(60));
+    println!("{:>12} {:>12} {:>13} {:>15}", "Move", "Nodes", "Elapsed", "NPS");
+    println!("{}", "-".repeat(60));
+
+    let now = Instant::now();
+
+    let mut nodes = 0;
+    let mut index = 1;
+
+    for mv in board.generate_moves() {
+        let now = Instant::now();
+
+        if board.make_move(mv).is_err() {
+            continue;
+        }
+
+        let count = perft(depth - 1, board);
+        nodes += count;
+        index += 1;
+
+        board.take_back();
+
+        let seconds = now.elapsed().as_secs_f32();
+        let knps = count as f32 / seconds / 1000f32;
+
+        println!("{:>3} {:>8} {:>12} {:>12.3}s {:>15.3} kN/s", index, mv, count, seconds, knps);
+    }
+
+    let seconds = now.elapsed().as_secs_f32();
+    let knps = nodes as f32 / seconds / 1000f32;
+
+    println!("{}", "-".repeat(60));
+    println!("{:>12} {:>12} {:>12.3}s {:>15.3} kN/s", "Total", nodes, seconds, knps);
+    println!("{}", "-".repeat(60));
+
+    nodes
+}
+
+#[inline(always)]
+fn perft(depth: u32, board: &mut Board) -> u64 {
+    if depth == 0 {
+        return 1;
+    }
+
+    let mut nodes = 0;
+
+    for mv in board.generate_moves() {
+        if board.make_move(mv).is_err() {
+            continue;
+        }
+
+        nodes += perft(depth - 1, board);
+        board.take_back();
+    }
+
+    nodes
+}
