532f824d0dc11f7d78599d94092616ed85bcc3f0
Arseny <93079612+codedeliveryservice@users.noreply.github.com>
2022-12-18 08:30:19 +0300

    Update test position

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -3,8 +3,6 @@ mod perft;
 use game::board::Board;
 
 fn main() {
-    perft::run(
-        5,
-        &mut Board::from_fen("8/8/2k5/3b4/4r3/8/7K/1Q6 w - - 0 1").unwrap(),
-    );
+    let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    perft::run(6, &mut Board::from_fen(fen).unwrap());
 }
