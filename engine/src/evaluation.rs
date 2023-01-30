c0b82cdd068ef48b3d6787050dbbfc7cb2e3da1d
Arseny <93079612+codedeliveryservice@users.noreply.github.com>
2023-01-30 15:16:03 +0300

    Add doc comment

--- engine/src/evaluation.rs
+++ engine/src/evaluation.rs
@@ -1,10 +1,13 @@
 mod material;
+
 pub mod score;
 
 use game::board::Board;
 
 use self::score::Score;
 
+/// Returns a statically evaluated `Score` relative to the white side,
+/// which means that positive values are better for the white side.
 pub fn evaluate(board: &Board) -> Score {
     material::evaluate_material(board)
 }
