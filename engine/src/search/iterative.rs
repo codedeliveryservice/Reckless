8609cb36374f4ac9f0b097e2fb82c2b32e43cc29
Arseny <93079612+codedeliveryservice@users.noreply.github.com>
2023-02-27 12:02:33 +0300

    Adjust aspiration window
    Also applies minor iterative deepening improvements

--- engine/src/search/iterative.rs
+++ engine/src/search/iterative.rs
@@ -1,37 +1,63 @@
 use std::time::Instant;
 
-use game::{Board, Score};
+use game::{Board, Move, Score};
 
 use super::{negamax, SearchParams, SearchThread};
 use crate::uci::{self, UciMessage};
 
+const WINDOW_MARGIN: Score = Score(50);
+
 pub fn iterative_search(board: &mut Board, mut thread: SearchThread) {
     let mut last_best = Default::default();
 
-    for depth in 1..=thread.tc.max_depth {
-        thread.nodes = 0;
-        thread.start_time = Instant::now();
+    let mut alpha = Score::NEGATIVE_INFINITY;
+    let mut beta = Score::INFINITY;
+    let mut depth = 1;
 
-        let params = SearchParams::new(board, Score::NEGATIVE_INFINITY, Score::INFINITY, depth, 0);
+    while depth <= thread.tc.max_depth {
+        let params = SearchParams::new(board, alpha, beta, depth, 0);
         let score = negamax::negamax_search(params, &mut thread);
 
-        if thread.tc.is_time_over() || thread.requested_termination() {
-            uci::send(UciMessage::BestMove(last_best));
-            return;
+        if interrupted(&thread) {
+            break;
         }
+
+        // Reset the window on failure and try again at the same depth with a full width window
+        if score <= alpha || score >= beta {
+            alpha = Score::NEGATIVE_INFINITY;
+            beta = Score::INFINITY;
+            continue;
+        }
+
+        // Set the window for the next iteration
+        alpha = score - WINDOW_MARGIN;
+        beta = score + WINDOW_MARGIN;
 
         let mut pv = vec![];
         thread.extract_pv_line(board, depth, &mut pv);
-        last_best = pv[0];
 
-        uci::send(UciMessage::SearchReport {
-            depth,
-            score,
-            pv: &pv,
-            nodes: thread.nodes,
-            duration: thread.start_time.elapsed(),
-        });
+        report_search_result(depth, score, &pv, &thread);
+
+        last_best = pv[0];
+        depth += 1;
+
+        thread.nodes = 0;
+        thread.start_time = Instant::now();
     }
 
     uci::send(UciMessage::BestMove(last_best));
+}
+
+fn interrupted(thread: &SearchThread) -> bool {
+    thread.tc.is_time_over() || thread.requested_termination()
+}
+
+fn report_search_result(depth: usize, score: Score, pv: &Vec<Move>, thread: &SearchThread) {
+    uci::send(UciMessage::SearchReport {
+        pv,
+        depth,
+        score,
+        nodes: thread.nodes,
+        duration: thread.start_time.elapsed(),
+    });
 }
