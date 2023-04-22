623baa5debf991dd3d81029e6482c523a50a5ddc
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2023-04-22 20:42:02 +0300

    Reset stopwatch and nodes for each iteration

--- search/src/iterative.rs
+++ search/src/iterative.rs
@@ -1,3 +1,5 @@
+use std::time::Instant;
+
 use game::{Board, Move, Score};
 
 use super::{negamax, SearchParams, SearchThread};
@@ -6,6 +8,8 @@ const WINDOW_MARGIN: Score = Score(50);
 
 pub fn iterative_search(mut board: Board, mut thread: SearchThread) {
     let mut last_best = Default::default();
+    let mut stopwatch = Instant::now();
+    let mut nodes = 0;
 
     let mut alpha = -Score::INFINITY;
     let mut beta = Score::INFINITY;
@@ -33,13 +37,13 @@ pub fn iterative_search(mut board: Board, mut thread: SearchThread) {
         let mut pv = vec![];
         thread.extract_pv_line(&mut board, depth, &mut pv);
 
-        report_search_result(depth, score, &pv, &thread);
+        report_search_result(depth, score, &pv, &stopwatch, thread.nodes - nodes);
 
         last_best = pv[0];
         depth += 1;
 
-        // thread.nodes = 0;
-        // thread.start_time = Instant::now();
+        stopwatch = Instant::now();
+        nodes = thread.nodes;
     }
 
     println!("bestmove {}", last_best);
@@ -49,15 +53,15 @@ fn interrupted(thread: &SearchThread) -> bool {
     thread.is_time_over() || thread.get_terminator()
 }
 
-fn report_search_result(depth: usize, score: Score, pv: &[Move], thread: &SearchThread) {
-    let duration = thread.start_time.elapsed();
+fn report_search_result(depth: usize, score: Score, pv: &[Move], stopwatch: &Instant, nodes: u32) {
+    let duration = stopwatch.elapsed();
 
-    let nps = thread.nodes as f32 / duration.as_secs_f32();
+    let nps = nodes as f32 / duration.as_secs_f32();
     let ms = duration.as_millis();
 
     print!(
         "info depth {} score cp {} nodes {} time {} nps {:.0} pv",
-        depth, score, thread.nodes, ms, nps
+        depth, score, nodes, ms, nps
     );
     pv.iter().for_each(|mv| print!(" {}", mv));
     println!();
