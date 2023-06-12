8bfa3d5084fff116ca779569b0872140b5edf28e
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2023-06-12 06:57:12 +0300

    Minor evaluation driver refactoring

--- evaluation/src/lib.rs
+++ evaluation/src/lib.rs
@@ -3,8 +3,10 @@ mod mobility;
 
 use game::{Board, Color, Piece, Score};
 
+/// The phase weights for each piece type.
 const PHASE_WEIGHTS: [i32; 6] = [0, 1, 1, 2, 4, 0];
-const TOTAL_PHASE: i32 = 24;
+/// The maximum reachable phase value (all pieces on the board).
+const MAX_PHASE: i32 = 24;
 
 /// Returns a statically evaluated `Score` relative to the white side,
 /// regardless of the color of the player who is currently making a move.
@@ -19,10 +21,7 @@ pub fn evaluate_absolute_score(board: &Board) -> Score {
     let mg_score = mg_psq + mg_material + mg_mobility;
     let eg_score = eg_psq + eg_material + eg_mobility;
 
-    let mg_phase = get_phase(board);
-    let eg_phase = TOTAL_PHASE - mg_phase;
-
-    (mg_score * mg_phase + eg_score * eg_phase) / TOTAL_PHASE
+    interpolate_score(board, mg_score, eg_score)
 }
 
 /// Returns a statically evaluated `Score` relative to the color
@@ -34,6 +33,12 @@ pub fn evaluate_relative_score(board: &Board) -> Score {
     }
 }
 
+/// Interpolates the midgame and endgame scores based on the current phase of the game.
+fn interpolate_score(board: &Board, mg_score: Score, eg_score: Score) -> Score {
+    let phase = get_phase(board);
+    (mg_score * phase + eg_score * (MAX_PHASE - phase)) / MAX_PHASE
+}
+
 /// Calculates the current phase of the game based on the number of pieces on the board.
 ///
 /// The phase is a number between `0` and `24`, where `24` means the game is in the opening
@@ -43,5 +48,5 @@ fn get_phase(board: &Board) -> i32 {
         .iter()
         .map(|&piece| board.pieces(piece).count() as i32 * PHASE_WEIGHTS[piece])
         .sum::<i32>()
-        .min(TOTAL_PHASE) // In case of early promotions
+        .min(MAX_PHASE) // In case of early promotions
 }
