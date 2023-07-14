8402694d79d8d40697d98d6ba585deea1786dd57
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2023-07-14 12:05:26 +0300

    Refactor `IterativeSearch`

--- search/src/search.rs
+++ search/src/search.rs
@@ -14,51 +14,52 @@ mod params;
 mod quiescence;
 mod thread;
 
-const WINDOW_MARGIN: Score = Score(50);
+const ASPIRATION_WINDOW_MARGIN: Score = Score(50);
 
 pub struct IterativeSearch {
     board: Board,
     thread: SearchThread,
+    alpha: Score,
+    beta: Score,
 }
 
 impl IterativeSearch {
+    /// Creates a new `IterativeSearch` instance.
     pub fn new(board: Board, thread: SearchThread) -> Self {
-        Self { board, thread }
+        Self {
+            board,
+            thread,
+            alpha: -Score::INFINITY,
+            beta: Score::INFINITY,
+        }
     }
 
+    /// Performs an iterative deepening search to find the best move.
     pub fn search(&mut self) {
         let mut last_best = Default::default();
-
-        let mut alpha = -Score::INFINITY;
-        let mut beta = Score::INFINITY;
         let mut depth = 1;
 
         while depth <= self.thread.tc.get_max_depth() {
             let stopwatch = Instant::now();
-            let mut search = AlphaBetaSearch::new(&mut self.board, &mut self.thread);
-            let score = search.search(SearchParams::new(alpha, beta, depth));
+            let score = self.alphabeta_search(depth);
 
             if self.thread.get_terminator() {
                 break;
             }
 
-            // Reset the window on failure and try again at the same depth with a full width window
-            if score <= alpha || score >= beta {
-                alpha = -Score::INFINITY;
-                beta = Score::INFINITY;
+            if !self.is_score_within_bounds(score) {
+                self.reset_aspiration_window();
                 continue;
             }
 
-            // Set the window for the next iteration
-            alpha = score - WINDOW_MARGIN;
-            beta = score + WINDOW_MARGIN;
+            self.update_aspiration_window(score);
 
             let pv = self.thread.get_principal_variation(&mut self.board, depth);
-
             self.report_search_result(depth, score, &pv, stopwatch);
 
             last_best = pv[0];
             depth += 1;
+
             self.thread.nodes = 0;
             self.thread.current_depth = depth;
         }
@@ -66,10 +67,34 @@ impl IterativeSearch {
         println!("bestmove {}", last_best);
     }
 
+    /// Performs a search at the given depth.
+    fn alphabeta_search(&mut self, depth: usize) -> Score {
+        let mut search = AlphaBetaSearch::new(&mut self.board, &mut self.thread);
+        search.search(SearchParams::new(self.alpha, self.beta, depth))
+    }
+
+    /// Returns `true` if the given score is within the aspiration window.
+    fn is_score_within_bounds(&self, score: Score) -> bool {
+        self.alpha < score && score < self.beta
+    }
+
+    /// Updates the aspiration window to be centered around the given score.
+    fn update_aspiration_window(&mut self, score: Score) {
+        self.alpha = score - ASPIRATION_WINDOW_MARGIN;
+        self.beta = score + ASPIRATION_WINDOW_MARGIN;
+    }
+
+    /// Resets the aspiration window to its default values (unbounded).
+    fn reset_aspiration_window(&mut self) {
+        self.alpha = -Score::INFINITY;
+        self.beta = Score::INFINITY;
+    }
+
+    /// Reports the result of a search iteration using the `info` UCI command.
     fn report_search_result(&self, depth: usize, score: Score, pv: &[Move], stopwatch: Instant) {
-        let duration = stopwatch.elapsed();
-        let nps = self.thread.nodes as f32 / duration.as_secs_f32();
-        let ms = duration.as_millis();
+        let nodes = self.thread.nodes;
+        let nps = nodes as f32 / stopwatch.elapsed().as_secs_f32();
+        let ms = stopwatch.elapsed().as_millis();
 
         let score = match score.checkmate_in() {
             Some(moves) => format!("mate {}", moves),
@@ -78,7 +103,7 @@ impl IterativeSearch {
 
         print!(
             "info depth {} score {} nodes {} time {} nps {:.0} pv",
-            depth, score, self.thread.nodes, ms, nps
+            depth, score, nodes, ms, nps
         );
         pv.iter().for_each(|mv| print!(" {}", mv));
         println!();
