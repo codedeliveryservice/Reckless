d240463af60c2ef5d0b5426f0d2338591aab8da2
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-01-01 23:20:33 +0300

    Quantization
    Bench: 2065353

--- src/nnue.rs
+++ src/nnue.rs
@@ -2,12 +2,15 @@ use crate::types::{Color, Piece, Square};
 
 const INPUT_SIZE: usize = 768;
 const HIDDEN_SIZE: usize = 32;
-const K: f32 = 400.0;
+
+const K: i32 = 400;
+const L0: i32 = 256;
+const L1: i32 = 64;
 
 #[derive(Clone)]
 pub struct Network {
-    accumulator: [f32; HIDDEN_SIZE],
-    stack: Vec<[f32; HIDDEN_SIZE]>,
+    accumulator: [i16; HIDDEN_SIZE],
+    stack: Vec<[i16; HIDDEN_SIZE]>,
 }
 
 impl Network {
@@ -23,11 +26,11 @@ impl Network {
 
     /// Computes the output score for the given color.
     pub fn evaluate(&self) -> i32 {
-        let mut output = PARAMETERS.output_bias;
+        let mut output = i32::from(PARAMETERS.output_bias);
         for i in 0..HIDDEN_SIZE {
-            output += relu(self.accumulator[i]) * PARAMETERS.output_weights[i];
+            output += i32::from(relu(self.accumulator[i])) * i32::from(PARAMETERS.output_weights[i]);
         }
-        (output * K) as i32
+        output * K / (L0 * L1)
     }
 
     /// Activates the specified piece.
@@ -51,8 +54,8 @@ fn index(color: Color, piece: Piece, square: Square) -> usize {
     384 * color as usize + 64 * piece as usize + square as usize
 }
 
-fn relu(x: f32) -> f32 {
-    x.max(0.0)
+fn relu(x: i16) -> i16 {
+    x.max(0)
 }
 
 impl Default for Network {
@@ -65,10 +68,10 @@ impl Default for Network {
 }
 
 struct Parameters {
-    input_weights: [[f32; HIDDEN_SIZE]; INPUT_SIZE],
-    input_bias: [f32; HIDDEN_SIZE],
-    output_weights: [f32; HIDDEN_SIZE],
-    output_bias: f32,
+    input_weights: [[i16; HIDDEN_SIZE]; INPUT_SIZE],
+    input_bias: [i16; HIDDEN_SIZE],
+    output_weights: [i16; HIDDEN_SIZE],
+    output_bias: i16,
 }
 
-static PARAMETERS: Parameters = unsafe { std::mem::transmute(*include_bytes!(r"D:\Repos\RecklessTrainer\networks\random.nnue")) };
+static PARAMETERS: Parameters = unsafe { std::mem::transmute(*include_bytes!(r"D:\Repos\RecklessTrainer\networks\nn-07-quantized-256-64.nnue")) };
