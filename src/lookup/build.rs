ccda19189e335c6e6445e4e1f2e67eed018fc91f
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-02-25 11:06:54 +0300

    Clean up `build.rs`
    Bench: 2681032

--- src/lookup/build.rs
+++ src/lookup/build.rs
@@ -9,53 +9,34 @@ mod attacks;
 mod magics;
 mod maps;
 
-macro_rules! write_map {
-    ($f:ident, $name:tt, $type:tt, $items:expr) => {
-        let size = $items.len();
-        writeln!($f, "static {}: [{}; {}] = [", $name, $type, size)?;
-        for item in $items {
-            write!($f, "{},", item)?;
-        }
-        writeln!($f, "];")?;
-    };
-}
-
 fn main() {
-    write_lookup(get_buf("lookup.rs")).unwrap();
+    let dir = env::var("OUT_DIR").unwrap();
+    let path = Path::new(&dir).join("lookup.rs");
+    let out = File::create(path).unwrap();
+    write(BufWriter::new(out)).unwrap();
 }
 
-fn write_lookup(mut f: BufWriter<File>) -> Result<(), std::io::Error> {
-    write_map!(f, "KING_MAP", "u64", maps::generate_king_map());
-    write_map!(f, "KNIGHT_MAP", "u64", maps::generate_knight_map());
+fn write(mut buf: BufWriter<File>) -> Result<(), std::io::Error> {
+    macro_rules! write_map {
+        ($name:tt, $type:tt, $items:expr) => {
+            writeln!(buf, "static {}: [{}; {}] = {:?};", $name, $type, $items.len(), $items)?;
+        };
+    }
 
-    write_map!(f, "WHITE_PAWN_MAP", "u64", maps::generate_white_pawn_map());
-    write_map!(f, "BLACK_PAWN_MAP", "u64", maps::generate_black_pawn_map());
+    write_map!("KING_MAP", "u64", maps::generate_king_map());
+    write_map!("KNIGHT_MAP", "u64", maps::generate_knight_map());
 
-    write_map!(f, "ROOK_MAP", "u64", maps::generate_rook_map());
-    write_map!(f, "BISHOP_MAP", "u64", maps::generate_bishop_map());
+    write_map!("WHITE_PAWN_MAP", "u64", maps::generate_white_pawn_map());
+    write_map!("BLACK_PAWN_MAP", "u64", maps::generate_black_pawn_map());
 
-    write_map!(f, "ROOK_MAGICS", "MagicEntry", magics::ROOK_MAGICS);
-    write_map!(f, "BISHOP_MAGICS", "MagicEntry", magics::BISHOP_MAGICS);
+    write_map!("ROOK_MAP", "u64", maps::generate_rook_map());
+    write_map!("BISHOP_MAP", "u64", maps::generate_bishop_map());
+
+    write_map!("ROOK_MAGICS", "MagicEntry", magics::ROOK_MAGICS);
+    write_map!("BISHOP_MAGICS", "MagicEntry", magics::BISHOP_MAGICS);
 
     writeln!(
-        f,
+        buf,
         "struct MagicEntry {{ pub mask: u64, pub magic: u64, pub shift: u32, pub offset: u32 }}"
     )
-}
-
-fn get_buf(file: &str) -> BufWriter<File> {
-    let dir = env::var("OUT_DIR").unwrap();
-    let path = Path::new(&dir).join(file);
-    let out = File::create(path).unwrap();
-    BufWriter::new(out)
-}
-
-impl std::fmt::Display for magics::MagicEntry {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(
-            f,
-            "MagicEntry {{ mask: {}, magic: {}, shift: {}, offset: {} }}",
-            self.mask, self.magic, self.shift, self.offset
-        )
-    }
 }
