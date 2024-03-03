14685b1745115c56ba1285876d0ff5da7a8a74ee
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-03-03 16:03:57 +0300

    Disable move loop pruning in data generation
    Bench: 2895957

--- src/search/alphabeta.rs
+++ src/search/alphabeta.rs
@@ -83,6 +83,7 @@ impl super::Searcher<'_> {
         let mut ordering = self.build_ordering(&moves, entry.map(|entry| entry.mv));
 
         while let Some(mv) = moves.next(&mut ordering) {
+            #[cfg(not(feature = "datagen"))]
             if !ROOT && !PV && mv.is_quiet() && moves_played > 0 && best_score > -Score::MATE_BOUND {
                 if futility_pruning(depth, alpha, eval) {
                     break;
