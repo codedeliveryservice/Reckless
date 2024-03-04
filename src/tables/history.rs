0145b583303438866b0492f3de8da96a46841eac
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-03-04 19:49:13 +0300

    Follow-up move history
    Bench: 2625317

--- src/tables/history.rs
+++ src/tables/history.rs
@@ -1,4 +1,21 @@
-use crate::types::{Move, Square};
+use crate::types::{FullMove, Move, Piece, Square};
+
+const MAX_HISTORY: i32 = 512;
+
+/// Returns the bonus for a move based on the depth of the search.
+fn bonus(depth: i32) -> i32 {
+    depth * depth
+}
+
+/// Updates the score of an entry using a gravity function.
+fn update<const IS_GOOD: bool>(v: &mut i32, depth: i32) {
+    let bonus = bonus(depth);
+    if IS_GOOD {
+        *v += bonus - bonus * *v / MAX_HISTORY;
+    } else {
+        *v -= bonus + bonus * *v / MAX_HISTORY;
+    }
+}
 
 /// The history heuristic is a table that keep track of how successful a move has been in the past.
 /// The idea is that if a move has been successful in the past, it's likely to be successful in the
@@ -9,8 +26,6 @@ pub struct HistoryMoves {
     table: [[i32; Square::NUM]; Square::NUM],
 }
 
-const MAX_HISTORY: i32 = 512;
-
 impl HistoryMoves {
     /// Returns the score of a move.
     pub fn get(&self, mv: Move) -> i32 {
@@ -19,14 +34,12 @@ impl HistoryMoves {
 
     /// Increases the score of a move based on the depth of the search.
     pub fn increase(&mut self, mv: Move, depth: i32) {
-        let bonus = depth * depth;
-        self.table[mv.start()][mv.target()] += bonus - bonus * self.get(mv) / MAX_HISTORY;
+        update::<true>(&mut self.table[mv.start()][mv.target()], depth);
     }
 
     /// Decreases the score of a move based on the depth of the search.
     pub fn decrease(&mut self, mv: Move, depth: i32) {
-        let bonus = depth * depth;
-        self.table[mv.start()][mv.target()] -= bonus + bonus * self.get(mv) / MAX_HISTORY;
+        update::<false>(&mut self.table[mv.start()][mv.target()], depth);
     }
 }
 
@@ -34,6 +47,34 @@ impl Default for HistoryMoves {
     fn default() -> Self {
         Self {
             table: [[0; Square::NUM]; Square::NUM],
+        }
+    }
+}
+
+pub struct ContinuationHistory {
+    table: Box<[[[[i32; Square::NUM]; Piece::NUM]; Square::NUM]; Piece::NUM]>,
+}
+
+impl ContinuationHistory {
+    pub fn get(&self, previous: FullMove, current: FullMove) -> i32 {
+        self.table[previous.piece()][previous.target()][current.piece()][current.target()]
+    }
+
+    pub fn increase(&mut self, previous: FullMove, current: FullMove, depth: i32) {
+        let entry = &mut self.table[previous.piece()][previous.target()][current.piece()][current.target()];
+        update::<true>(entry, depth);
+    }
+
+    pub fn decrease(&mut self, previous: FullMove, current: FullMove, depth: i32) {
+        let entry = &mut self.table[previous.piece()][previous.target()][current.piece()][current.target()];
+        update::<false>(entry, depth);
+    }
+}
+
+impl Default for ContinuationHistory {
+    fn default() -> Self {
+        Self {
+            table: Box::new([[[[0; Square::NUM]; Piece::NUM]; Square::NUM]; Piece::NUM]),
         }
     }
 }
