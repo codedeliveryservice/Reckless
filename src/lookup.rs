167bf82db8c960d4ee1c7c75e10d18120a3e809a
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-03-26 07:07:13 +0300

    Access lookup tables without doing bounds checking
    Noticeable speedup in perft runs
    
    Benchmark 1: dev.exe
      Time (mean ± σ):      2.408 s ±  0.023 s
    
    Benchmark 2: main.exe
      Time (mean ± σ):      2.596 s ±  0.031 s
    
    Bench: 1986820

--- src/lookup.rs
+++ src/lookup.rs
@@ -3,32 +3,38 @@ use crate::types::{Bitboard, Color, Square};
 include!(concat!(env!("OUT_DIR"), "/lookup.rs"));
 
 pub fn pawn_attacks(square: Square, color: Color) -> Bitboard {
-    Bitboard(match color {
-        Color::White => WHITE_PAWN_MAP[square],
-        Color::Black => BLACK_PAWN_MAP[square],
-    })
+    unsafe {
+        match color {
+            Color::White => Bitboard(*WHITE_PAWN_MAP.get_unchecked(square as usize)),
+            Color::Black => Bitboard(*BLACK_PAWN_MAP.get_unchecked(square as usize)),
+        }
+    }
 }
 
 pub fn king_attacks(square: Square) -> Bitboard {
-    Bitboard(KING_MAP[square])
+    unsafe { Bitboard(*KING_MAP.get_unchecked(square as usize)) }
 }
 
 pub fn knight_attacks(square: Square) -> Bitboard {
-    Bitboard(KNIGHT_MAP[square])
+    unsafe { Bitboard(*KNIGHT_MAP.get_unchecked(square as usize)) }
 }
 
 pub fn rook_attacks(square: Square, occupancies: Bitboard) -> Bitboard {
-    let entry = &ROOK_MAGICS[square];
-    let index = magic_index(occupancies, entry);
+    unsafe {
+        let entry = ROOK_MAGICS.get_unchecked(square as usize);
+        let index = magic_index(occupancies, entry);
 
-    Bitboard(ROOK_MAP[index as usize])
+        Bitboard(*ROOK_MAP.get_unchecked(index as usize))
+    }
 }
 
 pub fn bishop_attacks(square: Square, occupancies: Bitboard) -> Bitboard {
-    let entry = &BISHOP_MAGICS[square];
-    let index = magic_index(occupancies, entry);
+    unsafe {
+        let entry = BISHOP_MAGICS.get_unchecked(square as usize);
+        let index = magic_index(occupancies, entry);
 
-    Bitboard(BISHOP_MAP[index as usize])
+        Bitboard(*BISHOP_MAP.get_unchecked(index as usize))
+    }
 }
 
 pub fn queen_attacks(square: Square, occupancies: Bitboard) -> Bitboard {
