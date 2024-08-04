5e37aa97e1b1695730baad3950f9f0d411ba031c
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-08-04 15:53:37 +0300

    Update bench positions and depth
    Bench: 2561241

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     datagen(std::env::args());
 
     match std::env::args().nth(1).as_deref() {
-        Some("bench") => tools::bench::<false>(10),
+        Some("bench") => tools::bench::<false>(12),
         _ => uci::message_loop(),
     }
 }
