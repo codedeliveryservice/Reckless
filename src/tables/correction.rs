46e436ea702afed86c530c1ce335c65e4acb0540
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-10-08 10:13:30 +0300

    Bench: 1977951

--- src/tables/correction.rs
+++ src/tables/correction.rs
@@ -1,33 +1,36 @@
 use crate::{board::Board, types::Color};
 
-const CORR_SIZE: usize = 32768;
-const CORR_GRAIN: i32 = 128;
-const CORR_MAX: i32 = 64;
-const MAX_CORR_HISTORY: i32 = CORR_GRAIN * CORR_MAX;
+const SIZE: usize = 32768;
+const GRAIN: i32 = 256;
+
+const MAX_DELTA: i32 = 64;
+const SMOOTHING_FACTOR: i32 = 1024;
 
 pub struct CorrectionHistory {
-    table: Box<[[i32; CORR_SIZE]; Color::NUM]>,
+    table: Box<[[i32; SIZE]; Color::NUM]>,
 }
 
 impl CorrectionHistory {
     pub fn get(&self, board: &Board) -> i32 {
-        self.table[board.side_to_move()][index(board)] / CORR_GRAIN
+        self.table[board.side_to_move()][index(board)] / GRAIN
     }
 
     pub fn update(&mut self, board: &mut Board, depth: i32, delta: i32) {
         let entry = &mut self.table[board.side_to_move()][index(board)];
-        let weight = (8 * depth).min(128);
 
-        *entry = (*entry + delta * weight).clamp(-MAX_CORR_HISTORY, MAX_CORR_HISTORY);
+        let delta = delta.clamp(-MAX_DELTA, MAX_DELTA) * GRAIN;
+        let weight = (10 * depth).min(128);
+
+        *entry = (*entry * (SMOOTHING_FACTOR - weight) + delta * weight) / SMOOTHING_FACTOR;
     }
 }
 
 impl Default for CorrectionHistory {
     fn default() -> Self {
-        Self { table: Box::new([[0; CORR_SIZE]; Color::NUM]) }
+        Self { table: Box::new([[0; SIZE]; Color::NUM]) }
     }
 }
 
 fn index(board: &Board) -> usize {
-    board.pawn_key() as usize % CORR_SIZE
+    board.pawn_key() as usize % SIZE
 }
