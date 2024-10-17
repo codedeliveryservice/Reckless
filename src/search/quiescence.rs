8936829ef6a6be7760ba300af9044220d96bf70d
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-10-17 05:24:21 +0300

    Bench: 2095549

--- src/search/quiescence.rs
+++ src/search/quiescence.rs
@@ -22,8 +22,8 @@ impl super::SearchThread<'_> {
         let eval = match entry {
             Some(entry) if should_cutoff(entry, alpha, beta) => return entry.score,
             Some(entry) => entry.score,
-            None => self.board.evaluate(),
-        } + self.corrhist.get(self.board);
+            None => self.board.evaluate() + self.corrhist.get(self.board),
+        };
 
         if eval > alpha {
             alpha = eval;
