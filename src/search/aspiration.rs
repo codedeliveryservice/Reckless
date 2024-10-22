09f5adaf383c9bbc639d31376e1cb20993f2aaa1
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2024-10-22 07:23:37 +0300

    Bench: 1901597

--- src/search/aspiration.rs
+++ src/search/aspiration.rs
@@ -3,14 +3,16 @@ use crate::{parameters::*, types::Score};
 
 impl<'a> SearchThread<'a> {
     pub fn aspiration_search(&mut self, mut score: i32, depth: i32) -> i32 {
-        // Avoid using an aspiration window for shallow depths, as the score is inconsistent
-        if depth <= aspiration_depth() {
-            return self.alpha_beta::<true, true>(-Score::INFINITY, Score::INFINITY, depth);
+        let mut alpha = -Score::INFINITY;
+        let mut beta = Score::INFINITY;
+
+        let mut delta = aspiration_delta() + score.pow(2) / 10000;
+
+        if depth >= aspiration_depth() {
+            alpha = (score - delta).max(-Score::INFINITY);
+            beta = (score + delta).min(Score::INFINITY);
         }
 
-        let mut delta = (aspiration_delta() - depth).max(10);
-        let mut alpha = (score - delta).max(-Score::INFINITY);
-        let mut beta = (score + delta).min(Score::INFINITY);
         let mut fail_high_count = 0;
 
         loop {
