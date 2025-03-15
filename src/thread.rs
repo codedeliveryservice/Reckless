5a97f0cc6e89f5ec953782ba2e77c83f12de416e
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-03-15 15:52:14 +0300

    Refactor search stack into a module
    Bench: 3881851

--- src/thread.rs
+++ src/thread.rs
@@ -1,5 +1,4 @@
 use std::{
-    ops::{Index, IndexMut},
     sync::atomic::{AtomicBool, Ordering},
     time::Instant,
 };
@@ -7,9 +6,10 @@ use std::{
 use crate::{
     board::Board,
     history::{ContinuationHistory, CorrectionHistory, NoisyHistory, QuietHistory},
+    stack::Stack,
     time::{Limits, TimeManager},
     transposition::TranspositionTable,
-    types::{is_loss, is_win, Move, Piece, Score, MAX_PLY},
+    types::{is_loss, is_win, Move, Score, MAX_PLY},
 };
 
 pub struct ThreadPool<'a> {
@@ -172,53 +172,6 @@ impl Default for PrincipalVariationTable {
             table: [[Move::NULL; MAX_PLY + 1]; MAX_PLY + 1],
             len: [0; MAX_PLY + 1],
         }
-    }
-}
-
-pub struct Stack {
-    data: [StackEntry; MAX_PLY + 8],
-}
-
-impl Default for Stack {
-    fn default() -> Self {
-        Self { data: [Default::default(); MAX_PLY + 8] }
-    }
-}
-
-#[derive(Copy, Clone)]
-pub struct StackEntry {
-    pub mv: Move,
-    pub piece: Piece,
-    pub eval: i32,
-    pub excluded: Move,
-    pub tt_pv: bool,
-    pub cutoff_count: i32,
-}
-
-impl Default for StackEntry {
-    fn default() -> Self {
-        Self {
-            mv: Move::NULL,
-            piece: Piece::None,
-            eval: Score::NONE,
-            excluded: Move::NULL,
-            tt_pv: false,
-            cutoff_count: 0,
-        }
-    }
-}
-
-impl Index<usize> for Stack {
-    type Output = StackEntry;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.data[index]
-    }
-}
-
-impl IndexMut<usize> for Stack {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.data[index]
     }
 }
 
