e9415d4022a13f9557651b541ef98330cc1e604b
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-03-16 02:03:11 +0300

    Bench: 4323157

--- src/search.rs
+++ src/search.rs
@@ -404,7 +404,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, beta: i32, depth:
                 reduction += 1024;
             }
 
-            let reduced_depth = (new_depth - reduction / 1024).max(1).min(new_depth);
+            let reduced_depth = (new_depth - reduction / 1024).clamp(1, new_depth + PV as i32);
 
             score = -search::<false>(td, -alpha - 1, -alpha, reduced_depth, true);
 
