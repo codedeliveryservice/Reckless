07db5250e2b4526da7e719b773e03c8dc746177d
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-03-16 13:46:53 +0300

    Bench: 4034828

--- src/movepick.rs
+++ src/movepick.rs
@@ -1,18 +1,22 @@
 use crate::{
     parameters::PIECE_VALUES,
     thread::ThreadData,
-    types::{Move, MoveList},
+    types::{ArrayVec, Move, MoveList, MAX_MOVES},
 };
 
 #[derive(Copy, Clone, PartialEq, PartialOrd)]
 pub enum Stage {
     HashMove,
     Scoring,
-    EverythingElse,
+    GoodNoisy,
+    Quiets,
+    BadNoisy,
 }
 
 pub struct MovePicker {
     list: MoveList,
+    bad_noisy: ArrayVec<Move, MAX_MOVES>,
+    bad_noisy_index: usize,
     tt_move: Move,
     threshold: i32,
     stage: Stage,
@@ -22,6 +26,8 @@ impl MovePicker {
     pub fn new(td: &ThreadData, tt_move: Move) -> Self {
         Self {
             list: td.board.generate_all_moves(),
+            bad_noisy: ArrayVec::new(),
+            bad_noisy_index: 0,
             tt_move,
             threshold: -110,
             stage: Stage::HashMove,
@@ -31,6 +37,8 @@ impl MovePicker {
     pub fn new_noisy(td: &ThreadData, include_quiets: bool, threshold: i32) -> Self {
         Self {
             list: if include_quiets { td.board.generate_all_moves() } else { td.board.generate_capture_moves() },
+            bad_noisy: ArrayVec::new(),
+            bad_noisy_index: 0,
             tt_move: Move::NULL,
             threshold,
             stage: Stage::Scoring,
@@ -38,10 +46,6 @@ impl MovePicker {
     }
 
     pub fn next(&mut self, td: &ThreadData) -> Option<(Move, i32)> {
-        if self.list.len() == 0 {
-            return None;
-        }
-
         if self.stage == Stage::HashMove {
             self.stage = Stage::Scoring;
 
@@ -53,8 +57,67 @@ impl MovePicker {
         }
 
         if self.stage == Stage::Scoring {
-            self.stage = Stage::EverythingElse;
+            self.stage = Stage::GoodNoisy;
             self.score_moves(td);
+        }
+
+        if self.stage == Stage::GoodNoisy {
+            loop {
+                let index = match self.find_best() {
+                    Some(index) => index,
+                    None => {
+                        self.stage = Stage::Quiets;
+                        break;
+                    }
+                };
+
+                let mv = self.list[index].mv;
+
+                if !mv.is_noisy() {
+                    self.stage = Stage::Quiets;
+                    break;
+                }
+
+                self.list.remove(index);
+
+                if !td.board.see(mv, self.threshold) {
+                    self.bad_noisy.push(mv);
+                    continue;
+                }
+
+                return Some((mv, 1 << 20));
+            }
+        }
+
+        if self.stage == Stage::Quiets {
+            let index = match self.find_best() {
+                Some(index) => index,
+                None => {
+                    self.stage = Stage::BadNoisy;
+                    return self.next(td);
+                }
+            };
+
+            let mv = self.list[index].mv;
+            let score = self.list[index].score;
+
+            self.list.remove(index);
+            return Some((mv, score));
+        }
+
+        // Stage::BadNoisy
+        if self.bad_noisy_index < self.bad_noisy.len() {
+            let mv = self.bad_noisy[self.bad_noisy_index];
+            self.bad_noisy_index += 1;
+
+            return Some((mv, -(1 << 20)));
+        }
+        None
+    }
+
+    fn find_best(&self) -> Option<usize> {
+        if self.list.len() == 0 {
+            return None;
         }
 
         let mut index = 0;
@@ -63,10 +126,7 @@ impl MovePicker {
                 index = i;
             }
         }
-
-        let entry = self.list[index];
-        self.list.remove(index);
-        Some((entry.mv, entry.score))
+        Some(index)
     }
 
     fn score_moves(&mut self, td: &ThreadData) {
