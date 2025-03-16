052b7f39f67bbd28943f37f8800badde97f93207
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-03-16 18:43:58 +0300

    bench: 3454968

--- src/search.rs
+++ src/search.rs
@@ -195,7 +195,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, beta: i32, depth:
         && !in_check
         && !excluded
         && depth <= 8
-        && eval >= beta + 80 * depth - (80 * improving as i32) - (60 * cut_node as i32)
+        && eval >= beta + 80 * depth - (80 * improving as i32) - (60 * cut_node as i32) - 60
     {
         return (eval + beta) / 2;
     }
