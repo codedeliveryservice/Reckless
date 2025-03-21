35e15754ed0518fddabcaef6fd6584b434f584c4
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-03-21 05:31:11 +0300

    Bench: 4659452

--- src/search.rs
+++ src/search.rs
@@ -228,6 +228,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && !in_check
         && !excluded
         && depth <= 8
+        && eval >= beta
         && eval >= beta + 80 * depth - (80 * improving as i32) - (60 * cut_node as i32)
     {
         return (eval + beta) / 2;
