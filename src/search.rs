c5a321523326ffd0489e1b7ce8f6c0eccd833122
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-03-22 17:04:26 +0300

    bench: 4596301

--- src/search.rs
+++ src/search.rs
@@ -562,6 +562,8 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 td.ply -= 1;
                 update_continuation_histories(td, td.stack[td.ply].piece, mv.to(), bonus);
                 td.ply += 1;
+            } else if score > alpha && score < best_score + 3 {
+                new_depth -= 1;
             }
         }
         // Full Depth Search (FDS)
