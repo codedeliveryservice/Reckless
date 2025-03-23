f88a98358749aa687c420e3671ebcf89853498a1
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-03-23 16:43:12 +0300

    Bench: 4473746

--- src/search.rs
+++ src/search.rs
@@ -270,7 +270,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     // ProbCut
     let probcut_beta = beta + 256 - 64 * improving as i32;
 
-    if depth >= 3 && !is_decisive(beta) && entry.is_none_or(|entry| entry.score >= probcut_beta) {
+    if !in_check && depth >= 3 && !is_decisive(beta) && entry.is_none_or(|entry| entry.score >= probcut_beta) {
         let mut move_picker = MovePicker::new_noisy(false, probcut_beta - static_eval);
 
         let probcut_depth = 0.max(depth - 4);
