bb8fe44efcf8e8eb5407310409754c8120a3e6d4
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-02 18:22:08 +0300

    Bench: 4140916

--- src/search.rs
+++ src/search.rs
@@ -226,8 +226,8 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
 
     let improving = !in_check && td.ply >= 2 && static_eval > td.stack[td.ply - 2].static_eval;
 
-    if td.ply >= 1 && td.stack[td.ply - 1].reduction >= 3072 && static_eval + td.stack[td.ply - 1].static_eval < 0 {
-        depth += 1;
+    if td.ply >= 1 && static_eval + td.stack[td.ply - 1].static_eval < 0 {
+        depth += td.stack[td.ply - 1].reduction / 2560;
     }
 
     td.stack[td.ply].static_eval = static_eval;
