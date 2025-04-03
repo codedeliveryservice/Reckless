452c74216c693b982e52a289ddb7db0b5b371edb
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-03 14:15:35 +0300

    Bench: 3926338

--- src/history.rs
+++ src/history.rs
@@ -62,7 +62,7 @@ impl Default for NoisyHistory {
 
 pub struct CorrectionHistory {
     // [side_to_move][key]
-    entries: Box<[[i16; Self::SIZE]; 2]>,
+    entries: Box<[[i16; 2]; Self::SIZE]>,
 }
 
 impl CorrectionHistory {
@@ -72,11 +72,11 @@ impl CorrectionHistory {
     const MASK: usize = Self::SIZE - 1;
 
     pub fn get(&self, stm: Color, key: u64) -> i32 {
-        (self.entries[stm][key as usize & Self::MASK] / 96) as i32
+        (self.entries[key as usize & Self::MASK][stm] / 96) as i32
     }
 
     pub fn update(&mut self, stm: Color, key: u64, depth: i32, diff: i32) {
-        let entry = &mut self.entries[stm][key as usize & Self::MASK];
+        let entry = &mut self.entries[key as usize & Self::MASK][stm];
         let bonus = (diff * depth).clamp(-Self::MAX_HISTORY / 4, Self::MAX_HISTORY / 4);
 
         *entry += (bonus - bonus.abs() * (*entry) as i32 / Self::MAX_HISTORY) as i16;
