942221b9a7b25c576a799ff97dadf1eb0ec50bb6
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-05 09:16:47 +0300

    Bench: 4228795

--- src/search.rs
+++ src/search.rs
@@ -377,7 +377,9 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
             skip_quiets |= move_count >= lmp_threshold(depth, improving);
 
             // Futility Pruning (FP)
-            skip_quiets |= !in_check && is_quiet && lmr_depth < 10 && static_eval + 100 * lmr_depth + 150 <= alpha;
+            if !in_check && is_quiet && lmr_depth < 10 && static_eval + 100 * lmr_depth + 150 <= alpha {
+                continue;
+            }
 
             // Static Exchange Evaluation Pruning (SEE Pruning)
             let threshold = if is_quiet { -30 * lmr_depth * lmr_depth } else { -95 * depth } - history / 32;
