061ee8f7f090d516b72b1c13096b456d8a7cac30
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-04-11 02:55:51 +0300

    bench: 6494189

--- src/search.rs
+++ src/search.rs
@@ -277,7 +277,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && static_eval >= beta - 20 * depth + 128 * tt_pv as i32 + 180
         && td.board.has_non_pawns()
     {
-        let r = 4 - correction_value.abs() / 256
+        let r = 4 - correction_value.abs() / 128
             + depth / 3
             + ((eval - beta) / 256).min(3)
             + tt_move.is_noisy() as i32
