9ac842defa49f675e83b1f5fba5a15e6c4088239
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-11 16:13:28 +0300

    Bench: 5279515

--- src/search.rs
+++ src/search.rs
@@ -276,7 +276,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && eval >= static_eval
         && static_eval
             >= beta - 20 * depth + 128 * tt_pv as i32 + 180
-                - td.nmp_history.get(td.board.side_to_move(), td.stack[td.ply - 1].mv) / 16
+                - (td.nmp_history.get(td.board.side_to_move(), td.stack[td.ply - 1].mv) / 16).clamp(-64, 32)
         && td.board.has_non_pawns()
     {
         let r = 4 + depth / 3 + ((eval - beta) / 256).min(3) + (tt_move.is_null() || tt_move.is_noisy()) as i32;
