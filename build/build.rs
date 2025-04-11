d0973c7e3f9bb5974efae4333508da5622875d84
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-11 19:47:36 +0300

    Bench: 5104189

--- build/build.rs
+++ build/build.rs
@@ -11,7 +11,7 @@ mod magics;
 mod maps;
 
 const BASE_URL: &str = "https://github.com/codedeliveryservice/RecklessNetworks/raw/main";
-const NETWORK_NAME: &str = "v12-25011a2e.nnue";
+const NETWORK_NAME: &str = "v13-1aa4e763.nnue";
 
 fn main() {
     generate_model_env();
