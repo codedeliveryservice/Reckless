bb9907ab7fb2cfd536a11a9e6ea419a2c6f1c4aa
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-12 04:15:56 +0300

    Bench: 5265420

--- src/search.rs
+++ src/search.rs
@@ -488,22 +488,22 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 reduction += 1024;
             }
 
+            if td.board.in_check() {
+                reduction -= 1024;
+            }
+
+            if !improving {
+                reduction += 1024;
+            }
+
+            if td.stack[td.ply].cutoff_count > 3 {
+                reduction += 1024;
+            }
+
             if is_quiet {
                 reduction -= 4 * correction_value.abs();
 
                 reduction -= (history - 512) / 16;
-
-                if td.board.in_check() {
-                    reduction -= 1024;
-                }
-
-                if !improving {
-                    reduction += 1024;
-                }
-
-                if td.stack[td.ply].cutoff_count > 3 {
-                    reduction += 1024;
-                }
 
                 if td.stack[td.ply - 1].killer == mv {
                     reduction -= 1024;
