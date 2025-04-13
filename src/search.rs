d20d85de56981b18d64eb9a88bba7dbc92d56b51
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-13 03:18:19 +0300

    Bench: 4538059
    perform lmr for depth >= 2

--- src/search.rs
+++ src/search.rs
@@ -490,7 +490,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         }
 
         // Late Move Reductions (LMR)
-        if depth >= 3 && move_count > 1 + is_root as i32 && (is_quiet || !tt_pv) {
+        if depth >= 2 && move_count > 1 + is_root as i32 && (is_quiet || !tt_pv) {
             if tt_pv {
                 reduction -= 768 + entry.is_some_and(|entry| entry.score > alpha) as i32 * 768;
             }
