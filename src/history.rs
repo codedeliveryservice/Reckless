23e744c1e1a86b6f875076e2c5fae6ba6bcd80f3
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-14 05:45:33 +0300

    Bench: 4890810
    25% -> 40% of the maximum history value

--- src/history.rs
+++ src/history.rs
@@ -12,7 +12,7 @@ struct QuietHistoryEntry {
 }
 
 impl QuietHistoryEntry {
-    const MAX_FACTORIZER: i32 = 2048;
+    const MAX_FACTORIZER: i32 = 4096;
     const MAX_BUCKET: i32 = 6144;
 
     pub fn bucket(&self, threats: Bitboard, mv: Move) -> i16 {
