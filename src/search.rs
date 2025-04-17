73476640b13afa370d6e31a5e550cf07eb589892
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-17 09:36:13 +0300

    Bench: 5191641

--- src/search.rs
+++ src/search.rs
@@ -794,7 +794,7 @@ fn qsearch<const PV: bool>(td: &mut ThreadData, mut alpha: i32, beta: i32) -> i3
                 continue;
             }
 
-            if !in_check && futility_score <= alpha && !td.board.see(mv, 1) {
+            if !in_check && futility_score <= alpha && !td.board.see(mv, 128 + (futility_score - alpha) / 8) {
                 best_score = best_score.max(futility_score);
                 continue;
             }
