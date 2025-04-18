3e10d6120e4f66d641b79da2ed4cad4f7f7fd22d
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-18 03:57:51 +0300

    Bench: 4662411

--- src/search.rs
+++ src/search.rs
@@ -347,6 +347,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     let probcut_beta = beta + 256 - 64 * improving as i32;
 
     if depth >= 3 && !is_decisive(beta) && entry.is_none_or(|entry| entry.score >= probcut_beta) {
+        let mut move_count = 0;
         let mut move_picker = MovePicker::new_probcut(probcut_beta - static_eval);
 
         let probcut_depth = 0.max(depth - 4);
@@ -356,9 +357,15 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 break;
             }
 
+            if move_count >= 2 + 2 * cut_node as i32 {
+                break;
+            }
+
             if mv == td.stack[td.ply].excluded || !td.board.is_legal(mv) {
                 continue;
             }
+
+            move_count += 1;
 
             td.stack[td.ply].piece = td.board.moved_piece(mv);
             td.stack[td.ply].mv = mv;
