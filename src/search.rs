fca105065119cc299198d6f49929abb32aa6c02f
Arseniy <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-19 18:53:54 +0300

    Bench: 4995196

--- src/search.rs
+++ src/search.rs
@@ -528,6 +528,10 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 reduction -= 1024;
             }
 
+            if entry.is_some_and(|entry| (entry.score - static_eval).abs() < 16) {
+                reduction += 1024;
+            }
+
             reduction -= (history - 512) / 16;
 
             let reduced_depth = (new_depth - reduction / 1024).clamp(0, new_depth);
