43e3a5849066554d4fff8ed366303da7572234ec
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-21 13:44:15 +0300

    Bench: 4794802

--- src/board/movegen.rs
+++ src/board/movegen.rs
@@ -32,13 +32,17 @@ impl super::Board {
     fn generate_moves<const TYPE: u8>(&self, list: &mut MoveList) {
         let occupancies = self.occupancies();
 
+        if self.checkers().multiple() {
+            self.collect_for::<TYPE, _>(list, PieceType::King, king_attacks);
+            return;
+        }
+
         self.collect_pawn_moves::<TYPE>(list);
 
         self.collect_for::<TYPE, _>(list, PieceType::Knight, knight_attacks);
         self.collect_for::<TYPE, _>(list, PieceType::Bishop, |square| bishop_attacks(square, occupancies));
         self.collect_for::<TYPE, _>(list, PieceType::Rook, |square| rook_attacks(square, occupancies));
         self.collect_for::<TYPE, _>(list, PieceType::Queen, |square| queen_attacks(square, occupancies));
-        self.collect_for::<TYPE, _>(list, PieceType::King, king_attacks);
 
         if TYPE == QUIET {
             self.collect_castling(list);
