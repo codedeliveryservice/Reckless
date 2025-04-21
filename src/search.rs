8554372fe029d6c579204ebc66b956faea7b82aa
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-21 12:57:28 +0300

    Bench: 4874327

--- src/search.rs
+++ src/search.rs
@@ -249,10 +249,8 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         td.quiet_history.update(td.board.prior_threats(), !td.board.side_to_move(), td.stack[td.ply - 1].mv, bonus);
     }
 
-    let improving = !in_check
-        && td.ply >= 2
-        && td.stack[td.ply - 1].mv.is_valid()
-        && static_eval > td.stack[td.ply - 2].static_eval;
+    let improving = (!in_check && td.ply >= 2 && static_eval > td.stack[td.ply - 2].static_eval)
+        || (td.ply >= 1 && td.stack[td.ply - 1].mv.is_null());
 
     if td.ply >= 1 && td.stack[td.ply - 1].reduction >= 3072 && static_eval + td.stack[td.ply - 1].static_eval < 0 {
         depth += 1;
