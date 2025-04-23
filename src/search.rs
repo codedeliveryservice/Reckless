8331f0238ee370064624fb1129129f703957334a
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-04-23 14:07:47 +0300

    Bench: 4543146

--- src/search.rs
+++ src/search.rs
@@ -421,6 +421,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         move_count += 1;
 
         let is_quiet = mv.is_quiet();
+        let gives_check = td.board.might_give_check_who_knows(mv);
 
         let history = if is_quiet {
             td.quiet_history.get(td.board.threats(), td.board.side_to_move(), mv)
@@ -443,7 +444,9 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
             skip_quiets |= !in_check && is_quiet && lmr_depth < 10 && static_eval + 100 * lmr_depth + 150 <= alpha;
 
             // Static Exchange Evaluation Pruning (SEE Pruning)
-            let threshold = if is_quiet { -30 * lmr_depth * lmr_depth } else { -95 * depth + 50 } - history / 32;
+            let threshold =
+                if is_quiet && !gives_check { -30 * lmr_depth * lmr_depth } else { -95 * depth + 50 } - history / 32;
+
             if !td.board.see(mv, threshold) {
                 continue;
             }
