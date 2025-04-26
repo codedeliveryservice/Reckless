a56dcdafdfcb605fd03f20e109186dbfc078bdda
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-04-26 14:35:01 +0300

    bench: 4002545

--- src/search.rs
+++ src/search.rs
@@ -688,7 +688,12 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         td.ply -= 1;
         tt_pv |= td.stack[td.ply].tt_pv;
 
-        let factor = 2;
+        let factor = 1
+            + (depth > 5) as i32
+            + 2 * (!in_check && best_score <= td.stack[td.ply + 1].static_eval - 64) as i32
+            + 2 * (td.stack[td.ply].static_eval != Score::NONE && best_score <= -td.stack[td.ply].static_eval - 64)
+                as i32;
+
         let scaled_bonus = factor * stat_bonus(depth);
 
         let pcm_move = td.stack[td.ply].mv;
