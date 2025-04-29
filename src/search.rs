57245fbcdef38441c87e4e76ff2368c9e7ee9128
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-04-29 18:25:45 +0300

    bench: 5104120

--- src/search.rs
+++ src/search.rs
@@ -509,7 +509,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
             }
 
             if PV {
-                reduction -= 768 + 768 * (beta - alpha > td.root_delta / 4) as i32;
+                reduction -= 1536 + 768 * (beta - alpha > td.root_delta / 4) as i32;
             }
 
             if cut_node {
