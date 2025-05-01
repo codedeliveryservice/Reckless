7b2eae20483e23a0a1f66a6123c861c5c0fad48a
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-02 02:26:20 +0300

    Bench: 7925367

--- src/parameters.rs
+++ src/parameters.rs
@@ -39,132 +39,132 @@ macro_rules! define {
 }
 
 define! {
-    i32 asp_delta: 12;
-    i32 asp_div:   32768;
+    i32 asp_delta: 10;
+    i32 asp_div: 32247;
 
     i32 eval_history: 6;
-    i32 eval_history_min: 64;
-    i32 eval_history_max: 128;
+    i32 eval_history_min: 67;
+    i32 eval_history_max: 160;
 
-    i32 retro_v1: 3072;
-    i32 retro_v2: 1024;
-    i32 retro_v3: 96;
+    i32 retro_v1: 2761;
+    i32 retro_v2: 1053;
+    i32 retro_v3: 81;
 
-    i32 razor_v1: 300;
-    i32 razor_v2: 250;
+    i32 razor_v1: 307;
+    i32 razor_v2: 235;
 
-    i32 rfp_v1: 8;
+    i32 rfp_v1: 7;
     i32 rfp_v2: 80;
-    i32 rfp_v3: 80;
-    i32 rfp_v4: 30;
+    i32 rfp_v3: 70;
+    i32 rfp_v4: 33;
     i32 rfp_v5: 512;
     i32 rfp_v6: 25;
 
-    i32 nmp_v1: 20;
-    i32 nmp_v2: 128;
-    i32 nmp_v3: 180;
-    i32 nmp_v4: 256;
+    i32 nmp_v1: 16;
+    i32 nmp_v2: 131;
+    i32 nmp_v3: 205;
+    i32 nmp_v4: 250;
     i32 nmp_v5: 3;
 
-    i32 probcut_v1: 256;
-    i32 probcut_v2: 64;
+    i32 probcut_v1: 302;
+    i32 probcut_v2: 66;
 
-    i32 fp_v1: 10;
-    i32 fp_v2: 100;
-    i32 fp_v3: 150;
+    i32 fp_v1: 9;
+    i32 fp_v2: 93;
+    i32 fp_v3: 166;
 
     i32 bnp_v1: 6;
-    i32 bnp_v2: 128;
+    i32 bnp_v2: 132;
 
-    i32 see_v1: 30;
-    i32 see_v2: 95;
-    i32 see_v3: 50;
-    i32 see_v4: 32;
+    i32 see_v1: 19;
+    i32 see_v2: 97;
+    i32 see_v3: 54;
+    i32 see_v4: 37;
 
-    i32 se_v1: 8;
-    i32 se_v2: 24;
-    i32 se_v3: 128;
+    i32 se_v1: 5;
+    i32 se_v2: 20;
+    i32 se_v3: 132;
 
-    i32 lmr_v1: 64;
-    i32 lmr_v2: 512;
-    i32 lmr_v3: 4096;
-    i32 lmr_v4: 64;
-    i32 lmr_v5: 256;
-    i32 lmr_v6: 768;
-    i32 lmr_v7: 768;
-    i32 lmr_v8: 768;
-    i32 lmr_v9: 768;
-    i32 lmr_v10: 768;
-    i32 lmr_v11: 1024;
-    i32 lmr_v12: 1024;
-    i32 lmr_v13: 1024;
-    i32 lmr_v14: 896;
-    i32 lmr_v15: 64;
-    i32 lmr_v16: 8;
-    i32 lmr_v17: 1024;
+    i32 lmr_v1: 84;
+    i32 lmr_v2: 554;
+    i32 lmr_v3: 4071;
+    i32 lmr_v4: 56;
+    i32 lmr_v5: 280;
+    i32 lmr_v6: 724;
+    i32 lmr_v7: 590;
+    i32 lmr_v8: 747;
+    i32 lmr_v9: 666;
+    i32 lmr_v10: 642;
+    i32 lmr_v11: 1105;
+    i32 lmr_v12: 967;
+    i32 lmr_v13: 827;
+    i32 lmr_v14: 670;
+    i32 lmr_v15: 63;
+    i32 lmr_v16: 7;
+    i32 lmr_v17: 978;
 
-    i32 dod: 64;
+    i32 dod: 68;
 
-    i32 post_v1: 128;
-    i32 post_v2: 64;
-    i32 post_v3: 1280;
-    i32 post_v4: 128;
-    i32 post_v5: 64;
-    i32 post_v6: 1280;
+    i32 post_v1: 143;
+    i32 post_v2: 65;
+    i32 post_v3: 1212;
+    i32 post_v4: 135;
+    i32 post_v5: 71;
+    i32 post_v6: 1177;
 
-    i32 pv_v1: 16;
+    i32 pv_v1: 15;
 
-    i32 bonus_noisy_v1: 128;
-    i32 bonus_noisy_v2: 64;
-    i32 bonus_noisy_v3: 1280;
+    i32 bonus_noisy_v1: 133;
+    i32 bonus_noisy_v2: 65;
+    i32 bonus_noisy_v3: 1270;
 
-    i32 malus_noisy_v1: 128;
-    i32 malus_noisy_v2: 64;
-    i32 malus_noisy_v3: 1280;
-    i32 malus_noisy_v4: 16;
+    i32 malus_noisy_v1: 143;
+    i32 malus_noisy_v2: 75;
+    i32 malus_noisy_v3: 1270;
+    i32 malus_noisy_v4: 15;
 
-    i32 bonus_quiet_v1: 128;
-    i32 bonus_quiet_v2: 64;
-    i32 bonus_quiet_v3: 1280;
+    i32 bonus_quiet_v1: 126;
+    i32 bonus_quiet_v2: 75;
+    i32 bonus_quiet_v3: 1325;
 
-    i32 malus_quiet_v1: 128;
-    i32 malus_quiet_v2: 64;
-    i32 malus_quiet_v3: 1280;
-    i32 malus_quiet_v4: 16;
+    i32 malus_quiet_v1: 119;
+    i32 malus_quiet_v2: 59;
+    i32 malus_quiet_v3: 1180;
+    i32 malus_quiet_v4: 18;
 
-    i32 bonus_cont_v1: 128;
-    i32 bonus_cont_v2: 64;
-    i32 bonus_cont_v3: 1280;
+    i32 bonus_cont_v1: 139;
+    i32 bonus_cont_v2: 61;
+    i32 bonus_cont_v3: 1433;
 
-    i32 malus_cont_v1: 128;
-    i32 malus_cont_v2: 64;
-    i32 malus_cont_v3: 1280;
+    i32 malus_cont_v1: 171;
+    i32 malus_cont_v2: 67;
+    i32 malus_cont_v3: 1047;
     i32 malus_cont_v4: 16;
 
     i32 pcm_v1: 5;
-    i32 pcm_v2: 128;
-    i32 pcm_v3: 128;
-    i32 pcm_v4: 128;
-    i32 pcm_v5: 64;
-    i32 pcm_v6: 1280;
+    i32 pcm_v2: 130;
+    i32 pcm_v3: 120;
+    i32 pcm_v4: 146;
+    i32 pcm_v5: 54;
+    i32 pcm_v6: 1353;
 
-    i32 qs_v1: 128;
+    i32 qs_v1: 123;
 
-    i32 corrhist_v1: 1024;
-    i32 corrhist_v2: 1024;
-    i32 corrhist_v3: 1024;
-    i32 corrhist_v4: 1024;
-    i32 corrhist_v5: 1024;
+    i32 corrhist_v1: 1181;
+    i32 corrhist_v2: 975;
+    i32 corrhist_v3: 898;
+    i32 corrhist_v4: 972;
+    i32 corrhist_v5: 1083;
 
-    i32 conthist_v1: 1024;
-    i32 conthist_v2: 1024;
-    i32 conthist_v3: 1024;
+    i32 conthist_v1: 1164;
+    i32 conthist_v2: 1175;
+    i32 conthist_v3: 1035;
 
-    i32 mp_v1: 32;
-    i32 mp_v2: 100;
-    i32 mp_v3: 16;
-    i32 mp_v4: 1024;
-    i32 mp_v5: 1024;
-    i32 mp_v6: 1024;
-    i32 mp_v7: 512;
+    i32 mp_v1: 34;
+    i32 mp_v2: 107;
+    i32 mp_v3: 18;
+    i32 mp_v4: 1247;
+    i32 mp_v5: 1011;
+    i32 mp_v6: 978;
+    i32 mp_v7: 517;
 }
