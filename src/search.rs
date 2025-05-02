c0757ef4f9ae9629a570c8cd89415e426a4484a1
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-02 05:58:01 +0300

    Bench: 7776751

--- src/search.rs
+++ src/search.rs
@@ -261,6 +261,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     if !in_check
+        && !excluded
         && td.ply >= 1
         && td.stack[td.ply - 1].reduction >= 2761
         && static_eval + td.stack[td.ply - 1].static_eval < 0
@@ -269,6 +270,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     if !in_check
+        && !excluded
         && depth >= 2
         && td.ply >= 1
         && td.stack[td.ply - 1].reduction >= 1053
