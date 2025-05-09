5decd7fb1deb8309213b455c070218369f82ec77
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-09 21:37:49 +0300

    Bench: 8153057

--- src/tb.rs
+++ src/tb.rs
@@ -1,18 +1,16 @@
 use std::ffi::CString;
 
 use crate::{
-    bindings::{tb_init, tb_probe_wdl, TB_BLESSED_LOSS, TB_CURSED_WIN, TB_DRAW, TB_LARGEST, TB_LOSS, TB_WIN},
+    bindings::{tb_init, tb_probe_wdl, TB_DRAW, TB_LARGEST, TB_LOSS, TB_WIN},
     board::Board,
     types::{Color, PieceType},
 };
 
-pub enum TbResult {
+#[derive(PartialEq)]
+pub enum GameOutcome {
     Win,
     Loss,
     Draw,
-    BlessedLoss,
-    CursedWin,
-    Failed,
 }
 
 pub fn tb_initilize(path: &str) -> Option<usize> {
@@ -30,7 +28,7 @@ pub fn tb_size() -> usize {
     unsafe { TB_LARGEST as usize }
 }
 
-pub fn tb_probe(board: &Board) -> TbResult {
+pub fn tb_probe(board: &Board) -> Option<GameOutcome> {
     let code = unsafe {
         tb_probe_wdl(
             board.colors(Color::White).0,
@@ -49,11 +47,9 @@ pub fn tb_probe(board: &Board) -> TbResult {
     };
 
     match code {
-        TB_WIN => TbResult::Win,
-        TB_LOSS => TbResult::Loss,
-        TB_DRAW => TbResult::Draw,
-        TB_BLESSED_LOSS => TbResult::BlessedLoss,
-        TB_CURSED_WIN => TbResult::CursedWin,
-        _ => TbResult::Failed,
+        TB_WIN => Some(GameOutcome::Win),
+        TB_LOSS => Some(GameOutcome::Loss),
+        TB_DRAW => Some(GameOutcome::Draw),
+        _ => None,
     }
 }
