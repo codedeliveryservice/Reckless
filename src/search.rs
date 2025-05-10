606c880fd516be7d65ad3dc94c436ae314c6cee9
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-10 19:01:57 +0300

    Bench: 9474868

--- src/search.rs
+++ src/search.rs
@@ -203,7 +203,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
 
         if !PV
             && !excluded
-            && entry.depth >= depth
+            && entry.depth > depth - (entry.score <= beta) as i32
             && match entry.bound {
                 Bound::Upper => entry.score <= alpha,
                 Bound::Lower => entry.score >= beta,
