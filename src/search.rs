b692809749358b170e2d46dbeb7d3c06e28d684d
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-10 13:30:22 +0300

    Bench: 8148971

--- src/search.rs
+++ src/search.rs
@@ -812,11 +812,11 @@ fn qsearch<const PV: bool>(td: &mut ThreadData, mut alpha: i32, beta: i32) -> i3
 
         move_count += 1;
 
-        if !is_loss(best_score) && mv.to() != previous_square {
-            if move_picker.stage() == Stage::BadNoisy {
-                break;
-            }
+        if !is_loss(best_score) && move_picker.stage() == Stage::BadNoisy {
+            break;
+        }
 
+        if !is_loss(best_score) && mv.to() != previous_square {
             if move_count >= 3 {
                 break;
             }
