129cf27d4d34563b223bf944a77f5da77b2473d8
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-12 04:38:23 +0300

    Bench: 7461808

--- src/search.rs
+++ src/search.rs
@@ -714,7 +714,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     if !excluded {
-        td.tt.write(td.board.hash(), depth, raw_eval, best_score, bound, best_move, td.ply, tt_pv);
+        td.tt.write(td.board.hash(), initial_depth, raw_eval, best_score, bound, best_move, td.ply, tt_pv);
     }
 
     if !(in_check
