bb542deff04174326398d5f61c175d6e25697878
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-12 23:57:28 +0300

    Bench: 6551654

--- src/search.rs
+++ src/search.rs
@@ -309,7 +309,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && td.stack[td.ply - 1].mv.is_quiet()
         && td.stack[td.ply - 1].static_eval != Score::NONE
     {
-        let value = 6 * -(static_eval + td.stack[td.ply - 1].static_eval);
+        let value = 2 * depth / 3 * -(static_eval + td.stack[td.ply - 1].static_eval);
         let bonus = value.clamp(-67, 160);
 
         td.quiet_history.update(td.board.prior_threats(), !td.board.side_to_move(), td.stack[td.ply - 1].mv, bonus);
