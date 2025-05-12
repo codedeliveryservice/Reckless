ec080bf277bccf18de7474149a34017fe19cfd25
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-12 22:16:34 +0300

    Bench: 6460594

--- src/movepick.rs
+++ src/movepick.rs
@@ -185,7 +185,7 @@ impl MovePicker {
         for entry in self.list.iter_mut() {
             let mv = entry.mv;
 
-            entry.score += 1247 * td.quiet_history.get(td.board.threats(), td.board.side_to_move(), mv) / 1024
+            entry.score += 2048 * td.quiet_history.get(td.board.threats(), td.board.side_to_move(), mv) / 1024
                 + 1011 * td.conthist(1, mv) / 1024
                 + 978 * td.conthist(2, mv) / 1024
                 + 517 * td.conthist(3, mv) / 1024;
