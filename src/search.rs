21a0325b935c26cf1ab09e32fb904f233a06b792
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-05-12 18:16:00 +0300

    Bench: 6560187

--- src/search.rs
+++ src/search.rs
@@ -496,7 +496,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         let mut reduction = td.lmr.reduction(depth, move_count);
 
         if !is_root && !is_loss(best_score) {
-            let lmr_depth = (depth - reduction / 1024 + is_quiet as i32 * history / 7777).max(0);
+            let lmr_depth = (depth - reduction / 1024 + is_quiet as i32 * (history / 7777 + PV as i32)).max(0);
 
             // Late Move Pruning (LMP)
             skip_quiets |= move_count >= lmp_threshold(depth, improving);
