708926f6a3dff5b0aa66368148492675f513a385
styxdoto <164851643+styxdoto@users.noreply.github.com>
2025-05-20 09:22:15 -0700

    Update default network to v21-78a5970b.nnue (#334)
    STC:
    Elo | 6.82 +- 4.01 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.92 (-2.25, 2.89) [0.00, 4.00]
    Games | N: 8102 W: 2137 L: 1978 D: 3987
    Penta | [33, 911, 2020, 1038, 49]
    https://recklesschess.space/test/4973/
    
    LTC:
    Elo | 6.61 +- 3.84 (95%)
    SPRT | 40.0+0.40s Threads=1 Hash=64MB
    LLR | 2.90 (-2.25, 2.89) [0.00, 4.00]
    Games | N: 7728 W: 1864 L: 1717 D: 4147
    Penta | [10, 836, 2027, 979, 12]
    https://recklesschess.space/test/4975/
    
    Bench: 3072430

--- build/build.rs
+++ build/build.rs
@@ -11,7 +11,7 @@ mod magics;
 mod maps;
 
 const BASE_URL: &str = "https://github.com/codedeliveryservice/RecklessNetworks/raw/main";
-const NETWORK_NAME: &str = "v20-0e6ecbd4.nnue";
+const NETWORK_NAME: &str = "v21-78a5970b.nnue";
 
 fn main() {
     generate_model_env();
