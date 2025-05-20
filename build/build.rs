671a4c3d0e8b71c3cd68d3138a436aa49edcc556
styxdoto <164851643+styxdoto@users.noreply.github.com>
2025-05-20 09:25:00 -0700

    Update default network to v22-abc72242.nnue (#335)
    Nodes:
    Elo   | 2.18 +- 1.64 (95%)
    SPRT  | N=25000 Threads=1 Hash=8MB
    LLR   | 3.01 (-2.94, 2.94) [0.00, 4.00]
    Games | N: 90740 W: 30729 L: 30160 D: 29851
    Penta | [3383, 9908, 18434, 10047, 3598]
    https://recklesschess.space/test/4981/
    
    STC:
    Elo | 3.50 +- 2.60 (95%)
    SPRT | 8.0+0.08s Threads=1 Hash=16MB
    LLR | 2.94 (-2.25, 2.89) [0.00, 4.00]
    Games | N: 19334 W: 4976 L: 4781 D: 9577
    Penta | [98, 2244, 4786, 2443, 96]
    https://recklesschess.space/test/4982/
    
    Bench: 2863183

--- build/build.rs
+++ build/build.rs
@@ -11,7 +11,7 @@ mod magics;
 mod maps;
 
 const BASE_URL: &str = "https://github.com/codedeliveryservice/RecklessNetworks/raw/main";
-const NETWORK_NAME: &str = "v21-78a5970b.nnue";
+const NETWORK_NAME: &str = "v22-abc72242.nnue";
 
 fn main() {
     generate_model_env();
