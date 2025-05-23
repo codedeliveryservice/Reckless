9937f52b2e1d837b08fd1b77c0e904b1d029577a
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-05-24 00:46:52 +0300

    bench: 2424627

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -4,13 +4,14 @@ use crate::{
     types::{PieceType, Score},
 };
 
-const MATERIAL_VALUES: [i32; 6] = [132, 414, 432, 661, 1217, 0];
+const MATERIAL_VALUES: [i32; 6] = [232, 414, 432, 661, 1217, 0];
 
 /// Calculates the score of the current position from the perspective of the side to move.
 pub fn evaluate(td: &mut ThreadData) -> i32 {
     let mut eval = td.nnue.evaluate(&td.board);
 
     let material = material(&td.board);
+
     eval = (eval * (20000 + material) + td.optimism[td.board.side_to_move()] * (2000 + material)) / 32768;
 
     eval.clamp(-Score::TB_WIN_IN_MAX + 1, Score::TB_WIN_IN_MAX - 1)
