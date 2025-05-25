c49a6bca663e67caf3c9922e1baaffa6a54f387a
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-05-25 14:04:38 +0300

    bench: 2419001

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -11,7 +11,7 @@ pub fn evaluate(td: &mut ThreadData) -> i32 {
     let mut eval = td.nnue.evaluate(&td.board);
 
     let material = material(&td.board);
-    eval = (eval * (20000 + material) + td.optimism[td.board.side_to_move()] * (2000 + material)) / 32768;
+    eval = (eval * (77777 + material) + td.optimism[td.board.side_to_move()] * (7777 + material)) / 77777;
 
     eval.clamp(-Score::TB_WIN_IN_MAX + 1, Score::TB_WIN_IN_MAX - 1)
 }
