7da41c42c29984442957e6ce51211b1dbcab298d
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-05-25 15:18:07 +0300

    bench: 2155693

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -9,9 +9,12 @@ const MATERIAL_VALUES: [i32; 6] = [132, 414, 432, 661, 1217, 0];
 /// Calculates the score of the current position from the perspective of the side to move.
 pub fn evaluate(td: &mut ThreadData) -> i32 {
     let mut eval = td.nnue.evaluate(&td.board);
+    let mut optimism = td.optimism[td.board.side_to_move()];
 
     let material = material(&td.board);
-    eval = (eval * (20000 + material) + td.optimism[td.board.side_to_move()] * (2000 + material)) / 32768;
+    let complexity = eval.abs() / 4;
+    optimism += optimism * complexity / 468;
+    eval = (eval * (20000 + material) + optimism * (2000 + material)) / 32768;
 
     eval.clamp(-Score::TB_WIN_IN_MAX + 1, Score::TB_WIN_IN_MAX - 1)
 }
