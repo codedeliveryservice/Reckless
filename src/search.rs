a0724d75f16634c460ac8074ca21906134a75a66
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-05-29 01:23:53 +0300

    bench: 2301900

--- src/search.rs
+++ src/search.rs
@@ -786,7 +786,7 @@ fn search<const PV: bool>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         }
     }
 
-    if bound == Bound::Upper && td.ply >= 1 && (!quiet_moves.is_empty() || depth > 3) {
+    if bound != Bound::Lower && td.ply >= 1 && (!quiet_moves.is_empty() || depth > 3) {
         tt_pv |= td.stack[td.ply - 1].tt_pv;
 
         let pcm_move = td.stack[td.ply - 1].mv;
