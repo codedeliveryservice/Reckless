fb82d89716094c236af354535368486ddbcc2712
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-05-30 19:22:03 +0300

    bench: 2548310

--- src/search.rs
+++ src/search.rs
@@ -558,7 +558,8 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
             let lmr_depth = (depth - reduction / 1024 + is_quiet as i32 * history / 7084).max(0);
 
             // Late Move Pruning (LMP)
-            skip_quiets |= move_count >= (4 + depth * depth) / (2 - (improving || static_eval >= beta) as i32);
+            skip_quiets |=
+                move_count >= (4 + depth * depth) / (2 - (!tt_pv && (improving || static_eval >= beta)) as i32);
 
             // Futility Pruning (FP)
             let futility_value = static_eval + 120 * lmr_depth + 80;
