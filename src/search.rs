52b09ad89b3475be8f5bb20fa5d4cc04900e7184
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-05-31 14:56:59 +0300

    bench: 2094831

--- src/search.rs
+++ src/search.rs
@@ -428,6 +428,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && static_eval >= beta - 15 * depth + 153 * tt_pv as i32 + 190
         && td.ply as i32 >= td.nmp_min_ply
         && td.board.has_non_pawns()
+        && tt_depth < depth
     {
         let r = 4 + depth / 3 + ((eval - beta) / 252).min(3) + (tt_move.is_null() || tt_move.is_noisy()) as i32;
 
