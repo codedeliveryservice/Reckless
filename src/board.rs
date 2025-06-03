3cb6cb3259503d215c704760bad4020451c58356
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-06-03 16:35:00 +0300

    bench: 2223226

--- src/board.rs
+++ src/board.rs
@@ -200,7 +200,7 @@ impl Board {
     pub fn update_hash(&mut self, piece: Piece, square: Square) {
         self.state.key ^= ZOBRIST.pieces[piece][square];
 
-        if piece.piece_type() == PieceType::Pawn {
+        if piece.piece_type() == PieceType::Pawn || piece.piece_type() == PieceType::King {
             self.state.pawn_key ^= ZOBRIST.pieces[piece][square];
         } else {
             self.state.non_pawn_keys[piece.piece_color()] ^= ZOBRIST.pieces[piece][square];
