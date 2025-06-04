25a2bff0ded70249b9d16221bdc11ffb960fa9c0
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-04 16:05:15 +0300

    Bench: 2086753

--- src/search.rs
+++ src/search.rs
@@ -845,7 +845,8 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     if !(in_check
         || best_move.is_noisy()
         || (bound == Bound::Upper && best_score >= static_eval)
-        || (bound == Bound::Lower && best_score <= static_eval))
+        || (bound == Bound::Lower && best_score <= static_eval)
+        || (best_score - static_eval).abs() > 512)
     {
         update_correction_histories(td, depth, best_score - static_eval);
     }
