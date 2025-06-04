e8868ba594f6e0664a96996dae3ad207ae4d9eef
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-04 03:31:19 +0300

    Bench: 2280564

--- src/search.rs
+++ src/search.rs
@@ -487,15 +487,18 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         let mut move_picker = MovePicker::new_probcut(probcut_beta - static_eval);
 
         let probcut_depth = 0.max(depth - 4);
+        let mut move_count = 0;
 
         while let Some(mv) = move_picker.next(td, true) {
-            if move_picker.stage() == Stage::BadNoisy {
+            if move_picker.stage() == Stage::BadNoisy || move_count >= 2 {
                 break;
             }
 
             if mv == td.stack[td.ply].excluded || !td.board.is_legal(mv) {
                 continue;
             }
+
+            move_count += 1;
 
             make_move(td, mv);
 
