fd3ceb47edfc966f1cfade5d2e881ef375c3e985
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-06 06:49:10 +0300

    Bench: 2321154

--- src/search.rs
+++ src/search.rs
@@ -662,7 +662,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 reduction += 1141;
             }
 
-            if td.board.in_check() {
+            if in_check || td.board.in_check() {
                 reduction -= 820;
             }
 
