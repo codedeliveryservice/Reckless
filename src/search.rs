229ec59b40fbd3e5edc9d193104701ccadc9f714
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-06 08:51:53 +0300

    Bench: 2305149

--- src/search.rs
+++ src/search.rs
@@ -634,6 +634,14 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         }
 
         let initial_nodes = td.counter.local();
+        let history = if is_quiet {
+            td.quiet_history.get(td.board.threats(), td.board.side_to_move(), mv)
+                + td.conthist(1, mv)
+                + td.conthist(2, mv)
+        } else {
+            let captured = td.board.piece_on(mv.to()).piece_type();
+            td.noisy_history.get(td.board.threats(), td.board.moved_piece(mv), mv.to(), captured)
+        };
 
         make_move(td, mv);
 
