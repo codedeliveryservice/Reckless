4ef8702dbb290fa9487edd38c69db31991fb2dfe
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-06-08 19:21:34 +0300

    bench: 2099441

--- src/search.rs
+++ src/search.rs
@@ -800,10 +800,9 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         } else {
             td.stack[td.ply].killer = best_move;
 
+            td.quiet_history.update(td.board.threats(), td.board.side_to_move(), best_move, bonus_quiet);
+            update_continuation_histories(td, td.board.moved_piece(best_move), best_move.to(), bonus_cont);
             if !quiet_moves.is_empty() || depth > 3 {
-                td.quiet_history.update(td.board.threats(), td.board.side_to_move(), best_move, bonus_quiet);
-                update_continuation_histories(td, td.board.moved_piece(best_move), best_move.to(), bonus_cont);
-
                 for &mv in quiet_moves.iter() {
                     td.quiet_history.update(td.board.threats(), td.board.side_to_move(), mv, -malus_quiet);
                     update_continuation_histories(td, td.board.moved_piece(mv), mv.to(), -malus_cont);
