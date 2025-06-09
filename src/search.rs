4b8c49a3b5ff87e9ba17f9d3b720eac5068cb4ed
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-09 08:06:16 +0300

    Bench: 2324969

--- src/search.rs
+++ src/search.rs
@@ -1034,8 +1034,9 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32) -> i3
     }
 
     let bound = if best_score >= beta { Bound::Lower } else { Bound::Upper };
+    let depth = if in_check { 1 } else { TtDepth::SOME };
 
-    td.tt.write(td.board.hash(), TtDepth::SOME, raw_eval, best_score, bound, best_move, td.ply, tt_pv);
+    td.tt.write(td.board.hash(), depth, raw_eval, best_score, bound, best_move, td.ply, tt_pv);
 
     debug_assert!(-Score::INFINITE < best_score && best_score < Score::INFINITE);
 
