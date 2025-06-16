15c13679883d347a4b0073116303c3910c8d9fed
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-16 08:09:34 +0300

    Bench: 2327974

--- src/search.rs
+++ src/search.rs
@@ -134,6 +134,10 @@ pub fn start(td: &mut ThreadData, report: Report) -> SearchResult {
         best_move_changes += td.best_move_changes;
 
         let multiplier = || {
+            if depth < 8 {
+                return 1.0;
+            }
+
             let node_factor = 2.1 - 1.5 * (td.node_table.get(td.pv.best_move()) as f32 / td.counter.local() as f32);
 
             let score_factor = (800 + 20 * (td.previous_best_score - td.best_score)).clamp(750, 1500) as f32 / 1000.0;
