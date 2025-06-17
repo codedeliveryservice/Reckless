c2f819c7e47230be27aa8a900899374d9d7845c5
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-17 17:35:38 +0300

    Bench: 2282423

--- src/history.rs
+++ src/history.rs
@@ -14,8 +14,8 @@ struct QuietHistoryEntry {
 }
 
 impl QuietHistoryEntry {
-    const MAX_FACTORIZER: i32 = 2048;
-    const MAX_BUCKET: i32 = 6144;
+    const MAX_FACTORIZER: i32 = 3072;
+    const MAX_BUCKET: i32 = 7168;
 
     pub fn bucket(&self, threats: Bitboard, mv: Move) -> i16 {
         let from_threated = threats.contains(mv.from()) as usize;
