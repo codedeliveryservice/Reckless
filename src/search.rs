ac39ee9a4eb4d07f9de4127ddb9caa8b03205068
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-19 13:03:20 +0300

    Use existing variable
    Bench: 2333899
    
    No functional change

--- src/search.rs
+++ src/search.rs
@@ -1084,19 +1084,11 @@ fn update_correction_histories(td: &mut ThreadData, depth: i32, diff: i32) {
     td.non_pawn_corrhist[Color::Black].update(stm, td.board.non_pawn_key(Color::Black), 1056 * bonus / 1024);
 
     if td.ply >= 1 && td.stack[td.ply - 1].mv.is_some() {
-        td.prior_moves_corrhist[0].update(
-            td.board.side_to_move(),
-            td.stack[td.ply - 1].mv.encoded() as u64,
-            1039 * bonus / 1024,
-        );
+        td.prior_moves_corrhist[0].update(stm, td.stack[td.ply - 1].mv.encoded() as u64, 1039 * bonus / 1024);
     }
 
     if td.ply >= 2 && td.stack[td.ply - 2].mv.is_some() {
-        td.prior_moves_corrhist[1].update(
-            td.board.side_to_move(),
-            td.stack[td.ply - 2].mv.encoded() as u64,
-            986 * bonus / 1024,
-        );
+        td.prior_moves_corrhist[1].update(stm, td.stack[td.ply - 2].mv.encoded() as u64, 986 * bonus / 1024);
     }
 }
 
