719921fb1598dfd343f09ba05d276b1518c53c9a
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-20 20:24:09 +0300

    Bench: 2881098

--- src/search.rs
+++ src/search.rs
@@ -690,7 +690,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                     if mv.is_quiet() {
                         let bonus = match score {
                             s if s >= beta => {
-                                (1 + 2 * (move_count > depth) as i32 + 2 * (move_count > 2 * depth) as i32)
+                                (1 + 2 * (move_count > new_depth) as i32 + 2 * (move_count > 2 * new_depth) as i32)
                                     * (152 * depth - 50).min(973)
                             }
                             s if s <= alpha => -(139 * depth - 63).min(1166),
