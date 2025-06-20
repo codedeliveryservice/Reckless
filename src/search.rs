d5ef3f586fd8d5ac3593aaebdef50c74b857b713
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-20 20:40:54 +0300

    Bench: 2473800

--- src/search.rs
+++ src/search.rs
@@ -774,13 +774,15 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     if best_move.is_some() {
-        let bonus_noisy = (124 * depth - 65).min(1177);
+        let extra = 128 * (static_eval < alpha) as i32;
+
+        let bonus_noisy = (124 * depth - 65 + extra).min(1177);
         let malus_noisy = (145 * initial_depth - 75).min(1403) - 14 * (move_count - 1);
 
-        let bonus_quiet = (148 * depth - 71).min(1458);
+        let bonus_quiet = (148 * depth - 71 + extra).min(1458);
         let malus_quiet = (125 * initial_depth - 52).min(1263) - 17 * (move_count - 1);
 
-        let bonus_cont = (114 * depth - 53).min(1318);
+        let bonus_cont = (114 * depth - 53 + extra).min(1318);
         let malus_cont = (244 * initial_depth - 51).min(907) - 15 * (move_count - 1);
 
         if best_move.is_noisy() {
