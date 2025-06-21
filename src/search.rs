cfac249caa4b791aa116d80464921ecc0101fd88
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-21 17:21:29 +0300

    Bench: 1918282

--- src/search.rs
+++ src/search.rs
@@ -737,7 +737,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 }
 
                 if depth > 2 && depth < 15 && !is_decisive(score) {
-                    depth -= 1;
+                    depth -= 1 + (static_eval < score) as i32;
                 }
             }
         }
