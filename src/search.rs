d32d542997da574df12895a5ea08924dc8b25efb
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-22 17:29:13 +0300

    Bench: 2517681

--- src/search.rs
+++ src/search.rs
@@ -809,7 +809,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
 
             td.quiet_history.update(td.board.prior_threats(), !td.board.side_to_move(), pcm_move, scaled_bonus);
 
-            let conthist_bonus = (137 * depth - 43).min(1435);
+            let conthist_bonus = (102 + 141 * (depth > 5) as i32) * (137 * depth - 43).min(1435) / 128;
 
             td.ply -= 1;
             update_continuation_histories(td, td.stack[td.ply].piece, pcm_move.to(), conthist_bonus);
