18d71975692b5745cc09db230974a2c9694278b7
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-23 23:50:31 +0300

    Bench: 2145678

--- src/time.rs
+++ src/time.rs
@@ -33,8 +33,8 @@ impl TimeManager {
             }
             Limits::Fischer(main, inc) => {
                 let main = main.saturating_sub(move_overhead);
-                let soft_scale = 0.025 + 0.05 * (1.0 - (-0.034 * fullmove_number as f64).exp());
-                let hard_scale = 0.135 + 0.21 * (1.0 - (-0.030 * fullmove_number as f64).exp());
+                let soft_scale = 0.025 + 0.035 * (1.0 - (-0.034 * fullmove_number as f64).exp());
+                let hard_scale = 0.135 + 0.195 * (1.0 - (-0.030 * fullmove_number as f64).exp());
 
                 soft = (soft_scale * main as f64 + 0.75 * inc as f64) as u64;
                 hard = (hard_scale * main as f64 + 0.75 * inc as f64) as u64;
