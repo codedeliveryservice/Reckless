bbbc99cd178b671721f01819ef7897c9e9a0fc38
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-24 15:07:43 +0300

    Bench: 2169515

--- src/search.rs
+++ src/search.rs
@@ -545,7 +545,13 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
             let lmr_depth = (depth - reduction / 1024 + is_quiet as i32 * history / 7657).max(0);
 
             // Late Move Pruning (LMP)
-            skip_quiets |= move_count >= (4 + depth * depth) / (2 - (improving || static_eval >= beta + 18) as i32);
+            if move_count >= (4 + depth * depth) / (2 - (improving || static_eval >= beta + 18) as i32) {
+                skip_quiets = true;
+
+                if is_quiet {
+                    continue;
+                }
+            }
 
             // Futility Pruning (FP)
             let futility_value = static_eval + 122 * lmr_depth + 78;
