dd180d472be19b0e296214a684570db9db7e4ad2
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-28 18:03:06 +0300

    Bench: 2245685

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -5,14 +5,20 @@ use crate::{
 };
 
 const MATERIAL_VALUES: [i32; 6] = [132, 414, 432, 661, 1217, 0];
+const MATERIAL_BASE: [i32; 8] = [22324, 22185, 20670, 20807, 20825, 22232, 23045, 22593];
+const MATERIAL_MULT: [i32; 8] = [1952, 1896, 2003, 1814, 1878, 1964, 1892, 1980];
+const MATERIAL_DIV: [i32; 8] = [28529, 30004, 29633, 29126, 28955, 28400, 29051, 28837];
 
 /// Calculates the score of the current position from the perspective of the side to move.
 pub fn evaluate(td: &mut ThreadData) -> i32 {
     let mut eval = td.nnue.evaluate(&td.board);
 
+    let bucket = (td.board.occupancies().len() - 2) / 4;
     let material = material(&td.board);
 
-    eval = (eval * (21682 + material) + td.optimism[td.board.side_to_move()] * (1923 + material)) / 28993;
+    eval = (eval * (MATERIAL_BASE[bucket] + material)
+        + td.optimism[td.board.side_to_move()] * (MATERIAL_MULT[bucket] + material))
+        / MATERIAL_DIV[bucket];
 
     eval = (eval / 16) * 16 - 1 + (td.board.hash() & 0x2) as i32;
 
