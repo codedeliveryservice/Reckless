ac953079c370006e07c929cfeda34db52f03ae78
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-29 15:56:38 +0300

    Bench: 2051855

--- src/search.rs
+++ src/search.rs
@@ -505,7 +505,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     // Internal Iterative Reductions (IIR)
-    if depth >= 3 + 3 * cut_node as i32 && tt_move.is_null() && (NODE::PV || cut_node) {
+    if !excluded && depth >= 3 + 3 * cut_node as i32 && tt_move.is_null() && (NODE::PV || cut_node) {
         depth -= 1;
     }
 
