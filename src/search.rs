2324695a788001b95e20d00224aeeff6bf026d60
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-30 23:58:57 +0300

    Bench: 1902125

--- src/search.rs
+++ src/search.rs
@@ -397,10 +397,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && !excluded
         && depth <= 7
         && eval >= beta
-        && eval
-            >= beta + 80 * depth - (72 * improving as i32) - (25 * cut_node as i32)
-                + 556 * correction_value.abs() / 1024
-                + 24
+        && eval >= beta + 80 * depth - (improvement / 10) - (25 * cut_node as i32) + 556 * correction_value.abs() / 1024
         && !is_loss(beta)
         && !is_win(eval)
     {
