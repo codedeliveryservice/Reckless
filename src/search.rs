4a36974b1f72a6fb0ad1c5595fdf0be960454b62
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-06-30 17:00:17 +0300

    bench: 1915411

--- src/search.rs
+++ src/search.rs
@@ -464,7 +464,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     // ProbCut
     let probcut_beta = beta + 280 - 63 * improving as i32;
 
-    if depth >= 3 && !is_decisive(beta) && (!is_valid(tt_score) || tt_score >= probcut_beta) {
+    if depth >= 3 && !is_decisive(beta) && (!is_valid(tt_score) || tt_score >= probcut_beta) && !potential_singularity {
         let mut move_picker = MovePicker::new_probcut(probcut_beta - static_eval);
 
         let probcut_depth = 0.max(depth - 4);
