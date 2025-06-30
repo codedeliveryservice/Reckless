273f58fc836ecd620c644d1d0898e4a8e32a9f22
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-06-30 04:19:00 +0300

    Bench: 1939589

--- src/search.rs
+++ src/search.rs
@@ -466,7 +466,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     // ProbCut
-    let probcut_beta = beta + 280 - 63 * improving as i32;
+    let probcut_beta = beta + 280 - improvement / 10;
 
     if depth >= 3 && !is_decisive(beta) && (!is_valid(tt_score) || tt_score >= probcut_beta) {
         let mut move_picker = MovePicker::new_probcut(probcut_beta - static_eval);
