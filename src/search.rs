c8c284c4a5bb6a828ca23d428c1e0613eb476216
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-01 23:15:12 +0300

    Bench: 2094749

--- src/search.rs
+++ src/search.rs
@@ -400,7 +400,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && eval
             >= beta + 80 * depth - (72 * improving as i32) - (25 * cut_node as i32)
                 + 556 * correction_value.abs() / 1024
-                + 24
+                + td.board.threats().len() as i32
         && !is_loss(beta)
         && !is_win(eval)
     {
