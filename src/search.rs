7f6b27b9bcba94148ec3b3e63381f34ce0503d7a
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-01 23:41:48 +0300

    Bench: 1814087

--- src/search.rs
+++ src/search.rs
@@ -660,6 +660,10 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 reduction += 1196;
             }
 
+            if td.ply >= 2 && move_count >= 5 {
+                reduction -= td.stack[td.ply - 2].reduction / 5;
+            }
+
             let reduced_depth =
                 (new_depth - reduction / 1024).clamp(NODE::PV as i32, new_depth + (NODE::PV || cut_node) as i32);
 
