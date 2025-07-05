0b380f9cb78807a715f3e81f37c61899bea769e6
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-05 14:41:45 +0300

    Bench: 1731968

--- src/search.rs
+++ src/search.rs
@@ -401,6 +401,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
             >= beta + 80 * depth - (72 * improving as i32) - (25 * cut_node as i32)
                 + 556 * correction_value.abs() / 1024
                 + 24
+        && (!is_valid(tt_score) || tt_bound == Bound::Lower)
         && !is_loss(beta)
         && !is_win(eval)
     {
