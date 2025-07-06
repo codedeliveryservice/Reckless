641f410de7e3d98093091c58c367fb0d60c9f799
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-06 07:22:23 +0300

    Bench: 2124937

--- src/search.rs
+++ src/search.rs
@@ -387,7 +387,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     let improving = improvement > 0;
 
     // Razoring
-    if !NODE::PV && !in_check && eval < alpha - 303 - 260 * depth * depth {
+    if !NODE::PV && !in_check && eval < alpha - 303 - 260 * depth * depth - 64 * improving as i32 {
         return qsearch::<NonPV>(td, alpha, beta);
     }
 
