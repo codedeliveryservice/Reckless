f1ec54ae4fd05641702c26d00da923cb3820d20d
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-06 03:48:15 +0300

    Bench: 1735277

--- src/search.rs
+++ src/search.rs
@@ -413,7 +413,10 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && !excluded
         && eval >= beta
         && eval >= static_eval
-        && static_eval >= beta - 15 * depth + 159 * tt_pv as i32 - improvement / 10 + 185
+        && static_eval
+            >= beta - 15 * depth + 159 * tt_pv as i32 - improvement / 10
+                + 185
+                + (td.stack[td.ply - 1].history / 200).clamp(-64, 64)
         && td.ply as i32 >= td.nmp_min_ply
         && td.board.has_non_pawns()
         && !potential_singularity
@@ -425,6 +428,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         td.stack[td.ply].contcorrhist = std::ptr::null_mut();
         td.stack[td.ply].piece = Piece::None;
         td.stack[td.ply].mv = Move::NULL;
+        td.stack[td.ply].history = 0;
         td.ply += 1;
 
         td.board.make_null_move();
@@ -478,7 +482,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 continue;
             }
 
-            make_move(td, mv);
+            make_move(td, mv, 0);
 
             let mut score = -qsearch::<NonPV>(td, -probcut_beta, -probcut_beta + 1);
 
@@ -625,7 +629,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
 
         let initial_nodes = td.counter.local();
 
-        make_move(td, mv);
+        make_move(td, mv, history);
 
         let mut new_depth = depth + extension - 1;
         let mut score = Score::ZERO;
@@ -977,7 +981,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32) -> i3
             }
         }
 
-        make_move(td, mv);
+        make_move(td, mv, 0);
 
         let score = -qsearch::<NODE>(td, -beta, -alpha);
 
@@ -1081,11 +1085,12 @@ fn update_continuation_histories(td: &mut ThreadData, piece: Piece, sq: Square, 
     }
 }
 
-fn make_move(td: &mut ThreadData, mv: Move) {
+fn make_move(td: &mut ThreadData, mv: Move, history: i32) {
     td.stack[td.ply].conthist = td.continuation_history.subtable_ptr(td.board.moved_piece(mv), mv.to());
     td.stack[td.ply].contcorrhist = td.continuation_corrhist.subtable_ptr(td.board.moved_piece(mv), mv.to());
     td.stack[td.ply].piece = td.board.moved_piece(mv);
     td.stack[td.ply].mv = mv;
+    td.stack[td.ply].history = history;
     td.ply += 1;
 
     td.counter.increment();
