9cb25cb97628b185e78f118349eb0019a675fbbb
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-06 12:15:33 +0300

    Bench: 1796502

--- src/search.rs
+++ src/search.rs
@@ -347,11 +347,9 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && !excluded
         && td.stack[td.ply - 1].mv.is_quiet()
         && is_valid(td.stack[td.ply - 1].static_eval)
+        && static_eval + td.stack[td.ply - 1].static_eval > 0
     {
-        let value = 674 * (-(static_eval + td.stack[td.ply - 1].static_eval)) / 128;
-        let bonus = value.clamp(-61, 144);
-
-        td.quiet_history.update(td.board.prior_threats(), !td.board.side_to_move(), td.stack[td.ply - 1].mv, bonus);
+        td.quiet_history.update(td.board.prior_threats(), !td.board.side_to_move(), td.stack[td.ply - 1].mv, -60);
     }
 
     // Hindsight LMR
