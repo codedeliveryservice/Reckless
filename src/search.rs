8d7d2074cb57c92c01fef8b02261e4e32a0f0527
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-08 12:36:04 +0300

    Bench: 1859427

--- src/search.rs
+++ src/search.rs
@@ -546,7 +546,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         }
 
         if !NODE::ROOT && !is_loss(best_score) {
-            let lmr_reduction = if is_quiet { reduction - 137 * history / 1024 } else { reduction } + 384;
+            let lmr_reduction = if is_quiet { reduction - 137 * history / 1024 } else { reduction } - 384;
             let lmr_depth = (depth - lmr_reduction / 1024).max(0);
 
             // Late Move Pruning (LMP)
