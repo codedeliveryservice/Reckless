366ecdf2cd5dbcc70f3f0fc2be51424415ded50b
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-10 15:34:29 +0300

    Bench: 1879675

--- src/search.rs
+++ src/search.rs
@@ -358,6 +358,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     if !NODE::ROOT
         && !in_check
         && !excluded
+        && td.stack[td.ply - 1].mv.is_quiet()
         && td.stack[td.ply - 1].reduction >= 2691
         && static_eval + td.stack[td.ply - 1].static_eval < 0
     {
