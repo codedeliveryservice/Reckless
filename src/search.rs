0beed554f6ea2b01f6debcf32ad230be862dba48
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-07-11 04:36:42 +0300

    bench: 1754916

--- src/search.rs
+++ src/search.rs
@@ -832,6 +832,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     if !(in_check
+        || depth <= 3
         || had_best_noisy_move
         || (bound == Bound::Upper && best_score >= static_eval)
         || (bound == Bound::Lower && best_score <= static_eval))
