a2805d2b8f2a1691129450d2aac573b4317d7d92
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-07-11 05:12:30 +0300

    Bench: 1729772

--- src/search.rs
+++ src/search.rs
@@ -642,7 +642,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         let mut score = Score::ZERO;
 
         // Late Move Reductions (LMR)
-        if depth >= 3 && move_count > 1 + NODE::ROOT as i32 {
+        if depth >= 3 && move_count > 1 + 2 * NODE::ROOT as i32 {
             reduction -= 98 * (history - 568) / 1024;
             reduction -= 3295 * correction_value.abs() / 1024;
             reduction -= 54 * move_count;
