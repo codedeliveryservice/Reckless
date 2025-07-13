86cf8775cc6c7ee9228c7782225e63158ab351d0
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-13 15:21:32 +0300

    Bench: 1644593

--- src/search.rs
+++ src/search.rs
@@ -582,9 +582,9 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
 
             // Static Exchange Evaluation Pruning (SEE Pruning)
             let threshold = if is_quiet {
-                -22 * lmr_depth * lmr_depth - 44 * (history + 19) / 1024
+                -22 * lmr_depth * lmr_depth - 44 * (history + 256) / 1024
             } else {
-                -92 * depth + 45 - 43 * (history + 13) / 1024
+                -92 * depth + 45 - 43 * (history + 256) / 1024
             };
 
             if !td.board.see(mv, threshold) {
