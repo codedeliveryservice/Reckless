aad5c3578e9d545c43281d39bf6ef0d2fc114f19
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-14 15:11:49 +0300

    Bench: 1719474

--- src/search.rs
+++ src/search.rs
@@ -957,7 +957,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32) -> i3
 
         move_count += 1;
 
-        if !is_loss(best_score) && mv.to() != previous_square {
+        if !is_loss(best_score) && mv.to() != previous_square && !td.board.might_give_check_if_you_squint(mv) {
             if move_picker.stage() == Stage::BadNoisy {
                 break;
             }
