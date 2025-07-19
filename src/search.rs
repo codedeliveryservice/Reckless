dab459b730af46e106bfd9f49f6ccfda31cc2210
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-19 13:25:19 +0300

    Bench: 1384099

--- src/search.rs
+++ src/search.rs
@@ -243,8 +243,8 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
             && tt_depth >= depth
             && is_valid(tt_score)
             && match tt_bound {
-                Bound::Upper => tt_score <= alpha && (!cut_node || depth > 5),
-                Bound::Lower => tt_score >= beta && (cut_node || depth > 5),
+                Bound::Upper => tt_score <= alpha && !cut_node,
+                Bound::Lower => tt_score >= beta && cut_node,
                 _ => true,
             }
         {
