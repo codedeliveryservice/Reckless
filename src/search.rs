41812cd25e2f541e7e1aa3df51ee23ecf9b91dd5
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-23 17:34:56 +0300

    Bench: 1479606

--- src/search.rs
+++ src/search.rs
@@ -816,7 +816,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 * (is_valid(td.stack[td.ply - 1].static_eval) && best_score <= -td.stack[td.ply - 1].static_eval - 102)
                     as i32;
 
-            let scaled_bonus = factor * (148 * depth - 43).min(1673) / 128;
+            let scaled_bonus = factor.min(128 * 5) * (148 * depth - 43).min(1673) / 128;
 
             td.quiet_history.update(td.board.prior_threats(), !td.board.side_to_move(), pcm_move, scaled_bonus);
         }
