1b63f4d63ca784c0c295dfd8f54794d0b5fc0d63
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-28 00:39:37 +0300

    Bench: 1673351

--- src/nnue.rs
+++ src/nnue.rs
@@ -30,7 +30,7 @@ const DEQUANT_MULTIPLIER: f32 = (1 << FT_SHIFT) as f32 / (FT_QUANT * FT_QUANT * 
 #[rustfmt::skip]
 const BUCKETS: [usize; 64] = [
     0, 0, 1, 1, 1, 1, 0, 0,
-    2, 2, 2, 2, 2, 2, 2, 2,
+    3, 3, 3, 3, 3, 3, 3, 3,
     3, 3, 3, 3, 3, 3, 3, 3,
     3, 3, 3, 3, 3, 3, 3, 3,
     3, 3, 3, 3, 3, 3, 3, 3,
