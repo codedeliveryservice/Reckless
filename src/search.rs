6a3e86ec50b6429a0db9e11fc735df4254f655e2
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-07-28 08:42:06 +0300

    Bench: 1944910

--- src/search.rs
+++ src/search.rs
@@ -703,7 +703,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         }
         // Full Depth Search (FDS)
         else if !NODE::PV || move_count > 1 {
-            td.stack[td.ply - 1].reduction = 1024 * ((initial_depth - 1) - new_depth);
+            td.stack[td.ply - 1].reduction = 1024 * (initial_depth - new_depth);
             score = -search::<NonPV>(td, -alpha - 1, -alpha, new_depth, !cut_node);
             td.stack[td.ply - 1].reduction = 0;
         }
