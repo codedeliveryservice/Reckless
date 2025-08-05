f22bd568a7f7ac3166d835e7570bb3613c456a94
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-08-05 23:17:27 +0300

    Bench: 1961208

--- src/search.rs
+++ src/search.rs
@@ -379,7 +379,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         depth -= 1;
     }
 
-    let potential_singularity = depth >= 4 + tt_pv as i32
+    let potential_singularity = depth >= 5 + tt_pv as i32
         && tt_depth >= depth - 3
         && tt_bound != Bound::Upper
         && is_valid(tt_score)
