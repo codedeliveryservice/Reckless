0be7a775ba6fbe60ef80b04b4657877d9458f625
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-08-08 10:24:03 +0300

    Bench: 2300128

--- src/search.rs
+++ src/search.rs
@@ -782,7 +782,8 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     if best_move.is_some() {
-        let bonus_noisy = (128 * depth - 60).min(1150) - 69 * cut_node as i32;
+        let bonus_noisy =
+            (128 * depth - 60).min(1150) - 69 * cut_node as i32 + 256 * (move_picker.stage() == Stage::BadNoisy) as i32;
         let malus_noisy = (145 * initial_depth - 67).min(1457) - 26 * noisy_moves.len() as i32;
 
         let bonus_quiet = (151 * depth - 68).min(1597) - 64 * cut_node as i32;
