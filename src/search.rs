887c7e30dc776b5296e2459e6741735625774f35
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-08-11 15:39:47 +0300

    Bench: 2855191

--- src/search.rs
+++ src/search.rs
@@ -1005,7 +1005,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32) -> i3
                 break;
             }
 
-            if !in_check && futility_score <= alpha && !td.board.see(mv, 1) {
+            if !in_check && !td.board.see(mv, alpha - futility_score) {
                 best_score = best_score.max(futility_score);
                 continue;
             }
