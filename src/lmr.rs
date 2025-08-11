0875b7c93eec43d8fdcad1eda4d7f7c44e339edd
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-08-11 19:09:39 +0300

    Bench: 2293388

--- src/lmr.rs
+++ src/lmr.rs
@@ -2,14 +2,13 @@ const LEN: usize = 11;
 
 const SINGLE_VALUES_LEN: usize = LEN;
 const DOUBLE_VALUES_LEN: usize = LEN * (LEN - 1) / 2;
-const TRIPLE_VALUES_LEN: usize = LEN * (LEN - 1) * (LEN - 2) / 6;
 
-const SINGLE_VALUES: [i32; SINGLE_VALUES_LEN] = [-590, -663, 1193, 0, 0, 0, 0, 0, 1232, -794, -794];
+const SINGLE_VALUES: [i32; SINGLE_VALUES_LEN] = [-744, -608, 1717, -92, 121, -94, 236, -28, 1417, -767, -915];
 const DOUBLE_VALUES: [i32; DOUBLE_VALUES_LEN] = [
-    0, 0, 0, 0, 0, 0, -573, 0, 0, 0, -796, -652, 0, -783, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 768, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    41, -90, 40, 24, -19, 199, -489, -169, -54, 57, -727, -546, 48, -971, 57, 18, 11, -25, -156, 3, -139, -3, -25, -70,
+    -33, 9, 132, 109, -44, -50, -6, 37, 125, 83, 92, 886, 246, -104, 62, -30, -109, -3, -174, -87, 51, 118, 15, 52,
+    -60, -117, 44, -100, 26, 66, 34,
 ];
-const TRIPLE_VALUES: [i32; TRIPLE_VALUES_LEN] = [0; TRIPLE_VALUES_LEN];
 
 pub fn feature_interaction(features: &[bool]) -> i32 {
     let mut output = 0;
@@ -23,16 +22,6 @@ pub fn feature_interaction(features: &[bool]) -> i32 {
         for j in (i + 1)..LEN {
             output += DOUBLE_VALUES[idx] * (features[i] && features[j]) as i32;
             idx += 1;
-        }
-    }
-
-    let mut idx = 0;
-    for i in 0..LEN {
-        for j in (i + 1)..LEN {
-            for k in (j + 1)..LEN {
-                output += TRIPLE_VALUES[idx] * (features[i] && features[j] && features[k]) as i32;
-                idx += 1;
-            }
         }
     }
 
