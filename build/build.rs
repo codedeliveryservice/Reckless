1444643489f99c616622d450d2602a8b56227d4c
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-08-12 16:16:53 +0300

    v39-309b70ac

--- build/build.rs
+++ build/build.rs
@@ -11,7 +11,7 @@ mod magics;
 mod maps;
 
 const BASE_URL: &str = "https://github.com/codedeliveryservice/RecklessNetworks/raw/main";
-const NETWORK_NAME: &str = "v38-3d420181.nnue";
+const NETWORK_NAME: &str = "v39-309b70ac.nnue";
 
 fn main() {
     generate_model_env();
