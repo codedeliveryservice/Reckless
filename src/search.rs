a6aba6266bf5554314bdda835c09a4406f3f2d72
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-08-13 23:49:44 +0300

    No NMP when all pieces on the board
    Bench: 2734696

--- src/search.rs
+++ src/search.rs
@@ -421,6 +421,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && td.board.has_non_pawns()
         && !potential_singularity
         && !is_loss(beta)
+        && td.board.occupancies().len() < 32
     {
         let r = 5 + depth / 3 + ((eval - beta) / 244).min(3);
 
