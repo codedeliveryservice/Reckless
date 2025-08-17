e7361dcbff2b06b694bf9ddb20a5e3beec4e54c2
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-08-17 04:36:16 +0300

    Bench: 2632730

--- src/search.rs
+++ src/search.rs
@@ -1004,7 +1004,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32) -> i3
                 break;
             }
 
-            if !in_check && futility_score <= alpha && !td.board.see(mv, 1) {
+            if !in_check && futility_score <= alpha && !td.board.see(mv, correction_value(td) / 2) {
                 best_score = best_score.max(futility_score);
                 continue;
             }
