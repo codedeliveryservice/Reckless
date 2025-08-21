b970a17a24f759298c80138fc5ba0799ce8c352c
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-08-21 17:35:48 +0300

    Bench: 2513201

--- src/search.rs
+++ src/search.rs
@@ -428,6 +428,10 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         && !is_win(eval)
         && tt_bound != Bound::Upper
     {
+        if eval == tt_score && tt_depth > TtDepth::SOME {
+            return eval;
+        }
+
         return (eval + beta) / 2;
     }
 
