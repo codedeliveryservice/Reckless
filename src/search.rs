3e20ff48a59d441e3c3423c93c66fa9d4c6e82b4
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-08-27 10:18:06 +0300

    Bench: 2418487
    does it even matter?

--- src/search.rs
+++ src/search.rs
@@ -411,7 +411,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     let improving = improvement > 0;
 
     // Razoring
-    if !NODE::PV && !in_check && eval < alpha - 305 - 239 * depth * depth {
+    if !NODE::PV && !in_check && eval < alpha - 305 - 239 * initial_depth * initial_depth {
         return qsearch::<NonPV>(td, alpha, beta);
     }
 
