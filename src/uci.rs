15ccd1e8ffaf5685bf69c80471052173fadfdf2e
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-08-29 18:32:09 +0300

    Reckless v0.8.0
    Bench: 2052487

--- src/uci.rs
+++ src/uci.rs
@@ -55,7 +55,7 @@ pub fn message_loop() {
 }
 
 fn uci() {
-    println!("id name Reckless {}", env!("ENGINE_VERSION"));
+    println!("id name Reckless {}", env!("CARGO_PKG_VERSION"));
     println!("id author Arseniy Surkov, Shahin M. Shahin, and Styx");
     println!("option name Hash type spin default {DEFAULT_TT_SIZE} min 1 max 262144");
     println!("option name Threads type spin default 1 min 1 max 512");
