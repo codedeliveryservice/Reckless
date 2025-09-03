f8da29d1609a1184913bbf2df95d2f7e06acfad0
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-04 00:45:41 +0300

    Bench: 2030068

--- src/search.rs
+++ src/search.rs
@@ -787,9 +787,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 reduction -= 3072;
             }
 
-            td.stack[td.ply - 1].reduction = 1024 * ((initial_depth - 1) - new_depth);
             score = -search::<NonPV>(td, -alpha - 1, -alpha, new_depth - (reduction > 3000) as i32, !cut_node);
-            td.stack[td.ply - 1].reduction = 0;
         }
 
         // Principal Variation Search (PVS)
