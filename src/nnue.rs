3fdad2bc0beed0fdf8e2afab758d66a574f33a0e
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-04 10:55:24 +0300

    Bench: 2650070

--- src/nnue.rs
+++ src/nnue.rs
@@ -38,7 +38,7 @@ mod simd {
 
 const NETWORK_SCALE: i32 = 400;
 
-const INPUT_BUCKETS: usize = 10;
+const INPUT_BUCKETS: usize = 12;
 
 const FT_SIZE: usize = 768;
 const L1_SIZE: usize = 1024;
@@ -57,14 +57,14 @@ const DEQUANT_MULTIPLIER: f32 = (1 << FT_SHIFT) as f32 / (FT_QUANT * FT_QUANT * 
 
 #[rustfmt::skip]
 const BUCKETS: [usize; 64] = [
-    0, 1, 2, 3, 3, 2, 1, 0,
-    4, 5, 6, 7, 7, 6, 5, 4,
-    8, 8, 8, 8, 8, 8, 8, 8,
-    9, 9, 9, 9, 9, 9, 9, 9,
-    9, 9, 9, 9, 9, 9, 9, 9,
-    9, 9, 9, 9, 9, 9, 9, 9,
-    9, 9, 9, 9, 9, 9, 9, 9,
-    9, 9, 9, 9, 9, 9, 9, 9,
+     0,  1,  2,  3,  3,  2,  1,  0,
+     4,  5,  6,  7,  7,  6,  5,  4,
+     8,  8,  9,  9,  9,  9,  8,  8,
+    10, 10, 10, 10, 10, 10, 10, 10,
+    10, 10, 10, 10, 10, 10, 10, 10,
+    10, 10, 11, 11, 11, 11, 10, 10,
+    11, 11, 11, 11, 11, 11, 11, 11,
+    11, 11, 11, 11, 11, 11, 11, 11,
 ];
 
 #[repr(align(16))]
