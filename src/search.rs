40c21c29d6288cc6851936ad32f4a1c263ea0cf1
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-06 20:53:15 +0300

    Bench: 2101901

--- src/search.rs
+++ src/search.rs
@@ -420,7 +420,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     if !tt_pv
         && !in_check
         && !excluded
-        && eval >= beta
+        && eval >= beta + 32
         && eval
             >= beta + 10 * depth * depth + 30 * depth - (72 * improving as i32) - (23 * cut_node as i32)
                 + 588 * correction_value.abs() / 1024
