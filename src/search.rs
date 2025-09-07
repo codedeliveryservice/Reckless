b2251b646cce3d9e94b3c0cb255d377809b9824a
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-09-07 16:35:31 +0300

    Bench: 2096133

--- src/search.rs
+++ src/search.rs
@@ -710,7 +710,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
             }
 
             let reduced_depth = (new_depth - reduction / 1024)
-                .clamp(NODE::PV as i32, new_depth + cut_node as i32 + NODE::PV as i32)
+                .clamp(NODE::PV as i32, new_depth + cut_node as i32 + 2 * NODE::PV as i32)
                 + NODE::PV as i32;
 
             td.stack[td.ply - 1].reduction = reduction;
