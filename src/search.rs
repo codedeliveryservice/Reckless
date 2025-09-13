830834866c1eda495e4a1fef18e6d39df40f2064
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-09-13 05:47:57 +0300

    Bench: 3106457

--- src/search.rs
+++ src/search.rs
@@ -645,8 +645,8 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         if !NODE::ROOT && !excluded && td.ply < 2 * td.root_depth as usize && mv == tt_move && potential_singularity {
             debug_assert!(is_valid(tt_score));
 
-            let singular_beta = tt_score - depth;
-            let singular_depth = (depth - 1) / 2;
+            let singular_beta = tt_score - depth + 2;
+            let singular_depth = (depth - 2) / 2;
 
             td.stack[td.ply].excluded = tt_move;
             let score = search::<NonPV>(td, singular_beta - 1, singular_beta, singular_depth, cut_node);
