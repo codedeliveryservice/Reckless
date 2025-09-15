8432184af2aa94b34b324068e7078182f7edfae3
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-15 19:52:36 +0300

    Bench: 2752553

--- src/search.rs
+++ src/search.rs
@@ -181,7 +181,7 @@ pub fn start(td: &mut ThreadData, report: Report) {
         }
 
         let multiplier = || {
-            let nodes_factor = 2.15 - 1.5 * (td.root_moves[0].nodes as f32 / td.nodes.local() as f32);
+            let nodes_factor = 2.05 - 1.5 * (td.root_moves[0].nodes as f32 / td.nodes.local() as f32);
 
             let pv_stability = 1.25 - 0.05 * pv_stability.min(8) as f32;
 
