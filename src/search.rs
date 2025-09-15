86df83c0607be3f84a2bdb33267da721dc73598a
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-15 08:42:23 +0300

    Bench: 2566935

--- src/search.rs
+++ src/search.rs
@@ -658,7 +658,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
 
             if score < singular_beta {
                 let double_margin = 2 + 277 * NODE::PV as i32;
-                let triple_margin = 67 + 315 * NODE::PV as i32 - 16 * correction_value.abs() / 128;
+                let triple_margin = 67 + 315 * NODE::PV as i32 - 16 * correction_value.abs() / 128 + 96 * tt_pv as i32;
 
                 extension = 1;
                 extension += (score < singular_beta - double_margin) as i32;
