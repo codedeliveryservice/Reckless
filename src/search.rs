04da99e5a80d95a40d99e4d880a3e62986b63d63
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-15 12:00:14 +0300

    Bench: 2666563

--- src/search.rs
+++ src/search.rs
@@ -742,14 +742,14 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
 
                 if new_depth > reduced_depth {
                     score = -search::<NonPV>(td, -alpha - 1, -alpha, new_depth, !cut_node);
+                }
 
-                    if mv.is_quiet() && score >= beta {
-                        let bonus = (1 + (move_count / depth)) * (159 * depth - 62).min(866);
+                if mv.is_quiet() && score >= beta {
+                    let bonus = (1 + (move_count / depth)) * (159 * depth - 62).min(866);
 
-                        td.ply -= 1;
-                        update_continuation_histories(td, td.stack[td.ply].piece, mv.to(), bonus);
-                        td.ply += 1;
-                    }
+                    td.ply -= 1;
+                    update_continuation_histories(td, td.stack[td.ply].piece, mv.to(), bonus);
+                    td.ply += 1;
                 }
             } else if score > alpha && score < best_score + 15 {
                 new_depth -= 1;
