0ae29babdbf911e8a527f4050d1800e82b569727
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-19 11:48:22 +0300

    Bench: 2591008

--- src/search.rs
+++ src/search.rs
@@ -421,6 +421,11 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
 
     let improving = improvement > 0;
 
+    // Internal Iterative Reductions (IIR)
+    if depth >= 3 + 3 * cut_node as i32 && tt_move.is_null() && (NODE::PV || cut_node) {
+        depth -= 1;
+    }
+
     // Razoring
     if !NODE::PV && !in_check && eval < alpha - 320 - 237 * initial_depth * initial_depth {
         return qsearch::<NonPV>(td, alpha, beta);
@@ -536,11 +541,6 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 }
             }
         }
-    }
-
-    // Internal Iterative Reductions (IIR)
-    if depth >= 3 + 3 * cut_node as i32 && tt_move.is_null() && (NODE::PV || cut_node) {
-        depth -= 1;
     }
 
     let mut best_move = Move::NULL;
