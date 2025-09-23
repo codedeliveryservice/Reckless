ce09fb2907f379d45fb70a4db5710f29b882de0a
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-23 12:48:05 +0300

    Bench: 2840822

--- src/search.rs
+++ src/search.rs
@@ -913,7 +913,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
                 td.board.piece_on(best_move.to()).piece_type(),
                 bonus_noisy,
             );
-        } else if !quiet_moves.is_empty() || depth > 3 {
+        } else if !quiet_moves.is_empty() || initial_depth > 3 {
             td.quiet_history.update(td.board.threats(), td.board.side_to_move(), best_move, bonus_quiet);
             update_continuation_histories(td, td.board.moved_piece(best_move), best_move.to(), bonus_cont);
 
