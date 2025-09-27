83a0f878bf7226dccbaf5bd3005ed2f1bdc52a5c
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-28 00:39:57 +0300

    Bench: 3069523

--- src/search.rs
+++ src/search.rs
@@ -500,7 +500,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     // ProbCut
-    let probcut_beta = beta + 259 - 65 * improving as i32 - 32 * cut_node as i32;
+    let probcut_beta = beta + 269 - 65 * improving as i32 - 32 * cut_node as i32;
 
     if cut_node
         && depth >= 3
