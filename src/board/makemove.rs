960e9cc7bbbf025cb49d04fa32e70cb14ec630c5
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-27 15:33:59 +0300

    Consider pinned pieces in SEE (#522)
    Elo   | 1.40 +- 1.13 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.93 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 94744 W: 24288 L: 23905 D: 46551
    Penta | [240, 11237, 24056, 11578, 261]
    https://recklesschess.space/test/7993/
    
    Bench: 2605649
    
    Co-Authored-By: Shahin M. Shahin <41402573+peregrineshahin@users.noreply.github.com>

--- src/board/makemove.rs
+++ src/board/makemove.rs
@@ -1,5 +1,5 @@
 use super::Board;
-use crate::types::{Move, MoveKind, Piece, PieceType, Square, ZOBRIST};
+use crate::types::{Bitboard, Move, MoveKind, Piece, PieceType, Square, ZOBRIST};
 
 impl Board {
     pub fn make_null_move(&mut self) {
@@ -11,9 +11,9 @@ impl Board {
         self.state.plies_from_null = 0;
         self.state.repetition = 0;
         self.state.captured = None;
+        self.state.checkers = Bitboard::default();
 
         self.update_threats();
-        self.update_king_threats();
 
         if self.state.en_passant != Square::None {
             self.state.key ^= ZOBRIST.en_passant[self.state.en_passant];
