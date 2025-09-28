365160b2ff315c83232d84703e4b419a1c686e2b
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-28 13:40:18 +0300

    Bench: 2261445

--- src/thread.rs
+++ src/thread.rs
@@ -273,7 +273,7 @@ impl Default for LmrTable {
 
         for depth in 1..MAX_MOVES {
             for move_count in 1..MAX_MOVES {
-                let reduction = 970.0027 + 457.7087 * (depth as f32).ln() * (move_count as f32).ln();
+                let reduction = 570.0027 + 457.7087 * (depth as f32).ln() * (move_count as f32).log2();
                 table[depth][move_count] = reduction as i32;
             }
         }
