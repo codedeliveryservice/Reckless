6f130dab802a43142439fd5ef39a2deeb563b44b
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-09-28 06:06:28 +0300

    Bench: 2845823

--- src/movepick.rs
+++ src/movepick.rs
@@ -180,7 +180,7 @@ impl MovePicker {
                 continue;
             }
 
-            entry.score = td.quiet_history.get(threats, side, mv)
+            entry.score = td.quiet_history.get(td.board.in_check(), threats, side, mv)
                 + td.conthist(1, mv)
                 + td.conthist(2, mv)
                 + td.conthist(4, mv)
