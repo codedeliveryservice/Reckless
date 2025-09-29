79e9beb933b2cdef29ef13fe5e455d26545ee6ed
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-09-29 04:53:55 +0300

    Bench: 2642240

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -9,18 +9,26 @@ use crate::{
 pub fn evaluate(td: &mut ThreadData) -> i32 {
     let mut eval = td.nnue.evaluate(&td.board);
 
-    let material = material(&td.board);
+    let (our_material, total_material) = material(&td.board);
 
-    eval = (eval * (21366 + material) + td.optimism[td.board.side_to_move()] * (1747 + material)) / 27395;
+    eval = (eval * (21366 + total_material) + td.optimism[td.board.side_to_move()] * (1747 + 2 * our_material)) / 27395;
 
     eval = (eval / 16) * 16 - 1 + (td.board.hash() & 0x2) as i32;
 
     eval.clamp(-Score::TB_WIN_IN_MAX + 1, Score::TB_WIN_IN_MAX - 1)
 }
 
-fn material(board: &Board) -> i32 {
-    [PieceType::Pawn, PieceType::Knight, PieceType::Bishop, PieceType::Rook, PieceType::Queen]
-        .iter()
-        .map(|&pt| board.pieces(pt).len() as i32 * PIECE_VALUES[pt])
-        .sum::<i32>()
+fn material(board: &Board) -> (i32, i32) {
+    let mut our = 0;
+    let mut total = 0;
+
+    for &pt in &[PieceType::Pawn, PieceType::Knight, PieceType::Bishop, PieceType::Rook, PieceType::Queen] {
+        let count_all = board.pieces(pt).len() as i32;
+        let count_our = board.our(pt).len() as i32;
+
+        total += count_all * PIECE_VALUES[pt];
+        our += count_our * PIECE_VALUES[pt];
+    }
+
+    (our, total)
 }
