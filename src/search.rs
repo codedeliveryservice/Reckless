c1516276e7cc021ea524f22e20817511f45bd982
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-29 19:25:51 +0300

    Bench: 2917558

--- src/search.rs
+++ src/search.rs
@@ -987,7 +987,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         td.tt.write(tt_slot, hash, depth, raw_eval, best_score, bound, best_move, td.ply, tt_pv);
     }
 
-    if !(in_check
+    if !(!is_valid(static_eval)
         || best_move.is_noisy()
         || (bound == Bound::Upper && best_score >= static_eval)
         || (bound == Bound::Lower && best_score <= static_eval))
