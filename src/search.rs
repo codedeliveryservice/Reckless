48cc162f912d81cd25ec94f0f5481fcd922bf5fa
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-09-29 14:54:24 +0300

    Bench: 2937574

--- src/search.rs
+++ src/search.rs
@@ -988,6 +988,7 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
     }
 
     if !(in_check
+        || excluded
         || best_move.is_noisy()
         || (bound == Bound::Upper && best_score >= static_eval)
         || (bound == Bound::Lower && best_score <= static_eval))
