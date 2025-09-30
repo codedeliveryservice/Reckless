d8f226396bb33f0b8d6868ddf1a7d1adaae110d8
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-09-30 12:34:13 +0300

    Bench: 2721342

--- src/search.rs
+++ src/search.rs
@@ -582,9 +582,10 @@ fn search<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, mut beta: i32, de
         let is_quiet = mv.is_quiet();
 
         let history = if is_quiet {
-            td.quiet_history.get(td.board.threats(), td.board.side_to_move(), mv)
-                + td.conthist(1, mv)
-                + td.conthist(2, mv)
+            (15 * td.quiet_history.get(td.board.threats(), td.board.side_to_move(), mv)
+                + 15 * td.conthist(1, mv)
+                + 6 * td.conthist(2, mv))
+                / 12
         } else {
             let captured = td.board.piece_on(mv.to()).piece_type();
             td.noisy_history.get(td.board.threats(), td.board.moved_piece(mv), mv.to(), captured)
