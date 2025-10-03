91557193289de96213dec9dc0b794d5c44953bdc
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-10-03 03:27:26 +0300

    Revert "Introduce secondary TT aging (#514)"
    This reverts commit 4e1c372be22e95c7702c2fa2f58f9054ff8a58d1.
    
    Bench: 2940440

--- src/transposition.rs
+++ src/transposition.rs
@@ -201,9 +201,6 @@ impl TranspositionTable {
             || depth + 4 + 2 * pv as i32 > entry.depth as i32
             || entry.flags.age() != tt_age)
         {
-            if entry.depth >= 5 && entry.flags.bound() != Bound::Exact {
-                entry.depth -= 1;
-            }
             return;
         }
 
