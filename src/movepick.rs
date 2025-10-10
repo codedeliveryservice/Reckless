92def39b03c9158742013ae19b493f96ea16021f
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-10-10 13:37:07 +0300

    Bench: 3180434

--- src/movepick.rs
+++ src/movepick.rs
@@ -172,8 +172,10 @@ impl MovePicker {
             let captured =
                 if entry.mv.is_en_passant() { PieceType::Pawn } else { td.board.piece_on(mv.to()).piece_type() };
 
-            entry.score = 2007 * PIECE_VALUES[captured] / 128
-                + 1061 * td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured) / 1024;
+            entry.score = 16 * PIECE_VALUES[captured]
+                + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured);
+
+            entry.score /= 32;
         }
     }
 
