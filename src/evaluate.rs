1a7c4f9fb57a111ec12c84ae10e3443dd7b3669a
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-10-13 05:12:43 +0300

    Bench: 2735130

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -9,7 +9,7 @@ use crate::{
 pub fn evaluate(td: &mut ThreadData) -> i32 {
     let mut eval = td.nnue.evaluate(&td.board);
 
-    let material = material(&td.board);
+    let material = 550 * td.board.pieces(PieceType::Pawn).len() as i32 + non_pawn_material(&td.board);
 
     eval = (eval * (21366 + material) + td.optimism[td.board.side_to_move()] * (1747 + material)) / 27395;
 
@@ -18,9 +18,7 @@ pub fn evaluate(td: &mut ThreadData) -> i32 {
     eval.clamp(-Score::TB_WIN_IN_MAX + 1, Score::TB_WIN_IN_MAX - 1)
 }
 
-fn material(board: &Board) -> i32 {
-    [PieceType::Pawn, PieceType::Knight, PieceType::Bishop, PieceType::Rook, PieceType::Queen]
-        .iter()
-        .map(|&pt| board.pieces(pt).len() as i32 * PIECE_VALUES[pt])
-        .sum::<i32>()
+fn non_pawn_material(board: &Board) -> i32 {
+    use PieceType::*;
+    [Knight, Bishop, Rook, Queen].iter().map(|&pt| board.pieces(pt).len() as i32 * PIECE_VALUES[pt]).sum()
 }
