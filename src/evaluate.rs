e79ce8af4fb2a891fd038ba0d133a6ac50981246
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-10-13 06:20:12 +0300

    Bench: 3141212

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -9,7 +9,7 @@ use crate::{
 pub fn evaluate(td: &mut ThreadData) -> i32 {
     let mut eval = td.nnue.evaluate(&td.board);
 
-    let material = 550 * td.board.pieces(PieceType::Pawn).len() as i32 + non_pawn_material(&td.board);
+    let material = 320 * td.board.pieces(PieceType::Pawn).len() as i32 + non_pawn_material(&td.board);
 
     eval = (eval * (21366 + material) + td.optimism[td.board.side_to_move()] * (1747 + material)) / 27395;
 
