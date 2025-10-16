feeed5c9d3f2e33265bd56348212d5078eb057a2
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-10-16 08:17:16 +0300

    Bench: 4069976

--- src/search.rs
+++ src/search.rs
@@ -966,17 +966,11 @@ fn search<NODE: NodeType>(
                 * (is_valid(td.stack[ply - 1].static_eval) && best_score <= -td.stack[ply - 1].static_eval - 100)
                     as i32;
 
-            let scaled_bonus = factor * (156 * initial_depth - 42).min(1789) / 128;
+            let quiet_bonus = factor * (156 * initial_depth - 42).min(1789) / 128;
+            td.quiet_history.update(td.board.prior_threats(), !td.board.side_to_move(), pcm_move, quiet_bonus);
 
-            td.quiet_history.update(td.board.prior_threats(), !td.board.side_to_move(), pcm_move, scaled_bonus);
-
-            if ply >= 2 {
-                let entry = &td.stack[ply - 2];
-                if entry.mv.is_some() {
-                    let bonus = (151 * initial_depth - 41).min(1630);
-                    td.continuation_history.update(entry.conthist, td.stack[ply - 1].piece, pcm_move.to(), bonus);
-                }
-            }
+            let cont_bonus = (151 * initial_depth - 41).min(1630);
+            update_continuation_histories(td, ply - 1, td.stack[ply - 1].piece, pcm_move.to(), cont_bonus);
         }
     }
 
