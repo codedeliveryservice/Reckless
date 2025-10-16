bed39a97ff0a1f66a47259121b81459361e6451d
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-10-16 21:01:32 +0300

    Bench: 3052606

--- src/parameters.rs
+++ src/parameters.rs
@@ -34,3 +34,51 @@ macro_rules! define {
         }
     };
 }
+
+define!(
+    i32 lmr1: 489;
+    i32 lmr2: 412;
+    i32 lmr3: 1243;
+    i32 lmr4: 489;
+    i32 lmr5: 137;
+    i32 lmr6: 488;
+    i32 lmr7: 109;
+    i32 lmr8: 46;
+    i32 lmr9: 3607;
+    i32 lmr10: 69;
+    i32 lmr11: 427;
+    i32 lmr12: 677;
+    i32 lmr13: 729;
+    i32 lmr14: 393;
+    i32 lmr15: 552;
+    i32 lmr16: 1675;
+    i32 lmr17: 934;
+    i32 lmr18: 1049;
+    i32 lmr19: 1555;
+    i32 lmr20: 791;
+    i32 lmr21: 1397;
+    i32 dod1: 37;
+    i32 dod2: 495;
+    i32 post1: 155;
+    i32 post2: 63;
+    i32 post3: 851;
+    i32 fds1: 380;
+    i32 fds2: 153;
+    i32 fds3: 355;
+    i32 fds4: 68;
+    i32 fds5: 47;
+    i32 fds6: 2667;
+    i32 fds7: 52;
+    i32 fds8: 750;
+    i32 fds9: 537;
+    i32 fds10: 1081;
+    i32 fds11: 491;
+    i32 fds12: 780;
+    i32 fds13: 1478;
+    i32 fds14: 1048;
+    i32 fds15: 744;
+    i32 fds16: 1438;
+    i32 fds17: 849;
+    i32 fds18: 1052;
+    i32 fds19: 3034;
+);
