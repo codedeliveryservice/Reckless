63ba77f196b5758e8bcb6a210d3f8e312806d322
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-10-26 15:16:21 +0300

    Bench: 3306116

--- src/search.rs
+++ src/search.rs
@@ -969,6 +969,7 @@ fn search<NODE: NodeType>(
         if pcm_move.is_quiet() {
             let mut factor = 104;
             factor += 147 * (initial_depth > 5) as i32;
+            factor += 100 * (td.stack[ply].cutoff_count < 4) as i32;
             factor += 184 * (td.stack[ply - 1].move_count > 8) as i32;
             factor += 217 * (!in_check && best_score <= static_eval.min(raw_eval) - 132) as i32;
             factor += 297
