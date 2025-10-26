74421a2f1b12774364ae7cde1f140aa929a1c7ea
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-10-26 04:47:48 +0300

    Bench: 3053942

--- src/search.rs
+++ src/search.rs
@@ -173,9 +173,9 @@ pub fn start(td: &mut ThreadData, report: Report) {
         let multiplier = || {
             let nodes_factor = 2.15 - 1.5 * (td.root_moves[0].nodes as f32 / td.nodes.local() as f32);
 
-            let pv_stability = 1.25 - 0.05 * pv_stability.min(8) as f32;
+            let pv_stability = 1.45 - 0.10 * pv_stability.min(8) as f32;
 
-            let eval_stability = 1.2 - 0.04 * eval_stability.min(8) as f32;
+            let eval_stability = 1.45 - 0.10 * eval_stability.min(8) as f32;
 
             let score_trend = (0.8 + 0.05 * (td.previous_best_score - td.root_moves[0].score) as f32).clamp(0.80, 1.45);
 
