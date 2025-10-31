07e059934bfac71b4d8abb43a187e7515737b5e7
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-10-31 10:27:20 +0300

    (td.id & 3) == 2
    Bench: 3239556

--- src/search.rs
+++ src/search.rs
@@ -594,11 +594,11 @@ fn search<NODE: NodeType>(
             }
         }
 
-        if NODE::ROOT && td.id == 2 && !td.root_in_tb && move_count == 0 {
+        if NODE::ROOT && (td.id & 3) == 2 && !td.root_in_tb && move_count == 0 {
             depth += 4;
         }
 
-        if NODE::ROOT && td.id == 2 && !td.root_in_tb && move_count == 1 && best_move.is_some() {
+        if NODE::ROOT && (td.id & 3) == 2 && !td.root_in_tb && move_count == 1 && best_move.is_some() {
             break;
         }
 
