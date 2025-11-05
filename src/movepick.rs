b6887f274172353e4d3573fd348ab3f0cdb3f5d5
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-11-05 20:16:27 +0300

    Bench: 3247418

--- src/movepick.rs
+++ src/movepick.rs
@@ -1,4 +1,5 @@
 use crate::{
+    misc::dbg_stats,
     parameters::PIECE_VALUES,
     search::NodeType,
     thread::ThreadData,
@@ -164,8 +165,11 @@ impl MovePicker {
             let captured =
                 if entry.mv.is_en_passant() { PieceType::Pawn } else { td.board.piece_on(mv.to()).piece_type() };
 
-            entry.score = 16 * PIECE_VALUES[captured]
-                + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured);
+            entry.score = td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured);
+
+            if mv.to() != td.board.recapture_square() {
+                entry.score += 16 * PIECE_VALUES[captured];
+            }
         }
     }
 
