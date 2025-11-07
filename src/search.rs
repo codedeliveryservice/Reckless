1d4be88b7252b10d71605c4cc296456d79359058
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-11-07 05:59:02 +0300

    Bench: 3843302

--- src/search.rs
+++ src/search.rs
@@ -444,6 +444,7 @@ fn search<NODE: NodeType>(
         && is_valid(eval)
         && !excluded
         && depth < 9
+        && depth > tt_depth
         && eval >= beta
         && static_eval >= beta + 75 * depth - (85 * improving as i32) + 580 * correction_value.abs() / 1024
         && !is_loss(beta)
