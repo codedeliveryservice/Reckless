adf899b799d694a606fd6e1a6fd9562ade81a6cb
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-11-11 08:20:14 +0300

    Bench: 4319638

--- src/search.rs
+++ src/search.rs
@@ -556,7 +556,7 @@ fn search<NODE: NodeType>(
     }
 
     // Internal Iterative Reductions (IIR)
-    if depth >= 3 + 3 * cut_node as i32 && tt_move.is_null() && (NODE::PV || cut_node) {
+    if depth >= 3 + 3 * cut_node as i32 && tt_bound == Bound::None && (NODE::PV || cut_node) {
         depth -= 1;
     }
 
