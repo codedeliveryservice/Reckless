fdca29dac1663eec9cc20a33eb9b494b398970ac
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-11-11 09:48:49 +0300

    Bench: 3909586

--- src/movepick.rs
+++ src/movepick.rs
@@ -184,8 +184,8 @@ impl MovePicker {
             entry.score = td.quiet_history.get(threats, side, mv)
                 + td.conthist(ply, 1, mv)
                 + td.conthist(ply, 2, mv)
-                + td.conthist(ply, 4, mv)
-                + td.conthist(ply, 6, mv);
+                + td.conthist(ply, 3, mv)
+                + td.conthist(ply, 4, mv);
         }
     }
 }
