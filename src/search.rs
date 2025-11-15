fc824dc0ec77c2954c6aae46b7976fd3081d1e73
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-11-15 03:25:23 +0300

    Bench: 3106987

--- src/search.rs
+++ src/search.rs
@@ -455,6 +455,7 @@ fn search<NODE: NodeType>(
         && eval >= beta + 10 * depth * depth + 30 * depth - (75 * improving as i32) + correction_value.abs() / 2
         && !is_loss(beta)
         && !is_win(eval)
+        && td.stack[ply - 1].mv != td.stack[ply - 1].tt_move
     {
         return beta + (eval - beta) / 3;
     }
