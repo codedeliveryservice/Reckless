f8c15308f96564ba335b14ca3493e1a98806bdf0
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-11-18 01:16:03 +0300

    Bench: 4045351

--- src/search.rs
+++ src/search.rs
@@ -746,7 +746,7 @@ fn search<NODE: NodeType>(
                 reduction += 1555;
             }
 
-            if is_valid(tt_score) && tt_score < alpha && tt_bound == Bound::Upper {
+            if is_valid(tt_score) && tt_score.max(static_eval) < alpha && tt_bound == Bound::Upper {
                 reduction += 791;
             }
 
