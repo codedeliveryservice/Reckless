0a3056f475e6f4cc4648ad9e321014ba5a9466fe
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-11-26 04:54:53 +0300

    Make sense of some names as per request (#598)
    Bench: 3755992

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -5,17 +5,17 @@ use crate::{
     types::{PieceType, Score},
 };
 
-/// Calculates the score of the current position from the perspective of the side to move.
+/// Calculates the raw evaluation of the current position from the perspective of the side to move before corrections.
 pub fn evaluate(td: &mut ThreadData) -> i32 {
-    let mut eval = td.nnue.evaluate(&td.board);
+    let mut raw_eval = td.nnue.evaluate(&td.board);
 
     let material = material(&td.board);
 
-    eval = (eval * (21366 + material) + td.optimism[td.board.side_to_move()] * (1747 + material)) / 27395;
+    raw_eval = (raw_eval * (21366 + material) + td.optimism[td.board.side_to_move()] * (1747 + material)) / 27395;
 
-    eval = (eval / 16) * 16 - 1 + (td.board.hash() & 0x2) as i32;
+    raw_eval = (raw_eval / 16) * 16 - 1 + (td.board.hash() & 0x2) as i32;
 
-    eval.clamp(-Score::TB_WIN_IN_MAX + 1, Score::TB_WIN_IN_MAX - 1)
+    raw_eval.clamp(-Score::TB_WIN_IN_MAX + 1, Score::TB_WIN_IN_MAX - 1)
 }
 
 fn material(board: &Board) -> i32 {
