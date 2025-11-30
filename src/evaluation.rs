6ae1859d91bf855edd6cb103f217e07d69963d75
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-01 01:50:30 +0300

    Decouple raw evaluation from adjustments (#607)
    Elo   | 2.23 +- 2.36 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.99 (-2.25, 2.89) [-2.75, 0.25]
    Games | N: 21142 W: 5345 L: 5209 D: 10588
    Penta | [31, 2484, 5417, 2596, 43]
    https://recklesschess.space/test/9459/
    
    Bench: 4085577
    
    Co-authored-by: Shahin M. Shahin <peregrineshahin@gmail.com>

---
+++ src/evaluation.rs
@@ -0,0 +1,17 @@
+use crate::{thread::ThreadData, types::Score};
+
+pub fn correct_eval(td: &ThreadData, raw_eval: i32, correction_value: i32) -> i32 {
+    let mut eval = (raw_eval * (21366 + td.board.material())
+        + td.optimism[td.board.side_to_move()] * (1747 + td.board.material()))
+        / 27395;
+
+    eval = (eval / 16) * 16;
+
+    eval += (td.board.hash() & 0x2) as i32 - 1;
+
+    eval = eval * (200 - td.board.halfmove_clock() as i32) / 200;
+
+    eval += correction_value;
+
+    eval.clamp(-Score::TB_WIN_IN_MAX + 1, Score::TB_WIN_IN_MAX - 1)
+}
