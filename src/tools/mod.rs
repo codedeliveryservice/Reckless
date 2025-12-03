9fc9676a9e7dd121c968ad34ec41e529505d9e58
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-03 08:23:09 +0300

    Add rescoring tool

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -7,9 +7,11 @@ pub use perft::perft;
 mod binpack;
 mod duplicates;
 mod pgn;
+mod rescore;
 mod stats;
 
 pub use binpack::*;
 pub use duplicates::*;
 pub use pgn::*;
+pub use rescore::*;
 pub use stats::*;
