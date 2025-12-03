a8d78bc40a8647d9b5a88e60eae13f2dce15d48d
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-03 05:13:10 +0300

    Remove outdated buckets tool

--- src/main.rs
+++ src/main.rs
@@ -29,10 +29,6 @@ fn main() {
 
     match std::env::args().nth(1).as_deref() {
         Some("bench") => tools::bench::<false>(None),
-        Some("collect") => {
-            let files: Vec<_> = std::env::args().skip(2).collect();
-            tools::collect_buckets(&files);
-        }
         Some("duplicates") => {
             let files: Vec<_> = std::env::args().skip(2).collect();
             tools::duplicates(&files);
