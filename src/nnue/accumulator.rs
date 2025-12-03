b917af0870983539f6575c07215a0e119c129658
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-03 07:28:52 +0300

    Fix tiling for AVX-512 targets

--- src/nnue/accumulator.rs
+++ src/nnue/accumulator.rs
@@ -183,7 +183,7 @@ impl PstAccumulator {
 }
 
 unsafe fn apply_changes(entry: &mut CacheEntry, adds: ArrayVec<usize, 32>, subs: ArrayVec<usize, 32>) {
-    const REGISTERS: usize = 8;
+    const REGISTERS: usize = 6;
 
     let mut registers: [_; REGISTERS] = std::mem::zeroed();
 
