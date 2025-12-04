5a39fdb3405dbcd70ea858dfc10e137fd9cc41fd
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-04 05:44:42 +0300

    Bench: 3407399

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -7,8 +7,6 @@ pub fn correct_eval(td: &ThreadData, raw_eval: i32, correction_value: i32) -> i3
 
     eval = (eval / 16) * 16;
 
-    eval += (td.board.hash() & 0x2) as i32 - 1;
-
     eval = eval * (200 - td.board.halfmove_clock() as i32) / 200;
 
     eval += correction_value;
