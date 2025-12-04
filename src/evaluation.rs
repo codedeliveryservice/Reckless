c1f7d6b11b2ce6c726350cfb8ed02d8a8040db7e
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-04 12:22:27 +0300

    Bench: 3232915

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -5,9 +5,9 @@ pub fn correct_eval(td: &ThreadData, raw_eval: i32, correction_value: i32) -> i3
         + td.optimism[td.board.side_to_move()] * (1747 + td.board.material()))
         / 27395;
 
-    eval = (eval / 16) * 16;
-
     eval = eval * (200 - td.board.halfmove_clock() as i32) / 200;
+
+    eval = (eval / 16) * 16;
 
     eval += correction_value;
 
