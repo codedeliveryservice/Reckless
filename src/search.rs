e81d321b40bc490c3f0c19d30b2fdcc82ca4e95e
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-12-06 04:59:38 +0300

    Assume no singularity is likely to exists if the bound is exact and the tt_score is failing low
    Bench: 3181651

--- src/search.rs
+++ src/search.rs
@@ -476,8 +476,12 @@ fn search<NODE: NodeType>(
         depth -= 1;
     }
 
-    let potential_singularity =
-        depth >= 5 && tt_depth >= depth - 3 && tt_bound != Bound::Upper && is_valid(tt_score) && !is_decisive(tt_score);
+    let potential_singularity = depth >= 5
+        && tt_depth >= depth - 3
+        && tt_bound != Bound::Upper
+        && !(tt_bound == Bound::Exact && tt_score <= alpha)
+        && is_valid(tt_score)
+        && !is_decisive(tt_score);
 
     let mut improvement = 0;
 
