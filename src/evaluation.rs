defc989f8b7703c07232fe53774b1533428294e6
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-22 18:16:37 +0300

    Bench: 3227057

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -1,9 +1,9 @@
 use crate::{thread::ThreadData, types::Score};
 
 pub fn correct_eval(td: &ThreadData, raw_eval: i32, correction_value: i32) -> i32 {
-    let mut eval = (raw_eval * (21372 + td.board.material())
-        + td.optimism[td.board.side_to_move()] * (1536 + td.board.material()))
-        / 27380;
+    let mut eval = (raw_eval * (21372 + td.board.material())) / 27380;
+
+    eval += td.optimism[td.board.side_to_move()] * (100 - td.board.halfmove_clock() as i32) / 100;
 
     eval = (eval / 16) * 16;
 
