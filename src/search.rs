a97c690211851d59bdaf47b51ecf95c59906984f
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-27 10:07:12 +0300

    Bench: 3657516

--- src/search.rs
+++ src/search.rs
@@ -503,6 +503,7 @@ fn search<NODE: NodeType>(
             >= beta + 1085 * depth * depth / 128 + 25 * depth - (79 * improving as i32)
                 + 500 * correction_value.abs() / 1024
                 + 35 * (depth == 1) as i32
+                + 16
         && !is_loss(beta)
         && !is_win(estimated_score)
     {
