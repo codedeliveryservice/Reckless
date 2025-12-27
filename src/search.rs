c903e3852b7f44bbd6f94e086595710cc3e0f224
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-27 07:38:51 +0300

    Bench: 3745943

--- src/search.rs
+++ src/search.rs
@@ -1187,9 +1187,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32, ply: 
                 break;
             }
 
-            let futility_score = best_score + 42 * PIECE_VALUES[td.board.piece_on(mv.to()).piece_type()] / 128 + 109;
-
-            if !in_check && futility_score <= alpha && !td.board.see(mv, 1) {
+            if best_score + 222 <= alpha && !td.board.see(mv, 1) {
                 continue;
             }
         }
