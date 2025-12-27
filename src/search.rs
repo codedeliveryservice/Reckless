82af69722e078a75cee6eaacac1ff654888f5116
Shahin M. Shahin <peregrineshahin@gmail.com>
2025-12-27 03:45:22 +0300

    Bench: 3371059

--- src/search.rs
+++ src/search.rs
@@ -560,7 +560,7 @@ fn search<NODE: NodeType>(
     }
 
     // ProbCut
-    let probcut_beta = beta + 257 - 75 * improving as i32;
+    let probcut_beta = beta + 257 - 75 * improving as i32 + 128 * tt_pv as i32;
 
     if cut_node
         && depth >= 3
