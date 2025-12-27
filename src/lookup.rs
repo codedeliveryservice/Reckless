967318151c06dd17c42950984bfaffc93d897375
87 <dev@87flowers.com>
2025-12-28 00:06:33 +0100

    Setwise pawn threats
    Bench: 3504025

--- src/lookup.rs
+++ src/lookup.rs
@@ -1,4 +1,4 @@
-use crate::types::{Bitboard, Color, Piece, PieceType, Square, ZOBRIST};
+use crate::types::{Bitboard, Color, File, Piece, PieceType, Square, ZOBRIST};
 
 include!(concat!(env!("OUT_DIR"), "/lookup.rs"));
 
@@ -118,6 +118,18 @@ pub fn attacks(piece: Piece, square: Square, occupancies: Bitboard) -> Bitboard 
         PieceType::King => king_attacks(square),
         PieceType::None => Bitboard(0),
     }
+}
+
+pub fn pawn_attacks_setwise(bb: Bitboard, color: Color) -> Bitboard {
+    let (up_right, up_left) = match color {
+        Color::White => (9, 7),
+        Color::Black => (-7, -9),
+    };
+
+    let right_attacks = (bb & !Bitboard::file(File::H)).shift(up_right);
+    let left_attacks = (bb & !Bitboard::file(File::A)).shift(up_left);
+
+    right_attacks | left_attacks
 }
 
 pub fn pawn_attacks(square: Square, color: Color) -> Bitboard {
