f6ff2b2bc000c9ea83bd4e0a4fa304aa7fd67697
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2025-12-30 19:19:49 +0300

    Bench: 4441815

--- src/search.rs
+++ src/search.rs
@@ -607,7 +607,7 @@ fn search<NODE: NodeType>(
     // Singular Extensions (SE)
     let mut extension = 0;
 
-    if !NODE::ROOT && !excluded && potential_singularity && ply < 2 * td.root_depth as isize {
+    if !NODE::ROOT && !excluded && potential_singularity {
         debug_assert!(is_valid(tt_score));
 
         let singular_beta = tt_score - depth - depth * (tt_pv && !NODE::PV) as i32;
@@ -622,10 +622,15 @@ fn search<NODE: NodeType>(
         }
 
         if score < singular_beta {
-            let double_margin =
-                -4 + 256 * NODE::PV as i32 - 16 * tt_move.is_quiet() as i32 - 16 * correction_value.abs() / 128;
-            let triple_margin =
-                48 + 288 * NODE::PV as i32 - 16 * tt_move.is_quiet() as i32 - 16 * correction_value.abs() / 128;
+            let double_margin = -4 + 256 * NODE::PV as i32
+                - 16 * tt_move.is_quiet() as i32
+                - 16 * correction_value.abs() / 128
+                - 42 * (ply as i32 > td.root_depth) as i32;
+
+            let triple_margin = 48 + 288 * NODE::PV as i32
+                - 16 * tt_move.is_quiet() as i32
+                - 16 * correction_value.abs() / 128
+                - 42 * (ply as i32 > td.root_depth) as i32;
 
             extension = 1;
             extension += (score < singular_beta - double_margin) as i32;
