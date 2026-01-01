7554ce5a94c46a7cae88dfa36b2bc0cbd730e671
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-01-01 05:06:59 +0300

    Bench: 4933351

--- src/nnue.rs
+++ src/nnue.rs
@@ -46,11 +46,11 @@ mod simd {
     pub use scalar::*;
 }
 
-const NETWORK_SCALE: i32 = 380;
+const NETWORK_SCALE: i32 = 402;
 
 const INPUT_BUCKETS: usize = 10;
 
-const L1_SIZE: usize = 512;
+const L1_SIZE: usize = 640;
 const L2_SIZE: usize = 16;
 const L3_SIZE: usize = 32;
 
