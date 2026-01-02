61e8c28d8e357c6d9c7b17a8e72ff1971e999b0b
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-01-02 05:42:53 +0000

    NUMA support

--- src/history.rs
+++ src/history.rs
@@ -1,4 +1,4 @@
-use crate::types::{Bitboard, Color, Move, Piece, PieceType, Square};
+use crate::{numa::NumaValue, types::{Bitboard, Color, Move, Piece, PieceType, Square}};
 
 type FromToHistory<T> = [[T; 64]; 64];
 type PieceToHistory<T> = [[T; 64]; 13];
@@ -118,6 +118,8 @@ pub struct CorrectionHistory {
     // [side_to_move][key]
     entries: Box<[[i16; Self::SIZE]; 2]>,
 }
+
+unsafe impl NumaValue for CorrectionHistory {}
 
 impl CorrectionHistory {
     const MAX_HISTORY: i32 = 14734;
