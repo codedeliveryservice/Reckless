914d70e62d6a93c6988d7caa95dbe9828a39cc29
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-01-02 22:12:52 +0300

    Bench: 4543395

--- src/search.rs
+++ src/search.rs
@@ -634,9 +634,9 @@ fn search<NODE: NodeType>(
 
         if score < singular_beta {
             let double_margin =
-                -4 + 256 * NODE::PV as i32 - 16 * tt_move.is_quiet() as i32 - 16 * correction_value.abs() / 128;
+                -4 + 256 * NODE::PV as i32 - 128 * tt_move.is_quiet() as i32 - 16 * correction_value.abs() / 128;
             let triple_margin =
-                48 + 288 * NODE::PV as i32 - 16 * tt_move.is_quiet() as i32 - 16 * correction_value.abs() / 128;
+                48 + 288 * NODE::PV as i32 - 128 * tt_move.is_quiet() as i32 - 16 * correction_value.abs() / 128;
 
             extension = 1;
             extension += (score < singular_beta - double_margin) as i32;
