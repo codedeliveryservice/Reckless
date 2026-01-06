0651540ce67c6a297557e7bccaba82d0de099960
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-01-06 14:15:20 +0300

    Bench: 3846778

--- src/search.rs
+++ src/search.rs
@@ -689,7 +689,8 @@ fn search<NODE: NodeType>(
         let history = if is_quiet {
             td.quiet_history.get(td.board.threats(), td.board.side_to_move(), mv)
                 + td.conthist(ply, 1, mv)
-                + td.conthist(ply, 2, mv)
+                + td.conthist(ply, 2, mv) / 2
+                + td.conthist(ply, 4, mv) / 2
         } else {
             let captured = td.board.piece_on(mv.to()).piece_type();
             td.noisy_history.get(td.board.threats(), td.board.moved_piece(mv), mv.to(), captured)
