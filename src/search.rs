6c14f660955d0232b2558d5267f29165d003cdf8
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-01-06 15:03:06 +0300

    Bench: 4407698

--- src/search.rs
+++ src/search.rs
@@ -817,7 +817,7 @@ fn search<NODE: NodeType>(
                 reduction -= 1264;
             }
 
-            let reduced_depth = (new_depth - reduction / 1024).clamp(1, new_depth + 1) + 2 * NODE::PV as i32;
+            let reduced_depth = (new_depth - reduction / 1024).clamp(1, new_depth + 1) + 3 * NODE::PV as i32;
 
             td.stack[ply].reduction = reduction;
             score = -search::<NonPV>(td, -alpha - 1, -alpha, reduced_depth, true, ply + 1);
