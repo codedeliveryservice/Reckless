a6a6a6fecb4c2bc151471eb4adf78cd7e741444d
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-01-12 11:12:19 +0300

    Bench: 3387651

--- src/search.rs
+++ src/search.rs
@@ -1210,7 +1210,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32, ply: 
 
         move_count += 1;
 
-        if !is_loss(best_score) && mv.to() != td.board.recapture_square() {
+        if !NODE::PV && !is_loss(best_score) && mv.to() != td.board.recapture_square() {
             if move_picker.stage() == Stage::BadNoisy {
                 break;
             }
