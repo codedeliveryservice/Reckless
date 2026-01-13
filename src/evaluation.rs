b9e0d94dc4a212a69e4a630c01b47b0f930a1c1d
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-01-13 13:04:37 +0300

    Bench: 2820471

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -3,7 +3,7 @@ use crate::{thread::ThreadData, types::Score};
 pub fn correct_eval(td: &ThreadData, raw_eval: i32, correction_value: i32) -> i32 {
     let mut eval = (raw_eval * (21061 + td.board.material())
         + td.optimism[td.board.side_to_move()] * (1519 + td.board.material()))
-        / 26556;
+        / 22556;
 
     eval = eval * (200 - td.board.halfmove_clock() as i32) / 200;
 
