54f9845feccd4c0034669385ab7eef9ba979b108
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-01-23 19:26:05 +0300

    Update default network to v51-3f72a764.nnue (#685)
    Fixed nodes
    Elo   | 3.94 +- 2.17 (95%)
    Conf  | N=25000 Threads=1 Hash=8MB
    Games | N: 40144 W: 12286 L: 11831 D: 16027
    Penta | [775, 4720, 8797, 4835, 945]
    https://recklesschess.space/test/10896/
    
    STC
    Elo   | 3.02 +- 2.10 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 3.01 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 27524 W: 7057 L: 6818 D: 13649
    Penta | [63, 3216, 6973, 3439, 71]
    https://recklesschess.space/test/10895/
    
    LTC
    Elo   | 2.08 +- 1.62 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.90 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 41852 W: 10452 L: 10201 D: 21199
    Penta | [15, 4769, 11104, 5026, 12]
    https://recklesschess.space/test/10897/
    
    Bench: 4326244
    
    Co-Authored-By: Styx <164851643+styxdoto@users.noreply.github.com>

--- build/build.rs
+++ build/build.rs
@@ -11,7 +11,7 @@ mod magics;
 mod maps;
 
 const BASE_URL: &str = "https://github.com/codedeliveryservice/RecklessNetworks/releases/download/networks";
-const NETWORK_NAME: &str = "v50-441ec316.nnue";
+const NETWORK_NAME: &str = "v51-3f72a764.nnue";
 
 fn main() {
     generate_model_env();
