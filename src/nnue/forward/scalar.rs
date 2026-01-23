163d595af05689ff5ff9aafed820699ac9cf938c
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-01-23 19:21:06 +0300

    Bench: 4326244

--- src/nnue/forward/scalar.rs
+++ src/nnue/forward/scalar.rs
@@ -24,27 +24,7 @@ pub fn activate_ft(pst: &PstAccumulator, threat: &ThreatAccumulator, stm: Color)
     output
 }
 
-pub unsafe fn find_nnz(ft_out: &Aligned<[u8; L1_SIZE]>, _: &[SparseEntry]) -> (Aligned<[u16; L1_SIZE / 4]>, usize) {
-    let mut indexes = Aligned::new([0; L1_SIZE / 4]);
-    let mut count = 0;
-
-    for i in 0..L1_SIZE / 4 {
-        let mut nonzero = 0;
-
-        for j in 0..4 {
-            nonzero |= ft_out[i * 4 + j];
-        }
-
-        if nonzero != 0 {
-            indexes[count] = i as u16;
-            count += 1;
-        }
-    }
-
-    (indexes, count)
-}
-
-pub unsafe fn propagate_l1(ft_out: Aligned<[u8; L1_SIZE]>, nnz: &[u16]) -> Aligned<[f32; L2_SIZE]> {
+pub unsafe fn propagate_l1(ft_out: Aligned<[u8; L1_SIZE]>, nnz: &[u16], bucket: usize) -> Aligned<[f32; L2_SIZE]> {
     const CHUNKS: usize = 4;
 
     let mut pre_activations = [0i32; L2_SIZE];
@@ -54,7 +34,7 @@ pub unsafe fn propagate_l1(ft_out: Aligned<[u8; L1_SIZE]>, nnz: &[u16]) -> Align
     for i in 0..nnz.len() {
         let index = *nnz.get_unchecked(i) as usize;
         let input = packed.get_unchecked(index);
-        let weights = &PARAMETERS.l1_weights[index * L2_SIZE * CHUNKS..];
+        let weights = &PARAMETERS.l1_weights[bucket][index * L2_SIZE * CHUNKS..];
 
         for j in 0..L2_SIZE {
             let mut vector = 0;
@@ -73,32 +53,52 @@ pub unsafe fn propagate_l1(ft_out: Aligned<[u8; L1_SIZE]>, nnz: &[u16]) -> Align
     let mut output = Aligned::new([0.0; L2_SIZE]);
 
     for i in 0..L2_SIZE {
-        output[i] = (pre_activations[i] as f32 * DEQUANT_MULTIPLIER + PARAMETERS.l1_biases[i]).clamp(0.0, 1.0);
+        output[i] = (pre_activations[i] as f32 * DEQUANT_MULTIPLIER + PARAMETERS.l1_biases[bucket][i]).clamp(0.0, 1.0);
     }
 
     output
 }
 
-pub fn propagate_l2(l1_out: Aligned<[f32; L2_SIZE]>) -> Aligned<[f32; L3_SIZE]> {
+pub fn propagate_l2(l1_out: Aligned<[f32; L2_SIZE]>, bucket: usize) -> Aligned<[f32; L3_SIZE]> {
     let mut output = Aligned::new([0.0; L3_SIZE]);
 
     for i in 0..L2_SIZE {
         for j in 0..L3_SIZE {
-            output[j] += PARAMETERS.l2_weights[i][j] * l1_out[i];
+            output[j] += PARAMETERS.l2_weights[bucket][i][j] * l1_out[i];
         }
     }
 
     for i in 0..L3_SIZE {
-        output[i] += PARAMETERS.l2_biases[i];
+        output[i] += PARAMETERS.l2_biases[bucket][i];
         output[i] = output[i].clamp(0.0, 1.0);
     }
     output
 }
 
-pub fn propagate_l3(l2_out: Aligned<[f32; L3_SIZE]>) -> f32 {
+pub fn propagate_l3(l2_out: Aligned<[f32; L3_SIZE]>, bucket: usize) -> f32 {
     let mut output = 0.0;
     for i in 0..L3_SIZE {
-        output = PARAMETERS.l3_weights[i].mul_add(l2_out[i], output);
+        output = PARAMETERS.l3_weights[bucket][i].mul_add(l2_out[i], output);
     }
-    output + PARAMETERS.l3_biases
+    output + PARAMETERS.l3_biases[bucket]
+}
+
+pub unsafe fn find_nnz(ft_out: &Aligned<[u8; L1_SIZE]>, _: &[SparseEntry]) -> (Aligned<[u16; L1_SIZE / 4]>, usize) {
+    let mut indexes = Aligned::new([0; L1_SIZE / 4]);
+    let mut count = 0;
+
+    for i in 0..L1_SIZE / 4 {
+        let mut nonzero = 0;
+
+        for j in 0..4 {
+            nonzero |= ft_out[i * 4 + j];
+        }
+
+        if nonzero != 0 {
+            indexes[count] = i as u16;
+            count += 1;
+        }
+    }
+
+    (indexes, count)
 }
