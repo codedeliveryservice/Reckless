36016abfe862852d9f76563bb603116fe6e8f225
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-01-30 12:23:32 +0300

    Bench: 4423543

--- src/search.rs
+++ src/search.rs
@@ -1219,14 +1219,12 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32, ply: 
 
         move_count += 1;
 
-        if !is_loss(best_score) && mv.to() != td.board.recapture_square() {
+        if !is_loss(best_score) && !in_check && mv.to() != td.board.recapture_square() {
             if !NODE::PV && move_count >= 3 && !td.board.is_direct_check(mv) {
                 break;
             }
 
-            let futility_score = best_score + 42 * td.board.piece_on(mv.to()).piece_type().value() / 128 + 104;
-
-            if !in_check && futility_score <= alpha && !td.board.see(mv, 1) {
+            if best_score + 42 * td.board.piece_on(mv.to()).value() / 128 + 104 <= alpha && !td.board.see(mv, 1) {
                 continue;
             }
         }
