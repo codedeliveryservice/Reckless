d5d4ec62d1966bd03cacd43c6a57af7811cbe1c3
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-01-31 01:41:39 +0300

    Bench: 4267340

--- src/search.rs
+++ src/search.rs
@@ -990,19 +990,6 @@ fn search<NODE: NodeType>(
 
     if best_move.is_some() {
         let mut noisy_bonus = (106 * depth).min(808) - 54 - 80 * cut_node as i32;
-
-        if best_score >= 0
-            && td.noisy_history.get(
-                td.board.threats(),
-                td.board.moved_piece(best_move),
-                best_move.to(),
-                td.board.piece_on(best_move.to()).piece_type(),
-            ) < 0
-            && !td.board.see(best_move, 0)
-        {
-            noisy_bonus *= 5;
-        }
-
         let noisy_malus = (164 * depth).min(1329) - 52 - 23 * noisy_moves.len() as i32;
 
         let quiet_bonus = (172 * depth).min(1459) - 78 - 54 * cut_node as i32;
@@ -1012,6 +999,10 @@ fn search<NODE: NodeType>(
         let cont_malus = (352 * depth).min(868) - 47 - 19 * quiet_moves.len() as i32;
 
         if best_move.is_noisy() {
+            if !td.board.see(best_move, 0) {
+                noisy_bonus *= 2;
+            }
+
             td.noisy_history.update(
                 td.board.threats(),
                 td.board.moved_piece(best_move),
