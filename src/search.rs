9801be1defe3737c0389207042544ad445500f41
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-02-02 03:19:40 +0300

    Bench: 3859826

--- src/search.rs
+++ src/search.rs
@@ -488,7 +488,7 @@ fn search<NODE: NodeType>(
         && estimated_score
             >= beta + 1125 * depth * depth / 128 + 26 * depth - (77 * improving as i32)
                 + 519 * correction_value.abs() / 1024
-                + 32 * (depth == 1) as i32
+                + 64 * (depth == 1) as i32
         && !is_loss(beta)
         && !is_win(estimated_score)
     {
