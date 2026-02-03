e25740ab7640ff2d5a339ebfbe15618be05f39c5
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-02-03 10:02:17 +0300

    Bench: 3624502

--- src/search.rs
+++ src/search.rs
@@ -513,7 +513,7 @@ fn search<NODE: NodeType>(
     {
         debug_assert_ne!(td.stack[ply - 1].mv, Move::NULL);
 
-        let r = (5154 + 271 * depth + 535 * (estimated_score - beta).clamp(0, 1073) / 128) / 1024;
+        let r = (5154 + 271 * depth + 535 * (eval - beta) / 128) / 1024;
 
         td.stack[ply].conthist = td.stack.sentinel().conthist;
         td.stack[ply].contcorrhist = td.stack.sentinel().contcorrhist;
