ca64f7ad8770accd9e0c5eaa85ede325ac8584d5
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-02-04 20:25:39 +0300

    Bench: 4262482

--- build/build.rs
+++ build/build.rs
@@ -11,7 +11,7 @@ mod magics;
 mod maps;
 
 const BASE_URL: &str = "https://github.com/codedeliveryservice/RecklessNetworks/releases/download/networks";
-const NETWORK_NAME: &str = "v52-dd20aadb.nnue";
+const NETWORK_NAME: &str = "v53-0ba42a8c.nnue";
 
 fn main() {
     generate_model_env();
