08c4cc97c4c13fe7de802883b80104ff1db901ac
cj5716 <125858804+cj5716@users.noreply.github.com>
2026-02-04 22:07:31 +0800

    Bench 3791106

--- build/build.rs
+++ build/build.rs
@@ -11,7 +11,7 @@ mod magics;
 mod maps;
 
 const BASE_URL: &str = "https://github.com/codedeliveryservice/RecklessNetworks/releases/download/networks";
-const NETWORK_NAME: &str = "v51-3f72a764.nnue";
+const NETWORK_NAME: &str = "v52-dd20aadb.nnue";
 
 fn main() {
     generate_model_env();
