e23912e2ae0887ebadf66002d2446be012107eb8
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-02-13 19:35:44 +0300

    Bench: 3196394

--- src/search.rs
+++ src/search.rs
@@ -821,8 +821,8 @@ fn search<NODE: NodeType>(
 
             if score > alpha {
                 if !NODE::ROOT {
-                    new_depth += (score > best_score + 41 + 447 * depth / 128) as i32;
-                    new_depth -= (score < best_score + new_depth) as i32;
+                    new_depth += (score > best_score + 41 + 447 * reduced_depth / 128) as i32;
+                    new_depth -= (score < best_score + reduced_depth) as i32;
                 }
 
                 if new_depth > reduced_depth {
