22ac0a9c77c675c196f9cf83fa95bc0408fd6dc7
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-02-15 10:19:42 +0300

    Bench: 3368326

--- src/search.rs
+++ src/search.rs
@@ -814,8 +814,11 @@ fn search<NODE: NodeType>(
                 reduction += 600;
             }
 
-            let reduced_depth =
-                (new_depth - reduction / 1024).clamp((depth >= 2) as i32, new_depth + 1) + 2 * NODE::PV as i32;
+            if depth == 1 {
+                reduction = reduction.max(0);
+            }
+
+            let reduced_depth = (new_depth - reduction / 1024).clamp(1, new_depth + 1) + 2 * NODE::PV as i32;
 
             td.stack[ply].reduction = reduction;
             score = -search::<NonPV>(td, -alpha - 1, -alpha, reduced_depth, true, ply + 1);
