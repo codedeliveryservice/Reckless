1b4f0f0daed472f91051472d63a8e093bd9f3724
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-02-16 18:42:42 +0300

    Bench: 3391810

--- src/nnue.rs
+++ src/nnue.rs
@@ -59,7 +59,7 @@ const INPUT_BUCKETS: usize = 10;
 const OUTPUT_BUCKETS: usize = 8;
 
 const L1_SIZE: usize = 768;
-const L2_SIZE: usize = 16;
+const L2_SIZE: usize = 32;
 const L3_SIZE: usize = 32;
 
 const FT_QUANT: i32 = 255;
