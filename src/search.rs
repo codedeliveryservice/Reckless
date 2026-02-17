a28d9431e71a40e69a6ff447f6a8fe6ea7821be6
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-02-17 21:02:56 +0300

    Bench: 3915571

--- src/search.rs
+++ src/search.rs
@@ -219,7 +219,7 @@ pub fn start(td: &mut ThreadData, report: Report, thread_count: usize) {
 
             let eval_stability = (1.2 - 0.04 * eval_stability as f32).max(0.88);
 
-            let score_trend = (0.4
+            let score_trend = (0.2
                 + 0.05 * (td.previous_best_score - td.root_moves[0].score) as f32
                 + 0.05 * (score_history[0] - td.root_moves[0].score) as f32)
                 .clamp(0.75, 1.5);
