ab3a3f961ceecdcfe7ae3e8fc1d6c69888eb9bc3
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-02-20 22:00:34 +0300

    Bench: 3441731

--- src/search.rs
+++ src/search.rs
@@ -516,7 +516,7 @@ fn search<NODE: NodeType>(
         && !potential_singularity
         && estimated_score >= beta
         && estimated_score >= eval
-        && eval >= beta - 9 * depth + 126 * tt_pv as i32 - 128 * improvement / 1024 + 286
+        && eval >= beta - 9 * depth - 128 * improvement / 1024 + 286
         && ply as i32 >= td.nmp_min_ply
         && td.stack[ply - 1].mv.is_some()
         && td.board.has_non_pawns()
