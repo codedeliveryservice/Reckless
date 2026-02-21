b4c8c8c2bf79157c51fbbc1e49336e1f70adc9d3
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-02-21 18:18:07 +0300

    Implement `genfens`

--- src/tools/mod.rs
+++ src/tools/mod.rs
@@ -1,8 +1,10 @@
 mod bench;
+mod genfens;
 mod perft;
 mod speedtest;
 
 pub use bench::bench;
+pub use genfens::genfens;
 pub use perft::is_legal_perft;
 pub use perft::perft;
 pub use perft::simple_perft;
