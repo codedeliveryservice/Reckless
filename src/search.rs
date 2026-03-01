4ad5366d72ac3e5c584238c7143520ef7b369d8b
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-03-01 16:19:46 +0300

    Bench: 3626997

--- src/search.rs
+++ src/search.rs
@@ -1046,7 +1046,7 @@ fn search<NODE: NodeType>(
             }
         } else if pcm_move.is_noisy() {
             let captured = td.board.captured_piece().unwrap_or_default().piece_type();
-            let bonus = (30 * depth - 15).min(400);
+            let bonus = (40 * depth - 20).min(600);
 
             td.noisy_history.update(
                 td.board.prior_threats(),
