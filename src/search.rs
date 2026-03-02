4f51eef28c87d2061821ae24f546e1816fecb6f8
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-03-02 07:56:04 +0300

    Bench: 3936155

--- src/search.rs
+++ src/search.rs
@@ -115,7 +115,7 @@ pub fn start(td: &mut ThreadData, report: Report, thread_count: usize) {
             }
 
             // Aspiration Windows
-            delta += average[td.pv_index] * average[td.pv_index] / 23660;
+            delta += td.root_moves[td.pv_index].mean_squared_score / 23660;
 
             let mut alpha = (average[td.pv_index] - delta).max(-Score::INFINITE);
             let mut beta = (average[td.pv_index] + delta).min(Score::INFINITE);
@@ -153,11 +153,14 @@ pub fn start(td: &mut ThreadData, report: Report, thread_count: usize) {
                         delta += 63 * delta / 128;
                     }
                     _ => {
-                        average[td.pv_index] = if average[td.pv_index] == Score::NONE {
-                            score
+                        if average[td.pv_index] == Score::NONE {
+                            average[td.pv_index] = score;
+                            td.root_moves[td.pv_index].mean_squared_score = score * score;
                         } else {
-                            (average[td.pv_index] + score) / 2
-                        };
+                            average[td.pv_index] = (average[td.pv_index] + score) / 2;
+                            td.root_moves[td.pv_index].mean_squared_score =
+                                (td.root_moves[td.pv_index].mean_squared_score + score * score) / 2;
+                        }
 
                         td.shared.best_stats[td.pv_index].fetch_max(
                             ((depth as u32) << 16) | (average[td.pv_index] + 32768) as u32,
