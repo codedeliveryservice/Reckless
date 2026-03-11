e2a2e8788efe7dc4eaad5e5a7bc2353a0daf78a7
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-03-11 23:12:45 +0300

    Bench: 3242700

--- src/search.rs
+++ src/search.rs
@@ -1205,7 +1205,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32, ply: 
     let mut move_picker = MovePicker::new_qsearch();
 
     let skip_quiets = |best_score| {
-        !((in_check && is_loss(best_score)) || (!NODE::PV && tt_move.is_quiet() && tt_bound != Bound::Upper))
+        !((in_check && is_loss(best_score)) || (tt_move.is_quiet() && tt_bound != Bound::Upper))
     };
 
     while let Some(mv) = move_picker.next::<NODE>(td, skip_quiets(best_score), ply) {
