1f1743d82011bee73f1afb6f7c511ec0bfb87bdc
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-03-19 17:35:16 +0300

    Bench: 3249983

--- src/search.rs
+++ src/search.rs
@@ -772,7 +772,9 @@ fn search<NODE: NodeType>(
 
             reduction -= 65 * move_count;
             reduction -= 3183 * correction_value.abs() / 1024;
+
             reduction += 1300 * alpha_raises;
+            reduction += 256 * (tt_move.is_noisy() as i32);
 
             reduction += 600 * (is_valid(tt_score) && tt_score < alpha) as i32;
             reduction += 300 * (is_valid(tt_score) && tt_depth < depth) as i32;
