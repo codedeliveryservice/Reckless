32f923f99ff1f3059edf403212149f458149ce93
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-03-22 07:29:16 +0300

    Bench: 3099393

--- src/search.rs
+++ src/search.rs
@@ -480,12 +480,6 @@ fn search<NODE: NodeType>(
         depth -= 1;
     }
 
-    let potential_singularity = depth >= 5 + tt_pv as i32
-        && tt_depth >= depth - 3
-        && tt_bound != Bound::Upper
-        && is_valid(tt_score)
-        && !is_decisive(tt_score);
-
     let mut improvement = 0;
 
     if is_valid(td.stack[ply - 2].eval) && !in_check {
@@ -526,18 +520,18 @@ fn search<NODE: NodeType>(
     if cut_node
         && !in_check
         && !excluded
-        && !potential_singularity
+        && tt_depth < depth
         && estimated_score >= beta
         && estimated_score
             >= beta - 9 * depth + 126 * tt_pv as i32 - 128 * improvement / 1024 + 286
                 - 20 * (td.stack[ply + 1].cutoff_count < 2) as i32
         && ply as i32 >= td.nmp_min_ply
         && td.board.has_non_pawns()
-        && !is_loss(beta)
-        && !(tt_bound == Bound::Lower
-            && tt_move.is_present()
+        && !(tt_move.is_present()
             && tt_move.is_capture()
             && td.board.piece_on(tt_move.to()).value() >= PieceType::Knight.value())
+        && (!is_valid(tt_score) || tt_bound == Bound::Lower)
+        && !is_loss(beta)
     {
         debug_assert_ne!(td.stack[ply - 1].mv, Move::NULL);
 
@@ -636,7 +630,15 @@ fn search<NODE: NodeType>(
     // Singular Extensions (SE)
     let mut extension = 0;
 
-    if !NODE::ROOT && !excluded && potential_singularity && ply < 2 * td.root_depth as isize {
+    if !NODE::ROOT
+        && !excluded
+        && depth >= 5 + tt_pv as i32
+        && tt_depth >= depth - 3
+        && tt_bound != Bound::Upper
+        && is_valid(tt_score)
+        && !is_decisive(tt_score)
+        && ply < 2 * td.root_depth as isize
+    {
         debug_assert!(is_valid(tt_score));
 
         let singular_beta = tt_score - depth - depth * (tt_pv && !NODE::PV) as i32;
