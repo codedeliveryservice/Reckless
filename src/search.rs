8e90e3046cc99db14d774d43e5630c775f3eedb8
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-03-25 04:01:59 +0300

    Bench: 3274011

--- src/search.rs
+++ src/search.rs
@@ -642,7 +642,7 @@ fn search<NODE: NodeType>(
     // Singular Extensions (SE)
     let mut extension = 0;
 
-    if !NODE::ROOT && !excluded && potential_singularity && ply < 2 * td.root_depth as isize {
+    if !NODE::ROOT && !excluded && potential_singularity {
         debug_assert!(is_valid(tt_score));
 
         let singular_beta = tt_score - depth - depth * (tt_pv && !NODE::PV) as i32;
