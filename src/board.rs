dcaef5e7e7dc5bd77c10c1548006a30daff4a7da
Shahin M. Shahin <peregrineshahin@gmail.com>
2026-03-26 05:59:52 +0300

    Assume we will be slow in finding an upcoming repetition if ply is greater than 20
    Bench: 3566361

--- src/board.rs
+++ src/board.rs
@@ -291,7 +291,7 @@ impl Board {
     /// <http://web.archive.org/web/20201107002606/https://marcelk.net/2013-04-06/paper/upcoming-rep-v2.pdf>
     pub fn upcoming_repetition(&self, ply: usize) -> bool {
         let hm = self.state.plies_from_null.min(self.state.halfmove_clock as usize);
-        if hm < 3 {
+        if hm < 3 || ply > 20 {
             return false;
         }
 
