0ba110c26e605b6d17b493e0a27e3e5913e105d6
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-03-28 14:00:31 +0300

    Simplify do-deeper margin (#828)
    STC
    Elo   | 1.45 +- 2.05 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.92 (-2.25, 2.89) [-2.75, 0.25]
    Games | N: 28790 W: 7292 L: 7172 D: 14326
    Penta | [72, 3395, 7346, 3505, 77]
    https://recklesschess.space/test/12768/
    
    LTC
    Elo   | 3.13 +- 2.66 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.90 (-2.25, 2.89) [-2.75, 0.25]
    Games | N: 14984 W: 3743 L: 3608 D: 7633
    Penta | [4, 1636, 4076, 1773, 3]
    https://recklesschess.space/test/12769/
    
    Bench: 3260740

--- src/search.rs
+++ src/search.rs
@@ -843,7 +843,7 @@ fn search<NODE: NodeType>(
 
             if score > alpha {
                 if !NODE::ROOT {
-                    new_depth += (score > best_score + 50 + 4 * reduced_depth) as i32;
+                    new_depth += (score > best_score + 60) as i32;
                     new_depth += (score > best_score + 768) as i32;
                     new_depth -= (score < best_score + 5 + reduced_depth) as i32;
                 }
