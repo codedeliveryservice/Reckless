7d6f4589cfdb75e242c16485efefa31e4a869de1
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-04-05 00:43:30 +0300

    Bench: 2885778

--- src/search.rs
+++ src/search.rs
@@ -1221,6 +1221,10 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32, ply: 
                 break;
             }
 
+            if move_picker.stage() == Stage::BadNoisy && td.board.piece_on(mv.to()).value() < PieceType::Rook.value() {
+                continue;
+            }
+
             // Static Exchange Evaluation Pruning (SEE Pruning)
             if is_valid(eval) && !td.board.see(mv, (alpha - eval) / 8 - 100) {
                 continue;
