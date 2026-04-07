551e80f5fb9d930ee8c9fa54e79755386f51080d
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-04-08 02:13:25 +0300

    Bench: 2890939

--- src/movepick.rs
+++ src/movepick.rs
@@ -36,12 +36,12 @@ impl MovePicker {
         }
     }
 
-    pub const fn new_probcut(threshold: i32) -> Self {
+    pub const fn new_probcut(tt_move: Move, threshold: i32) -> Self {
         Self {
             list: MoveList::new(),
-            tt_move: Move::NULL,
+            tt_move,
             threshold: Some(threshold),
-            stage: Stage::GenerateNoisy,
+            stage: if tt_move.is_present() { Stage::HashMove } else { Stage::GenerateNoisy },
             bad_noisy: ArrayVec::new(),
             bad_noisy_idx: 0,
         }
--- src/search.rs
+++ src/search.rs
@@ -595,9 +595,8 @@ fn search<NODE: NodeType>(
     if cut_node
         && !is_win(beta)
         && if is_valid(tt_score) { tt_score >= probcut_beta && !is_decisive(tt_score) } else { eval >= beta }
-        && !tt_move.is_quiet()
     {
-        let mut move_picker = MovePicker::new_probcut(probcut_beta - eval);
+        let mut move_picker = MovePicker::new_probcut(tt_move, probcut_beta - eval);
 
         while let Some(mv) = move_picker.next::<NODE>(td, true, ply) {
             if move_picker.stage() == Stage::BadNoisy {
