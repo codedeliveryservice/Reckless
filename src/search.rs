ed73260f12853ea1b1fa6b1b5095bd6ddef0facd
87 <dev@87flowers.com>
2026-04-11 23:12:23 +0000

    Discard TT move if potentially better move exists (#878)
    STC
    Elo   | 2.16 +- 1.68 (95%)
    SPRT  | 8.0+0.08s Threads=1 Hash=16MB
    LLR   | 2.92 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 43302 W: 11045 L: 10776 D: 21481
    Penta | [121, 5066, 11006, 5339, 119]
    https://recklesschess.space/test/13320/
    
    LTC
    Elo   | 2.62 +- 1.89 (95%)
    SPRT  | 40.0+0.40s Threads=1 Hash=64MB
    LLR   | 2.94 (-2.25, 2.89) [0.00, 3.00]
    Games | N: 29744 W: 7503 L: 7279 D: 14962
    Penta | [11, 3243, 8143, 3461, 14]
    https://recklesschess.space/test/13322/
    
    Bench: 2718318

--- src/search.rs
+++ src/search.rs
@@ -678,7 +678,9 @@ fn search<NODE: NodeType>(
             return (2 * score + beta) / 3;
         }
         // Negative Extensions
-        else if tt_score >= beta {
+        else if score > tt_score {
+            tt_move = Move::NULL;
+        } else if tt_score >= beta {
             extension = -2;
         } else if cut_node {
             extension = -2;
