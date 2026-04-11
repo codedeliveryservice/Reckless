62b895adf1f25bce486047559743f4f2c8802c9d
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-04-11 08:53:14 +0300

    Bench: 2937511

--- src/search.rs
+++ src/search.rs
@@ -1224,7 +1224,7 @@ fn qsearch<NODE: NodeType>(td: &mut ThreadData, mut alpha: i32, beta: i32, ply: 
             }
 
             // Static Exchange Evaluation Pruning (SEE Pruning)
-            if is_valid(eval) && !td.board.see(mv, (alpha - eval) / 8 - correction_value.abs().min(64) - 79) {
+            if is_valid(eval) && !td.board.see(mv, (alpha - eval) / 8 - correction_value.abs() / 2 - 79) {
                 continue;
             }
         }
