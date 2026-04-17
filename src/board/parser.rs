5dc41bce171baee736deaf0fc060789d6edf3c81
Michael Whiteley <protonspring@gmail.com>
2026-04-17 11:27:43 -0600

    cargo formatting.
    bench 2934206

--- src/board/parser.rs
+++ src/board/parser.rs
@@ -88,10 +88,8 @@ impl Board {
 
             let rights = CastlingKind::KINDS[color][king_side];
 
-            let king_to =
-                Square::from_rank_file(HOME_RANK[color].clone() as u8, KING_TO_FILE[king_side].clone() as u8);
-            let rook_to =
-                Square::from_rank_file(HOME_RANK[color].clone() as u8, ROOK_TO_FILE[king_side].clone() as u8);
+            let king_to = Square::from_rank_file(HOME_RANK[color].clone() as u8, KING_TO_FILE[king_side].clone() as u8);
+            let rook_to = Square::from_rank_file(HOME_RANK[color].clone() as u8, ROOK_TO_FILE[king_side].clone() as u8);
 
             self.set_castling_for(rights, king_from, king_to, rook_from, rook_to);
         }
