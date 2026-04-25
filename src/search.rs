f51fc1d90e11887c184523e749ed85d2938eefa9
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-04-25 07:49:34 +0300

    Bench: 2679048

--- src/search.rs
+++ src/search.rs
@@ -593,7 +593,7 @@ fn search<NODE: NodeType>(
         && if is_valid(tt_score) { tt_score >= probcut_beta && !is_decisive(tt_score) } else { eval >= beta }
         && !tt_move.is_quiet()
     {
-        let mut move_picker = MovePicker::new_probcut(probcut_beta - eval);
+        let mut move_picker = MovePicker::new_probcut((probcut_beta - eval).max(0));
 
         while let Some(mv) = move_picker.next::<NODE>(td, true, ply) {
             if move_picker.stage() == Stage::BadNoisy {
