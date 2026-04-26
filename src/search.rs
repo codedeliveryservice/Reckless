357ffece5f643976e7cf54dd1f886d5f4e87634d
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-04-26 19:57:34 +0300

    Bench: 2887564

--- src/search.rs
+++ src/search.rs
@@ -585,7 +585,8 @@ fn search<NODE: NodeType>(
 
     if cut_node
         && !is_win(beta)
-        && if is_valid(tt_score) { tt_score >= probcut_beta && !is_decisive(tt_score) } else { eval >= beta }
+        && ((is_valid(tt_score) && !is_decisive(tt_score) && tt_score >= probcut_beta)
+            || (is_valid(eval) && eval >= beta))
         && !tt_move.is_quiet()
     {
         let mut move_picker = MovePicker::new_probcut(probcut_beta - eval);
