d639038fedbc59e53829451dc788aa434f4182f7
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-04-27 14:21:16 +0300

    Bench: 2789899

--- src/search.rs
+++ src/search.rs
@@ -133,7 +133,7 @@ pub fn start(td: &mut ThreadData, report: Report, thread_count: usize) {
 
                 match score {
                     s if s <= alpha => {
-                        beta = (3 * alpha + beta) / 4;
+                        beta = (score + delta).min(beta);
                         alpha = (score - delta).max(-Score::INFINITE);
                         reduction = 0;
                         delta += 28 * delta / 128;
