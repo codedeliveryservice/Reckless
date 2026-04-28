2dc5a9cd21f8bf6defbcabb9d73281eb119dd737
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-04-28 13:33:02 +0300

    Bench: 2286147

--- src/search.rs
+++ src/search.rs
@@ -795,7 +795,7 @@ fn search<NODE: NodeType>(
             }
 
             if NODE::PV {
-                reduction -= 424 + 433 * (beta - alpha) / td.root_delta;
+                reduction -= 1192 + 433 * (beta - alpha) / td.root_delta;
             }
 
             if tt_pv {
@@ -833,7 +833,7 @@ fn search<NODE: NodeType>(
             reduction += ((td.nodes() + td.id as u64 * 23) % 128) as i32 - 64;
 
             let reduced_depth =
-                (new_depth - reduction / 1024).clamp(1, new_depth + (move_count <= 3) as i32 + 1) + 2 * NODE::PV as i32;
+                (new_depth - reduction / 1024).clamp(1, new_depth + (move_count <= 3) as i32 + 1) + NODE::PV as i32;
 
             td.stack[ply].reduction = reduction;
             score = -search::<NonPV>(td, -alpha - 1, -alpha, reduced_depth, true, ply + 1);
