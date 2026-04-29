8624737b75b045cbed6fc074f9b0d88eb1959fb8
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-04-29 05:57:18 +0300

    Bench: 2569711

--- src/search.rs
+++ src/search.rs
@@ -196,10 +196,10 @@ pub fn start(td: &mut ThreadData, report: Report, thread_count: usize) {
         let multiplier = || {
             let nodes = {
                 let fraction = td.root_moves[0].nodes as f32 / td.nodes() as f32;
-                2.250 - 1.500 * fraction
+                (2.415 - 1.875 * fraction).clamp(0.562, 2.399)
             };
 
-            let best_move_changes = 1.000 + (0.2500 * td.best_move_changes as f32).ln_1p();
+            let best_move_changes = 1.088 + (0.288 * td.best_move_changes as f32).ln_1p();
 
             nodes * best_move_changes
         };
