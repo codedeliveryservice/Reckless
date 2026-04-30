3247d33ad79b12dc2958c761c74887c125a491f9
87 <dev@87flowers.com>
2026-04-30 13:09:02 +0200

    Bench: 2982858

--- build/build.rs
+++ build/build.rs
@@ -83,8 +83,7 @@ fn write(mut buf: BufWriter<File>) -> Result<(), std::io::Error> {
     write_map!("KING_MAP", "u64", maps::generate_king_map());
     write_map!("KNIGHT_MAP", "u64", maps::generate_knight_map());
 
-    write_map!("WHITE_PAWN_MAP", "u64", maps::generate_white_pawn_map());
-    write_map!("BLACK_PAWN_MAP", "u64", maps::generate_black_pawn_map());
+    write_map!("PAWN_MAP", "[u64; 64]", maps::generate_pawn_map());
 
     write_map!("ROOK_MAP", "u64", maps::generate_rook_map());
     write_map!("BISHOP_MAP", "u64", maps::generate_bishop_map());
