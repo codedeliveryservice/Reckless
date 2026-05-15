502d2f36eaf50b5261383ddb78653bae1487c92c
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-05-15 20:06:46 +0300

    Bench: 2267345

--- src/search.rs
+++ src/search.rs
@@ -663,6 +663,7 @@ fn search<NODE: NodeType>(
         td.stack[ply].mv = Move::NULL;
         singular_score = search::<NonPV>(td, singular_beta - 1, singular_beta, singular_depth, cut_node, ply);
         td.stack[ply].excluded = Move::NULL;
+        td.stack[ply].tt_pv = tt_pv;
 
         if td.shared.status.get() == Status::STOPPED {
             return Score::ZERO;
