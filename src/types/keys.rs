bf3340670ef7660a361c330eae7de92cc13976cf
87 <dev@87flowers.com>
2026-05-25 22:10:59 +0200

    Bench: 3466720

--- src/types/keys.rs
+++ src/types/keys.rs
@@ -1,4 +1,4 @@
-use crate::types::{Castling, Color, Piece, PieceType, Square, ZOBRIST};
+use crate::types::{Castling, Color, Piece, Square, ZOBRIST};
 
 #[repr(C)]
 #[derive(Clone, Copy, Default)]
@@ -37,10 +37,12 @@ impl Keys {
         self.update_full(ZOBRIST.en_passant[en_passant]);
     }
 
+    #[cfg(not(target_feature = "avx2"))]
     fn update_full(&mut self, key: u64) {
         self.full ^= key;
     }
 
+    #[cfg(not(target_feature = "avx2"))]
     fn update_piece(&mut self, piece: Piece, piece_key: u64) {
         self.full ^= piece_key;
 
@@ -49,4 +51,75 @@ impl Keys {
             _ => self.non_pawn[piece.color()] ^= piece_key,
         }
     }
+
+    #[cfg(target_feature = "avx2")]
+    fn update_full(&mut self, key: u64) {
+        use std::arch::x86_64::*;
+
+        unsafe {
+            let ptr = self as *mut Keys as *mut __m256i;
+            let keys = _mm256_loadu_si256(ptr);
+            let key = _mm256_zextsi128_si256(_mm_cvtsi64_si128(key as i64));
+            _mm256_storeu_si256(ptr, _mm256_xor_si256(keys, key));
+        }
+    }
+
+    #[cfg(all(target_feature = "avx2", not(target_feature = "avx512f")))]
+    fn update_piece(&mut self, piece: Piece, piece_key: u64) {
+        use std::arch::x86_64::*;
+
+        unsafe {
+            let ptr = self as *mut Keys as *mut __m256i;
+            let keys = _mm256_loadu_si256(ptr);
+            let key = _mm256_and_si256(_mm256_set1_epi64(key as i64), PIECE_MASK[piece]);
+            _mm256_storeu_si256(ptr, _mm256_xor_si256(keys, key));
+        }
+    }
+
+    #[cfg(target_feature = "avx512f")]
+    fn update_piece(&mut self, piece: Piece, piece_key: u64) {
+        use std::arch::x86_64::*;
+
+        unsafe {
+            let ptr = self as *mut Keys as *mut __m256i;
+            let keys = _mm256_loadu_si256(ptr);
+            let piece_key = _mm256_maskz_set1_epi64(PIECE_MASK[piece], piece_key as i64);
+            _mm256_storeu_si256(ptr, _mm256_xor_si256(keys, piece_key));
+        }
+    }
 }
+
+#[cfg(all(target_feature = "avx2", not(target_feature = "avx512f")))]
+type PieceMask = __m256i;
+#[cfg(target_feature = "avx512f")]
+type PieceMask = u8;
+
+#[cfg(all(target_feature = "avx2", not(target_feature = "avx512f")))]
+const PAWN: __m256i = unsafe { std::arch::x86_64::_mm256_set_epi64x(0, 0, -1, -1) };
+#[cfg(all(target_feature = "avx2", not(target_feature = "avx512f")))]
+const WHITE_NON_PAWN: __m256i = unsafe { std::arch::x86_64::_mm256_set_epi64x(0, -1, 0, -1) };
+#[cfg(all(target_feature = "avx2", not(target_feature = "avx512f")))]
+const BLACK_NON_PAWN: __m256i = unsafe { std::arch::x86_64::_mm256_set_epi64x(-1, 0, 0, -1) };
+
+#[cfg(target_feature = "avx512f")]
+const PAWN: u8 = 0b0011;
+#[cfg(target_feature = "avx512f")]
+const WHITE_NON_PAWN: u8 = 0b0101;
+#[cfg(target_feature = "avx512f")]
+const BLACK_NON_PAWN: u8 = 0b1001;
+
+#[cfg(all(target_feature = "avx2"))]
+const PIECE_MASK: [PieceMask; 12] = [
+    PAWN,
+    PAWN,
+    WHITE_NON_PAWN,
+    BLACK_NON_PAWN,
+    WHITE_NON_PAWN,
+    BLACK_NON_PAWN,
+    WHITE_NON_PAWN,
+    BLACK_NON_PAWN,
+    WHITE_NON_PAWN,
+    BLACK_NON_PAWN,
+    WHITE_NON_PAWN,
+    BLACK_NON_PAWN,
+];
