010afbdebc3da6c7a4ffdb9549d9023427c9e366
Arseniy Surkov <93079612+codedeliveryservice@users.noreply.github.com>
2026-05-25 16:38:37 +0300

    Bench: 3074832

--- src/search.rs
+++ src/search.rs
@@ -797,17 +797,19 @@ fn search<NODE: NodeType>(
         if depth >= 2 && move_count >= 2 {
             let mut reduction = 256 * depth.ilog2() as i32;
 
+            reduction -= (238 * improvement / 128).clamp(-600, 300);
             reduction -= 3403 * correction_value.abs() / 1024;
+
             reduction += 1405 * (bound == Bound::Exact) as i32;
 
             reduction += 459 * (is_valid(tt_score) && tt_score <= alpha) as i32;
             reduction += 286 * (is_valid(tt_score) && tt_depth < depth) as i32;
 
             if is_quiet {
-                reduction += 1971;
+                reduction += 2271;
                 reduction -= 179 * history / 1024;
             } else {
-                reduction += 1424;
+                reduction += 1574;
                 reduction -= 107 * history / 1024;
             }
 
@@ -824,10 +826,6 @@ fn search<NODE: NodeType>(
             if !tt_pv && cut_node {
                 reduction += 1810;
                 reduction += 2113 * tt_move.is_null() as i32;
-            }
-
-            if !improving {
-                reduction += (414 - 238 * improvement / 128).min(1014);
             }
 
             if td.board.in_check() {
@@ -873,13 +871,14 @@ fn search<NODE: NodeType>(
         else if !NODE::PV || move_count >= 2 {
             let mut reduction = 243 * depth.ilog2() as i32;
 
+            reduction -= (232 * improvement / 128).clamp(-1024, 384);
             reduction -= 2382 * correction_value.abs() / 1024;
 
             if is_quiet {
-                reduction += 1385;
+                reduction += 1485;
                 reduction -= 136 * history / 1024;
             } else {
-                reduction += 1049;
+                reduction += 1149;
                 reduction -= 55 * history / 1024;
             }
 
@@ -891,10 +890,6 @@ fn search<NODE: NodeType>(
             if !tt_pv && cut_node {
                 reduction += 1366;
                 reduction += 2045 * tt_move.is_null() as i32;
-            }
-
-            if !improving {
-                reduction += (402 - 232 * improvement / 128).min(1426);
             }
 
             if td.stack[ply + 1].cutoff_count > 2 {
