fc5e4c0b712ac7a531564fc3a080f80e76310f57
87 <dev@87flowers.com>
2026-05-25 21:52:33 +0200

    split
    Bench: 3466720

--- src/types/keys.rs
+++ src/types/keys.rs
@@ -22,25 +22,31 @@ impl Keys {
     }
 
     pub fn toggle(&mut self, piece: Piece, sq: Square) {
-        let piece_key = ZOBRIST.pieces[piece][sq];
+        self.update_piece(piece, ZOBRIST.pieces[piece][sq]);
+    }
 
+    pub fn toggle_side(&mut self) {
+        self.update_full(ZOBRIST.side);
+    }
+
+    pub fn toggle_castling(&mut self, castling: Castling) {
+        self.update_full(ZOBRIST.castling[castling]);
+    }
+
+    pub fn toggle_en_passant(&mut self, en_passant: Square) {
+        self.update_full(ZOBRIST.en_passant[en_passant]);
+    }
+
+    fn update_full(&mut self, key: u64) {
+        self.full ^= key;
+    }
+
+    fn update_piece(&mut self, piece: Piece, piece_key: u64) {
         self.full ^= piece_key;
 
         match piece.piece_type() {
             PieceType::Pawn => self.pawn ^= piece_key,
             _ => self.non_pawn[piece.color()] ^= piece_key,
         }
-    }
-
-    pub fn toggle_side(&mut self) {
-        self.full ^= ZOBRIST.side;
-    }
-
-    pub fn toggle_castling(&mut self, castling: Castling) {
-        self.full ^= ZOBRIST.castling[castling];
-    }
-
-    pub fn toggle_en_passant(&mut self, en_passant: Square) {
-        self.full ^= ZOBRIST.en_passant[en_passant];
     }
 }
