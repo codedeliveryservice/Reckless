903390d7064be9d29c3c1b1660e09af559b9143b
87 <dev@87flowers.com>
2026-07-01 17:31:23 +0200

    Bench: 2862950

--- src/search.rs
+++ src/search.rs
@@ -737,7 +737,7 @@ fn search<NODE: NodeType>(
 
     // Internal Iterative Deepening (IID)
     if !NODE::ROOT && NODE::PV && depth >= 8 && !in_check && !excluded && tt_move.is_null() {
-        let _ = search::<PV>(td, alpha, beta, (3 * depth - 7) / 4, cut_node, ply);
+        let _ = search::<PV>(td, alpha, beta, (768 * depth - 1792) / 1024, cut_node, ply);
 
         if let Some(entry) = td.shared.tt.read(hash, td.board.fiftymove_clock(), ply) {
             tt_move = entry.mv;
